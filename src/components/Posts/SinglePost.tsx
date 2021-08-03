67f45cc7c790b2fbe69e3a9ebcf0f37aeab29ea9
Yogi <yoginth@hey.com>
2021-08-03 10:35:53 +0530

    update: Single Post

--- src/components/Posts/SinglePost.tsx
+++ src/components/Posts/SinglePost.tsx
@@ -4,6 +4,7 @@ import { Post, User } from '~/__generated__/schema.generated'
 import { Card, CardBody } from '../ui/Card'
 import UserProfileLarge from '../ui/UserProfileLarge'
 import { formatDistanceToNowStrict } from 'date-fns'
+import Linkify from 'linkifyjs/react'
 
 interface Props {
   post: Post
@@ -12,23 +13,19 @@ interface Props {
 export const SinglePost: React.FC<Props> = ({ post }) => {
   return (
     <Card>
-      <CardBody>
-        <div>
-          <div className="flex justify-between items-center">
-            <UserProfileLarge user={post?.user as User} />
-            <Link href={`/posts/${post?.id}`} passHref>
-              <div className="text-sm cursor-pointer">
-                {formatDistanceToNowStrict(new Date(post?.createdAt), {
-                  addSuffix: true
-                })}
-              </div>
-            </Link>
-          </div>
-          <Link key={post.id} href={`/posts/${post.id}`}>
-            <a>
-              <div>{post.text}</div>
-            </a>
+      <CardBody className="space-y-4">
+        <div className="flex justify-between items-center">
+          <UserProfileLarge user={post?.user as User} />
+          <Link href={`/posts/${post?.id}`} passHref>
+            <div className="text-sm cursor-pointer">
+              {formatDistanceToNowStrict(new Date(post?.createdAt), {
+                addSuffix: true
+              })}
+            </div>
           </Link>
+        </div>
+        <div className="text-lg post">
+          <Linkify>{post.text}</Linkify>
         </div>
       </CardBody>
     </Card>
