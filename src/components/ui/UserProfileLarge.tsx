2a162689fd56238d3655231fbd5619ea05a660d3
Yogi <yoginth@hey.com>
2021-08-03 14:37:11 +0530

    fix: Lint errors

--- src/components/ui/UserProfileLarge.tsx
+++ src/components/ui/UserProfileLarge.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import React from 'react'
 import { User } from '~/__generated__/schema.generated'
 import Username from './Username'
 
--- src/components/ui/Username.tsx
+++ src/components/ui/Username.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 interface Props {
   username: string | undefined
   showAt?: boolean
