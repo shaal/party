c64159ebe2b3547298f0a845670db887baa13f9f
Yogi <yoginth@hey.com>
2021-08-03 19:03:07 +0530

    add: Shimmer to recent users

--- src/components/Home/RecentUsers.tsx
+++ src/components/Home/RecentUsers.tsx
@@ -1,11 +1,11 @@
 import { gql, useQuery } from '@apollo/client'
-import { UsersIcon } from '@heroicons/react/outline'
-import React, { Fragment } from 'react'
+import React from 'react'
 import { User } from '@__generated__/schema.generated'
 import { Card, CardBody } from '../ui/Card'
 import { ErrorMessage } from '../ui/ErrorMessage'
 import UserProfileLarge from '../ui/UserProfileLarge'
 import { RecentUsersQuery } from './__generated__/RecentUsers.generated'
+import UserProfileLargeShimmer from '@components/ui/Shimmer/UserProfileLargeShimmer'
 
 export const query = gql`
   query RecentUsersQuery {
@@ -19,25 +19,41 @@ export const query = gql`
   }
 `
 
+const RecentUsersCard = ({ children }: any) => {
+  return (
+    <div className="mb-4">
+      <div className="mb-2">Recent users</div>
+      <Card>
+        <CardBody>{children}</CardBody>
+      </Card>
+    </div>
+  )
+}
+
 export const RecentUsers: React.FC = () => {
   const { data, loading, error } = useQuery<RecentUsersQuery>(query)
 
+  if (loading)
+    return (
+      <RecentUsersCard>
+        <div className="space-y-3">
+          <UserProfileLargeShimmer showFollow />
+          <UserProfileLargeShimmer showFollow />
+          <UserProfileLargeShimmer showFollow />
+          <UserProfileLargeShimmer showFollow />
+          <UserProfileLargeShimmer showFollow />
+        </div>
+      </RecentUsersCard>
+    )
+
   return (
-    <Fragment>
-      <div className="font-bold mb-1 flex items-center gap-1.5">
-        <UsersIcon className="h-4 w-4" />
-        <div>Recent users</div>
+    <RecentUsersCard>
+      <ErrorMessage title="Failed to load posts" error={error} />
+      <div className="space-y-3">
+        {data?.users.map((user: any) => (
+          <UserProfileLarge key={user?.id} user={user as User} />
+        ))}
       </div>
-      <Card>
-        <CardBody>
-          <ErrorMessage title="Failed to load posts" error={error} />
-          <div className="space-y-3">
-            {data?.users.map((user: any) => (
-              <UserProfileLarge key={user?.id} user={user as User} />
-            ))}
-          </div>
-        </CardBody>
-      </Card>
-    </Fragment>
+    </RecentUsersCard>
   )
 }
