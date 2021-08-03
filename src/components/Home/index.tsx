2cca0f7b4e65d70fb78422cad4822e34f4d61eea
Yogi <yoginth@hey.com>
2021-08-03 19:10:41 +0530

    add: Banner

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Posts from '@pages/posts'
 import { ErrorMessage } from '../ui/ErrorMessage'
 import { GridItemEight, GridItemFour, GridLayout } from '../ui/GridLayout'
@@ -6,25 +6,30 @@ import { RecentUsers } from './RecentUsers'
 import { NewPost } from '@components/Posts/NewPost'
 import { useContext } from 'react'
 import AppContext from '@components/utils/AppContext'
+import Banner from '@components/ui/Banner'
 
 export const Home: React.FC = () => {
-  const { currentUser, currentUserError } = useContext(AppContext)
+  const { currentUser, currentUserLoading, currentUserError } =
+    useContext(AppContext)
 
   return (
-    <GridLayout>
-      <GridItemEight>
-        <div className="space-y-6">
-          {currentUser && <NewPost />}
-          <Posts />
-          <ErrorMessage
-            title="Failed to load the current user."
-            error={currentUserError}
-          />
-        </div>
-      </GridItemEight>
-      <GridItemFour>
-        <RecentUsers />
-      </GridItemFour>
-    </GridLayout>
+    <Fragment>
+      {!currentUser && !currentUserLoading && <Banner />}
+      <GridLayout>
+        <GridItemEight>
+          <div className="space-y-6">
+            {currentUser && <NewPost />}
+            <Posts />
+            <ErrorMessage
+              title="Failed to load the current user."
+              error={currentUserError}
+            />
+          </div>
+        </GridItemEight>
+        <GridItemFour>
+          <RecentUsers />
+        </GridItemFour>
+      </GridLayout>
+    </Fragment>
   )
 }
