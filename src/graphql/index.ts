f555f0a83e46ee731f5110f6e250d24944fa2d11
Yogi <yoginth@hey.com>
2021-08-04 10:35:48 +0530

    update: src/graphql/index.ts

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,15 +1,23 @@
-import path from 'path'
 import fs from 'fs'
-import { printSchema, lexicographicSortSchema } from 'graphql'
+import path from 'path'
+import { printSchema, lexicographicSortSchema, GraphQLSchema } from 'graphql'
 import { builder } from './builder'
 import './resolvers'
 
 export const schema = builder.toSchema({})
-const schemaAsString = printSchema(lexicographicSortSchema(schema))
+
+function writeSchema(schema: GraphQLSchema) {
+  const schemaAsString = printSchema(lexicographicSortSchema(schema))
+  const schemaPath = path.join(process.cwd(), 'src/graphql/schema.graphql')
+
+  const existingSchema =
+    fs.existsSync(schemaPath) && fs.readFileSync(schemaPath, 'utf-8')
+
+  if (existingSchema !== schemaAsString) {
+    fs.writeFileSync(schemaPath, schemaAsString)
+  }
+}
 
 if (process.env.NODE_ENV !== 'production') {
-  fs.writeFileSync(
-    path.join(process.cwd(), 'src/graphql/schema.gql'),
-    schemaAsString
-  )
+  writeSchema(schema)
 }
