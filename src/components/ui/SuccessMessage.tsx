61bb7c5fe6f2ce0ae37cf607567579f022795d58
Yogi <yoginth@hey.com>
2021-08-04 20:22:14 +0530

    move: To es6

--- src/components/ui/SuccessMessage.tsx
+++ src/components/ui/SuccessMessage.tsx
@@ -1,10 +1,10 @@
-import { ReactNode } from 'react'
+import React, { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
 }
 
-export function SuccessMessage({ children }: Props) {
+export const SuccessMessage: React.FC<Props> = ({ children }) => {
   return (
     <div className="rounded-md bg-green-50 dark:bg-green-900 dark:bg-opacity-10 border-2 border-green-500 border-opacity-50 p-4">
       <div className="text-sm text-green-700 dark:text-green-200">
