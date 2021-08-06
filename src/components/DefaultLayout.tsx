90be0840c61250c86be5396e9139ece0d801f60f
Yogi <yoginth@hey.com>
2021-08-06 18:25:52 +0530

    fix: eslint sort import errors

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,9 +1,10 @@
 import { gql, useQuery } from '@apollo/client'
 import { useEffect, useState } from 'react'
+
+import { CurrentUserQuery } from './__generated__/DefaultLayout.generated'
 import Footer from './shared/Footer'
 import Navbar from './shared/Navbar'
 import AppContext from './utils/AppContext'
-import { CurrentUserQuery } from './__generated__/DefaultLayout.generated'
 
 const query = gql`
   query CurrentUserQuery {
