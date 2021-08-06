9b375f92f285f49eec1dc5002183a0e85540d4d3
Yogi <yoginth@hey.com>
2021-08-06 13:36:57 +0530

    fix: eslint sort import errors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next'
-import { unauthenticatedRoute } from '~/utils/redirects'
+
 import LoginForm from '~/components/Auth/LoginForm'
+import { unauthenticatedRoute } from '~/utils/redirects'
 
 export const getServerSideProps: GetServerSideProps = unauthenticatedRoute
 
