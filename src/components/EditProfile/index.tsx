69d93b6ea8d94cc45e1717a1772df956ce9b02e2
Yogi <yoginth@hey.com>
2021-08-06 11:07:53 +0530

    delete: Shimmer comp

--- src/components/EditProfile/index.tsx
+++ src/components/EditProfile/index.tsx
@@ -2,7 +2,6 @@ import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 import { ErrorMessage } from '../ui/ErrorMessage'
 import { GridLayout } from '../ui/GridLayout'
-import { Shimmer } from '../ui/Shimmer'
 import { ProfileForm } from './ProfileForm'
 import { EditProfileQuery } from './__generated__/index.generated'
 
@@ -18,8 +17,6 @@ export const EditProfile: React.FC = () => {
 
   return (
     <GridLayout>
-      {loading && <Shimmer />}
-
       {error && (
         <ErrorMessage title="Failed to load your profile" error={error} />
       )}
