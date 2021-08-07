7048ce6985c2a1b8b9e30dc115d76d32aae0eb57
Yogi <yoginth@hey.com>
2021-08-07 19:50:59 +0530

    refactor: Resolvers

--- src/graphql/resolvers/ProfileResolver.ts
+++ src/graphql/resolvers/ProfileResolver.ts
@@ -6,6 +6,12 @@ builder.prismaObject('Profile', {
     id: t.exposeID('id', {}),
     name: t.exposeString('name', {}),
     avatar: t.exposeString('avatar', { nullable: true }),
-    bio: t.exposeString('bio', { nullable: true })
+    bio: t.exposeString('bio', { nullable: true }),
+
+    // Social
+    website: t.exposeString('website', { nullable: true }),
+    twitter: t.exposeString('twitter', { nullable: true }),
+    github: t.exposeString('github', { nullable: true }),
+    discord: t.exposeString('discord', { nullable: true })
   })
 })
