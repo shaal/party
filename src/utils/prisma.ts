b0c3eac4fa2f2a7918541ea0425acbfac79a79a0
Yogi <yoginth@hey.com>
2021-08-08 21:46:09 +0530

    refactor: All

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-// Ensure that there's only a single Prisma instance in dev. This is detailed here:
-// https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
 declare global {
   var __globalPrisma__: PrismaClient
 }
