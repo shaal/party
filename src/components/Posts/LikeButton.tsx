2daef30c31bb0b1395a2b2b0c6c767fd0d18d2fb
Yogi <yoginth@hey.com>
2021-08-08 12:29:30 +0530

    finish: Like button

--- src/components/Posts/LikeButton.tsx
+++ src/components/Posts/LikeButton.tsx
@@ -1,7 +1,7 @@
 import { Switch } from '@headlessui/react'
 import { HeartIcon } from '@heroicons/react/outline'
 import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Post } from '~/__generated__/schema.generated'
 
@@ -12,16 +12,23 @@ type Props = {
 }
 
 const LikeButton: React.FC<Props> = ({ entity, handleLike, loading }) => {
+  const [isLiked, setIsLiked] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (entity?.hasLiked) setIsLiked(entity?.hasLiked)
+  }, [])
+
   return (
     <Switch
       checked={entity?.hasLiked}
       onChange={() => {
+        setIsLiked(!isLiked)
         handleLike(entity)
       }}
       className="text-pink-500 hover:text-pink-400 flex items-center space-x-2"
       disabled={loading}
     >
-      {entity?.hasLiked ? (
+      {isLiked ? (
         <HeartIconSolid className="h-5 w-5" />
       ) : (
         <HeartIcon className="h-5 w-5" />
