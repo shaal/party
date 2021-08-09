68e8eeead8a6e61fd85649add760cbcbba403f0e
Yogi <yoginth@hey.com>
2021-08-09 12:18:16 +0530

    add: dynamic load settings

--- src/components/Profile/Settings/index.tsx
+++ src/components/Profile/Settings/index.tsx
@@ -1,12 +1,37 @@
 import { gql, useQuery } from '@apollo/client'
+import dynamic from 'next/dynamic'
 import React, { Fragment, useContext } from 'react'
 
 import { User } from '~/__generated__/schema.generated'
+import {
+  GridItemEight,
+  GridItemFour,
+  GridLayout
+} from '~/components/GridLayout'
 import AppContext from '~/components/utils/AppContext'
 
 import { SettingsQuery } from './__generated__/index.generated'
-import AccountSettings from './AccountSettings'
-import SocialSettings from './SocialSettings'
+
+const AccountSettings = dynamic(() => import('./AccountSettings'), {
+  // eslint-disable-next-line react/display-name
+  loading: () => <Loading />
+})
+
+const SocialSettings = dynamic(() => import('./SocialSettings'), {
+  // eslint-disable-next-line react/display-name
+  loading: () => <Loading />
+})
+
+const Loading = () => (
+  <GridLayout>
+    <GridItemFour>
+      <div className="shimmer rounded-lg h-10"></div>
+    </GridItemFour>
+    <GridItemEight>
+      <div className="shimmer rounded-lg h-10"></div>
+    </GridItemEight>
+  </GridLayout>
+)
 
 export const query = gql`
   query SettingsQuery {
