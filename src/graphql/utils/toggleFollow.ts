c37d0493825d798811b2495f938d94f8a19c13b0
Yogi <yoginth@hey.com>
2021-08-09 13:08:22 +0530

    add: hasFollowed field

--- src/graphql/utils/toggleFollow.ts
+++ src/graphql/utils/toggleFollow.ts
@@ -4,7 +4,7 @@ import { hasFollowed } from './hasFollowed'
 
 export const toggleFollow = async (currentUserId: string, userId: string) => {
   // Unfollow
-  if (await hasFollowed(userId, currentUserId)) {
+  if (await hasFollowed(currentUserId, userId)) {
     return await db.user.update({
       where: { id: userId },
       data: {
