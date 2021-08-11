ee57317e92558c8b4251b85ea7c38a6f6094e4f2
Yogi <yoginth@hey.com>
2021-08-11 11:55:03 +0530

    add: SSR for posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,8 @@
-import Home from '~/components/Home'
+import Home, { HOME_QUERY as query } from '~/components/Home'
+import { preloadQuery } from '~/utils/apollo'
+
+export const getServerSideProps = async (ctx: any) => {
+  return preloadQuery(ctx, { query })
+}
 
 export default Home
