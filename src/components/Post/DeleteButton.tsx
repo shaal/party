370adf37d7b3797dc2f4f9a2232f87f232b74c22
Yogi <yoginth@hey.com>
2021-08-11 19:24:17 +0530

    add: Delete post query

--- src/components/Post/DeleteButton.tsx
+++ src/components/Post/DeleteButton.tsx
@@ -1,17 +1,36 @@
+import { gql, useMutation } from '@apollo/client'
 import { TrashIcon } from '@heroicons/react/outline'
 import React from 'react'
 
 import { Post } from '~/__generated__/schema.generated'
+
+import {
+  DeletePostMutation,
+  DeletePostMutationVariables
+} from './__generated__/DeleteButton.generated'
 
 type Props = {
   entity: Post
 }
 
 const DeleteButton: React.FC<Props> = ({ entity }) => {
+  const [deleteNote, deleteNoteResult] = useMutation<
+    DeletePostMutation,
+    DeletePostMutationVariables
+  >(
+    gql`
+      mutation DeletePostMutation($input: DeletePostInput!) {
+        deletePost(input: $input) {
+          id
+        }
+      }
+    `
+  )
+
   return (
     <button
       className="text-red-500 hover:text-red-400 flex items-center space-x-2"
-      onClick={() => console.log('WIP')}
+      onClick={() => deleteNote({ variables: { input: { id: entity?.id } } })}
     >
       <TrashIcon className="h-5 w-5" />
     </button>
