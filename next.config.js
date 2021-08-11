a2f1701b23fbf988f1e07bd28170866f85eea694
Yogi <yoginth@hey.com>
2021-08-11 17:03:32 +0530

    remove: Preact

--- next.config.js
+++ next.config.js
@@ -2,15 +2,5 @@ module.exports = {
   images: {
     domains: ['cloudflare-ipfs.com', 'avatar.tobi.sh']
   },
-  reactStrictMode: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
-  }
+  reactStrictMode: true
 }
