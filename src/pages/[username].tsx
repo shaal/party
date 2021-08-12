336f214bc60f6270d4877d7dfc22f554ff68a1ed
Yogi <yoginth@hey.com>
2021-08-12 09:04:48 +0530

    remove: TS path alias

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -1,5 +1,5 @@
-import Profile, { PROFILE_QUERY as query } from '~/components/Profile'
-import { preloadQuery } from '~/utils/apollo'
+import Profile, { PROFILE_QUERY as query } from 'src/components/Profile'
+import { preloadQuery } from 'src/utils/apollo'
 
 export const getServerSideProps = async (ctx: any) => {
   return preloadQuery(ctx, {
