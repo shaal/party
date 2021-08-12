72cdff37f4a7fa7385f2af179180faedce9c66ff
Yogi <yoginth@hey.com>
2021-08-12 09:03:37 +0530

    remove: TS path alias

--- src/pages/settings/change-password.tsx
+++ src/pages/settings/change-password.tsx
@@ -1,7 +1,6 @@
 import { GetServerSideProps } from 'next'
-
-import ChangePassword from '~/components/Profile/Settings/ChangePassword'
-import { authenticatedRoute } from '~/utils/redirects'
+import ChangePassword from 'src/components/Profile/Settings/ChangePassword'
+import { authenticatedRoute } from 'src/utils/redirects'
 
 export const getServerSideProps: GetServerSideProps = authenticatedRoute
 
