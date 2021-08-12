1769095f46d5e673ea18ba08f7a1f17eecfdc27d
Yogi <yoginth@hey.com>
2021-08-12 18:09:23 +0530

    update: NewProduct component

--- src/components/Products/New.tsx
+++ src/components/Products/New.tsx
@@ -1,10 +1,43 @@
+import { gql, useMutation } from '@apollo/client'
 import React from 'react'
+import { object, string } from 'zod'
 
 import { GridItemEight, GridItemFour, GridLayout } from '../GridLayout'
 import SettingsHelper from '../shared/SettingsHelper'
+import { Button } from '../ui/Button'
 import { Card, CardBody } from '../ui/Card'
+import { ErrorMessage } from '../ui/ErrorMessage'
+import { Form, useZodForm } from '../ui/Form'
+import { Input } from '../ui/Input'
+import { TextArea } from '../ui/TextArea'
+import {
+  CreateProductMutation,
+  CreateProductMutationVariables
+} from './__generated__/New.generated'
+
+const newProductSchema = object({
+  name: string().min(1),
+  slug: string().min(1),
+  bio: string().min(1)
+})
 
 const NewProduct: React.FC = () => {
+  const [createProduct, createProductResult] = useMutation<
+    CreateProductMutation,
+    CreateProductMutationVariables
+  >(gql`
+    mutation CreateProductMutation($input: CreateProductInput!) {
+      createProduct(input: $input) {
+        id
+        slug
+      }
+    }
+  `)
+
+  const form = useZodForm({
+    schema: newProductSchema
+  })
+
   return (
     <GridLayout>
       <GridItemFour>
@@ -15,7 +48,47 @@ const NewProduct: React.FC = () => {
       </GridItemFour>
       <GridItemEight>
         <Card className="space-y-5">
-          <CardBody className="space-y-4">WIP</CardBody>
+          <CardBody className="space-y-4">
+            <Form
+              form={form}
+              className="space-y-4"
+              onSubmit={({ name, slug }) =>
+                createProduct({
+                  variables: {
+                    input: {
+                      name,
+                      slug
+                    }
+                  }
+                })
+              }
+            >
+              <ErrorMessage
+                title="Error creating product"
+                error={createProductResult.error}
+              />
+              <Input
+                label="Username"
+                type="text"
+                placeholder="Minecraft"
+                {...form.register('name')}
+              />
+              <Input
+                label="Email"
+                type="email"
+                placeholder="minecraft"
+                {...form.register('slug')}
+              />
+              <TextArea
+                label="Bio"
+                placeholder="Tell us what you made!"
+                {...form.register('bio')}
+              />
+              <div className="flex items-center justify-between pt-3">
+                <Button type="submit">Create</Button>
+              </div>
+            </Form>
+          </CardBody>
         </Card>
       </GridItemEight>
     </GridLayout>
