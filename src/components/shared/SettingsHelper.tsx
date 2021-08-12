cac1170e319af55e309d2a992d2d34a208162433
Yogi <yoginth@hey.com>
2021-08-12 17:28:38 +0530

    update: SettingsHelper

--- src/components/shared/SettingsHelper.tsx
+++ src/components/shared/SettingsHelper.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const SettingsHelper: React.FC<Props> = ({ icon, heading, description }) => {
   return (
-    <div className="space-y-2 w-5/6">
+    <div className="space-y-2 w-5/6 mb-4 ml-4 md:ml-0 lg-ml-0">
       <div className="text-xl flex items-center gap-1.5">
         {icon}
         <div>{heading}</div>
