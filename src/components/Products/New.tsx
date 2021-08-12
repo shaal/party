1cee42cd76fbfb50193c3f9b5b896018af739829
Yogi <yoginth@hey.com>
2021-08-12 17:51:46 +0530

    add: NewProduct

--- src/components/Products/New.tsx
+++ src/components/Products/New.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
 
 import { GridItemEight, GridItemFour, GridLayout } from '../GridLayout'
-import { Button } from '../ui/Button'
+import SettingsHelper from '../shared/SettingsHelper'
 import { Card, CardBody } from '../ui/Card'
 
 const NewProduct: React.FC = () => {
   return (
     <GridLayout>
+      <GridItemFour>
+        <SettingsHelper
+          heading="New Product"
+          description={'Launch your dream product in Devparty'}
+        />
+      </GridItemFour>
       <GridItemEight>
         <Card className="space-y-5">
-          <CardBody className="space-y-4">Hello</CardBody>
+          <CardBody className="space-y-4">WIP</CardBody>
         </Card>
       </GridItemEight>
-      <GridItemFour>
-        <Card>
-          <CardBody>
-            <Button>Create new Product</Button>
-          </CardBody>
-        </Card>
-      </GridItemFour>
     </GridLayout>
   )
 }
