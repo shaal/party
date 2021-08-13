6d2e879b972aac665fe7603620568ffcf83aa9d6
Yogi <yoginth@hey.com>
2021-08-13 05:12:31 +0000

    add: Posts seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,12 +2,13 @@ import { PrismaClient } from '@prisma/client'
 import { md5 } from 'hash-wasm'
 
 import { hashPassword } from '../src/utils/auth'
+import { postData } from './seeds/posts'
 import { userData } from './seeds/user'
 
 const db = new PrismaClient()
 
 async function main() {
-  console.log('Start seeding...')
+  // User
   for (const user of userData) {
     console.log(`Seeding User - @${user.username} ✅`)
     await db.user.create({
@@ -20,6 +21,21 @@ async function main() {
           create: {
             name: user.name,
             avatar: `https://avatar.tobi.sh/${await md5(user.email)}.svg`
+          }
+        }
+      }
+    })
+  }
+
+  // Post
+  for (const post of postData) {
+    console.log(`Seeding Post - ${post.body} ✅`)
+    await db.post.create({
+      data: {
+        body: post.body,
+        user: {
+          connect: {
+            username: post.username
           }
         }
       }
---
+++ prisma/seeds/posts.ts
@@ -0,0 +1,14 @@
+export const postData = [
+  {
+    body: 'Hello, World!',
+    username: 'yoginth'
+  },
+  {
+    body: 'Hello, World!',
+    username: 'yoginth'
+  },
+  {
+    body: 'Hello, World!',
+    username: 'yoginth'
+  }
+]
