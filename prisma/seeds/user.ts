f1383dc2c8c986ece90573a3c40a567f7495ec1b
Yogi <yoginth@hey.com>
2021-08-13 04:50:49 +0000

    add: User Seed Data

---
+++ prisma/seeds/user.ts
@@ -0,0 +1,17 @@
+export const userData = [
+  {
+    email: 'yoginth@hey.com',
+    username: 'yoginth',
+    isStaff: true
+  },
+  {
+    email: 'filip@hey.com',
+    username: 'filip',
+    isStaff: true
+  },
+  {
+    email: 'vladja@hey.com',
+    username: 'vladja',
+    isStaff: false
+  }
+]
