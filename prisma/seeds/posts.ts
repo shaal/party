a61006677927c75a7a30abd689581fe2875e18d6
Yogi <yoginth@hey.com>
2021-08-13 05:14:19 +0000

    update: Seed data

--- prisma/seeds/posts.ts
+++ prisma/seeds/posts.ts
@@ -1,14 +1,14 @@
 export const postData = [
   {
-    body: 'Hello, World!',
+    body: 'Setting people up for nightmares.',
     username: 'yoginth'
   },
   {
-    body: 'Hello, World!',
-    username: 'yoginth'
+    body: "I'm a senior engineer now! 🎉",
+    username: 'filip'
   },
   {
-    body: 'Hello, World!',
-    username: 'yoginth'
+    body: "After almost 9 years, I'm saying goodbye to working at @Google . As my first job out of school, I feel so lucky to have had a chance to work with so many incredible people across Local Search, Apps, Chrome, and Payments!",
+    username: 'vladja'
   }
 ]
