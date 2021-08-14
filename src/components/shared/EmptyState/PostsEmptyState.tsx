f191e37e581c0a23f4e378ccf320806d24570ba7
Yogi <yoginth@hey.com>
2021-08-14 07:58:34 +0000

    add: PostsEmptyState

---
+++ src/components/shared/EmptyState/PostsEmptyState.tsx
@@ -0,0 +1,13 @@
+import { Card, CardBody } from "../../ui/Card"
+
+const PostsEmptyState: React.FC = () => {
+  return (
+    <Card>
+      <CardBody>
+        WIP
+        </CardBody>
+    </Card>
+  )
+}
+
+export default PostsEmptyState
