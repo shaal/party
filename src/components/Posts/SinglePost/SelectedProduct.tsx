05b3ea167d6248db5aff39140f77b789099dc7c8
Yogi <yoginth@hey.com>
2021-08-14 03:58:05 +0000

    add: bradlc.vscode-tailwindcss

--- src/components/Posts/SinglePost/SelectedProduct.tsx
+++ src/components/Posts/SinglePost/SelectedProduct.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 import { Product } from '../../../__generated__/schema.generated'
@@ -10,16 +11,18 @@ const SelectedProduct: React.FC<Props> = ({ product }) => {
   return (
     <div className="text-sm flex items-center space-x-2 ml-auto">
       <div>Posted in</div>
-      <div className="flex items-center space-x-1">
-        <img
-          className="h-4 w-4 rounded"
-          src={product?.avatar as string}
-          alt={`#${product?.slug}'s avatar'`}
-        />
-        <div className="font-bold text-gray-600 dark:text-gray-200">
-          {product?.name}
+      <Link href={`/products/${product?.slug}`} passHref>
+        <div className="flex items-center space-x-1 cursor-pointer">
+          <img
+            className="h-4 w-4 rounded"
+            src={product?.avatar as string}
+            alt={`#${product?.slug}'s avatar'`}
+          />
+          <div className="font-bold text-gray-600 dark:text-gray-200">
+            {product?.name}
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
