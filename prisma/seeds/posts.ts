38f675481b23b1e53ef5cb450cf7bbe2b6839cd6
Yogi <yoginth@hey.com>
2021-08-16 14:14:42 +0000

    add: More data to seeder

--- prisma/seeds/posts.ts
+++ prisma/seeds/posts.ts
@@ -22,5 +22,37 @@ export const postData = [
   {
     body: 'We’re dealing with a pretty significant spike in deploys. Things should normalize soon, the team is on it.',
     username: 'svobodavl'
+  },
+  {
+    body: "My keyboard feels weird. That's how much I was unplugged from work this PTO.",
+    username: 'aellopos'
+  },
+  {
+    body: 'You don’t need better ideas to get started, you need to get started to have better ideas.',
+    username: 'aellopos'
+  },
+  {
+    body: 'news drought is real. if someone, idk, has like images of the next iPhone or something, feel free to DM me. 🥱',
+    username: 'kahy'
+  },
+  {
+    body: 'Designer friends, what are some of the best design communities/accounts to follow?',
+    username: 'kahy'
+  },
+  {
+    body: 'Alpha preview: Modern JavaScript in Rails 7 without #webpack by @dhh https://www.youtube.com/watch?v=PtxZvFnL2i0',
+    username: 'filip'
+  },
+  {
+    body: 'My biggest challenge right now is that I have 1 million ideas and only seven days',
+    username: 'kahy'
+  },
+  {
+    body: 'I still wish Rust would let you omit Default::default when using the spread operator.',
+    username: 'yoginth'
+  },
+  {
+    body: "Cryptocurrency is like the end of 2001: A Space Odyssey; lots of people talk about it, but no-one really knows how to explain what they've seen.",
+    username: 'yoginth'
   }
 ]
