75cee52e915f36b9ffcbbff89ac91c1db2c1f3de
Yogi <yoginth@hey.com>
2021-08-16 12:23:57 +0000

    update: uploadToIPFS

--- src/components/utils/uploadToIPFS.ts
+++ src/components/utils/uploadToIPFS.ts
@@ -1,12 +1,12 @@
-require('dotenv').config()
-import { Web3Storage } from 'web3.storage'
+import { create } from 'ipfs-http-client'
 
 export async function uploadToIPFS(data: any) {
-  console.log(data)
-  var blob = data.slice(0, data.size, data.type)
-  var file = new File([blob], 'img', { type: data.type })
-  const client = new Web3Storage({ token: process.env.WEB3_STORAGE_TOKEN })
-  const rootCid = await client.put([file])
+  const ipfs = create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https'
+  })
 
-  return `https://cloudflare-ipfs.com/ipfs/${rootCid}/img`
+  const { path }: { path: string } = await ipfs.add(data)
+  return `https://cloudflare-ipfs.com/ipfs/${path}`
 }
