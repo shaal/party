610bfe1b125403794241532d293b28171f94e36c
Yogi <yoginth@hey.com>
2021-08-16 04:58:23 +0000

    add: linkifyOptions

--- src/components/Posts/SinglePost/Type/Post.tsx
+++ src/components/Posts/SinglePost/Type/Post.tsx
@@ -1,3 +1,5 @@
+import * as linkify from 'linkifyjs'
+import hashtag from 'linkifyjs/plugins/hashtag'
 import Linkify from 'linkifyjs/react'
 import React from 'react'
 
@@ -8,11 +10,22 @@ interface Props {
   post: Post
 }
 
+hashtag(linkify)
+
 const PostType: React.FC<Props> = ({ post }) => {
+  var linkifyOptions = {
+    formatHref: function (href: string, type: any) {
+      if (type === 'hashtag') {
+        href = 'https://twitter.com/hashtag/' + href.substring(1)
+      }
+      return href
+    }
+  }
+
   return (
     <div className="space-y-3">
       <div className="text-lg linkify space-y-3 inline-flex">
-        <Linkify>{post?.body}</Linkify>
+        <Linkify options={linkifyOptions}>{post?.body}</Linkify>
         {post?.attachments && <Attachments attachments={post?.attachments} />}
       </div>
     </div>
