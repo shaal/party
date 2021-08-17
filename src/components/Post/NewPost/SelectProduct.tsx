7fb5716d0e9f713d634343b6dca66347d9e45322
Yogi <yoginth@hey.com>
2021-08-17 12:27:36 +0000

    update: Product dropdown

--- src/components/Post/NewPost/SelectProduct.tsx
+++ src/components/Post/NewPost/SelectProduct.tsx
@@ -40,7 +40,6 @@ const SelectProduct: React.FC<Props> = ({ setSelectedProduct }) => {
   })
 
   const handleSelectProduct = (product: Product) => {
-    console.log(product)
     setProduct(product)
     setSelectedProduct(product?.id)
   }
@@ -48,58 +47,56 @@ const SelectProduct: React.FC<Props> = ({ setSelectedProduct }) => {
   return (
     <Listbox value={product} onChange={handleSelectProduct}>
       {({ open }) => (
-        <Fragment>
-          <div className="relative">
-            <Listbox.Button className="flex items-center justify-between bg-white dark:bg-gray-900 relative w-full border border-gray-300 dark:border-gray-800 rounded-lg px-3 py-0.5 cursor-default focus:outline-none text-sm">
-              <span className="w-16 block truncate dark:text-gray-400 flex-1 text-left font-medium">
-                {product ? product?.name : 'Product'}
-              </span>
-              <ChevronDownIcon className="h-4 w-4" />
-            </Listbox.Button>
+        <div className="">
+          <Listbox.Button className="flex items-center justify-between bg-white dark:bg-gray-900 relative w-full border border-gray-300 dark:border-gray-800 rounded-lg px-3 py-0.5 cursor-default focus:outline-none text-sm">
+            <span className="w-16 block truncate dark:text-gray-400 flex-1 text-left font-medium">
+              {product ? product?.name : 'Product'}
+            </span>
+            <ChevronDownIcon className="h-4 w-4" />
+          </Listbox.Button>
 
-            <Transition show={open} as={Fragment}>
-              <Listbox.Options
-                static
-                className="absolute mt-1 border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900 shadow-md rounded-lg py-1 px-2 text-base focus:outline-none sm:text-sm"
-              >
-                {data?.products?.edges?.map((product: any) => (
-                  <Listbox.Option
-                    key={product?.node?.id}
-                    className={({ active }) =>
-                      clsx(
-                        { 'bg-gray-100 dark:bg-gray-800': active },
-                        'block px-3 py-1.5 my-1 text-sm text-gray-700 dark:text-gray-200 rounded-lg cursor-pointer'
-                      )
-                    }
-                    value={product?.node}
-                  >
-                    <div className="flex items-center gap-2">
-                      <img
-                        className="h-5 w-5 rounded-md"
-                        src={product?.node?.avatar}
-                        alt={`#${product?.node?.slug}'s avatar`}
-                      />
-                      <div className="block truncate font-medium">
-                        {product?.node?.name}
-                      </div>
-                    </div>
-                  </Listbox.Option>
-                ))}
+          <Transition show={open} as={Fragment}>
+            <Listbox.Options
+              static
+              className="absolute mt-1 border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900 shadow-md rounded-lg py-1 px-2 text-base focus:outline-none sm:text-sm"
+            >
+              {data?.products?.edges?.map((product: any) => (
                 <Listbox.Option
+                  key={product?.node?.id}
                   className={({ active }) =>
                     clsx(
                       { 'bg-gray-100 dark:bg-gray-800': active },
-                      'block px-3 py-1.5 my-1 text-sm text-red-500 rounded-lg cursor-pointer'
+                      'block px-3 py-1.5 my-1 text-sm text-gray-700 dark:text-gray-200 rounded-lg cursor-pointer'
                     )
                   }
-                  value={null}
+                  value={product?.node}
                 >
-                  <span className="block truncate font-medium">Reset</span>
+                  <div className="flex items-center gap-2">
+                    <img
+                      className="h-5 w-5 rounded-md"
+                      src={product?.node?.avatar}
+                      alt={`#${product?.node?.slug}'s avatar`}
+                    />
+                    <div className="block truncate font-medium">
+                      {product?.node?.name}
+                    </div>
+                  </div>
                 </Listbox.Option>
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </Fragment>
+              ))}
+              <Listbox.Option
+                className={({ active }) =>
+                  clsx(
+                    { 'bg-gray-100 dark:bg-gray-800': active },
+                    'block px-3 py-1.5 my-1 text-sm text-red-500 rounded-lg cursor-pointer'
+                  )
+                }
+                value={null}
+              >
+                <span className="block truncate font-medium">Reset</span>
+              </Listbox.Option>
+            </Listbox.Options>
+          </Transition>
+        </div>
       )}
     </Listbox>
   )
