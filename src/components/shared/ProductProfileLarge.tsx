bd64e8cb9ecc9a75501e53c65dbb7c97ea1be593
Yogi <yoginth@hey.com>
2021-08-18 12:27:29 +0000

    add: ProductProfileLarge

---
+++ src/components/shared/ProductProfileLarge.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import React from 'react'
+
+import { Product } from '../../__generated__/schema.generated'
+import Slug from './Slug'
+
+interface Props {
+  product: Product
+}
+
+const ProductProfileLarge: React.FC<Props> = ({ product }) => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex space-x-3 items-center">
+        <img
+          src={product?.avatar as string}
+          className="h-11 w-11 rounded-lg bg-gray-200"
+          alt={`#${product?.slug}'s avatar`}
+        />
+        <div>
+          <div className="flex items-center gap-1.5">
+            <Link href={`/products/${product?.slug}`} passHref>
+              <div className="font-bold cursor-pointer">{product?.name}</div>
+            </Link>
+          </div>
+          <Slug slug={product?.slug} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductProfileLarge
