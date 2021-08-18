a43dd8f6474d181db641c4137140dfbfdc660e86
Yogi <yoginth@hey.com>
2021-08-18 17:00:06 +0530

    update: session type :D

--- src/graphql/resolvers/Stafftools/modUser.ts
+++ src/graphql/resolvers/Stafftools/modUser.ts
@@ -1,7 +1,11 @@
 import { ModUserInput } from '../../../__generated__/schema.generated'
 import { db } from '../../../utils/prisma'
+import { Session } from '.prisma/client'
 
-export const modUser = async (input: ModUserInput, session: any) => {
+export const modUser = async (
+  input: ModUserInput,
+  session: Session | null | undefined
+) => {
   const actionUser = await db.user.findUnique({
     where: { id: session!.userId }
   })
