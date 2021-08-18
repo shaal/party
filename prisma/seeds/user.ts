99a80101c61cce96bf444a8fe5cc73ce3d1a85c3
Yogi <yoginth@hey.com>
2021-08-18 12:39:57 +0000

    add: description to seeder

--- prisma/seeds/user.ts
+++ prisma/seeds/user.ts
@@ -4,34 +4,38 @@ export const userData = [
     username: 'yoginth',
     name: 'Yoginth',
     isStaff: true,
-    avatar: 'https://i.ibb.co/Kw0Jbzf/avatar.png'
+    avatar: 'https://i.ibb.co/Kw0Jbzf/avatar.png',
+    bio: ''
   },
   {
     email: 'filip@hey.com',
     username: 'filip',
     name: 'Filip Troníček',
     isStaff: true,
-    avatar: 'https://i.ibb.co/1nyH58W/avatar.jpg'
+    avatar: 'https://i.ibb.co/1nyH58W/avatar.jpg',
+    bio: ''
   },
   {
     email: 'svobodavl@hey.com',
     username: 'svobodavl',
     name: 'Vláďa Svoboda',
     isStaff: false,
-    avatar: 'https://i.ibb.co/TkDmJ5V/avatar.jpg'
+    avatar: 'https://avatars.githubusercontent.com/u/58887042',
+    bio: '“Dark mode everything”'
   },
   {
     email: 'aellopos@hey.com',
     username: 'aellopos',
     name: 'Michal Kučera',
     isStaff: false,
-    avatar: 'https://i.ibb.co/fvcsL0Z/avatar.jpg'
+    avatar: 'https://avatars.githubusercontent.com/u/39790985',
+    bio: 'Czechitas Senior lecturer | Microsoft STC Learning & Development Lead | Microsoft MLSA Country Lead of Czech Republic'
   },
   {
     email: 'kahy@hey.com',
     username: 'kahy',
     name: 'Josef Kahoun',
     isStaff: false,
-    avatar: 'https://i.ibb.co/gMwNd7p/avatar.png'
+    avatar: 'https://avatars.githubusercontent.com/u/48121432'
   }
 ]
