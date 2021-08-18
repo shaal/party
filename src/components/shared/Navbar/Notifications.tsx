7313f3b2640e79a3b1be773d83fd29b2d15b8cfd
Yogi <yoginth@hey.com>
2021-08-18 04:11:10 +0000

    update: Notifications comp

--- src/components/shared/Navbar/Notifications.tsx
+++ src/components/shared/Navbar/Notifications.tsx
@@ -24,29 +24,8 @@ const Notifications: React.FC = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Popover.Panel className="text-black dark:text-gray-200 absolute right-0 w-full min-w-max">
+        <Popover.Panel className="text-black dark:text-gray-200 absolute right-0 w-full min-w-max py-1 mt-2">
           <div className="bg-white dark:bg-gray-900 py-2 px-2 shadow-md rounded-lg transition border border-gray-200 dark:border-gray-800 max-h-[80vh] space-y-5 overflow-y-scroll">
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
-            <div>WIP</div>
             <div>WIP</div>
             <div>WIP</div>
             <div>WIP</div>
