ac743e86ebf845099b3cafb270c3752f665dfc7e
Yogi <yoginth@hey.com>
2021-08-19 13:59:53 +0530

    rename: db to import as a part of unicorn/prevent-abbreviations

--- src/graphql/resolvers/Common/hasLiked.ts
+++ src/graphql/resolvers/Common/hasLiked.ts
@@ -1,11 +1,11 @@
-import { db } from '../../../utils/prisma'
+import { prisma } from '../../../utils/prisma'
 
 export const hasLiked = async (
   userId: string,
   postId: string | null,
   replyId: string | null
 ) => {
-  const count: number = await db.like.count({
+  const count: number = await prisma.like.count({
     where: { userId, postId, replyId }
   })
 
