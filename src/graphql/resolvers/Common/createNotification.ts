dd92daac56717e47ee3503f23cbaca46152e2fc9
Yogi <yoginth@hey.com>
2021-08-19 13:59:46 +0530

    rename: db to import as a part of unicorn/prevent-abbreviations

--- src/graphql/resolvers/Common/createNotification.ts
+++ src/graphql/resolvers/Common/createNotification.ts
@@ -1,6 +1,6 @@
 import { NotificationType } from '@prisma/client'
 
-import { db } from '../../../utils/prisma'
+import { prisma } from '../../../utils/prisma'
 
 export const createNotification = async (
   receiverId: string,
@@ -9,7 +9,7 @@ export const createNotification = async (
   type: NotificationType
 ) => {
   if (receiverId !== dispatcherId) {
-    return await db.notification.create({
+    return await prisma.notification.create({
       data: {
         receiverId,
         dispatcherId,
