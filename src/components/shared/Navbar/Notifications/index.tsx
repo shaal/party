169e4fbb1ca45c39cf13c6dd4b82b3a8ecda9fa3
Yogi <yoginth@hey.com>
2021-08-19 10:39:23 +0530

    design: Notification dropdown

--- src/components/shared/Navbar/Notifications/index.tsx
+++ src/components/shared/Navbar/Notifications/index.tsx
@@ -2,8 +2,10 @@ import { gql, useQuery } from '@apollo/client'
 import { Popover, Transition } from '@headlessui/react'
 import { LightningBoltIcon } from '@heroicons/react/outline'
 import { Fragment } from 'react'
+import * as timeago from 'timeago.js'
 
 import { NotificationsQuery } from './__generated__/index.generated'
+import PostLike from './PostLike'
 
 export const NOTIFICATIONS_QUERY = gql`
   query NotificationsQuery {
@@ -16,11 +18,14 @@ export const NOTIFICATIONS_QUERY = gql`
         node {
           id
           type
+          entityId
+          createdAt
           dispatcher {
             id
             username
             profile {
               id
+              name
               avatar
             }
           }
@@ -52,9 +57,26 @@ const Notifications: React.FC = () => {
         leaveTo="transform opacity-0 scale-95"
       >
         <Popover.Panel className="text-black dark:text-gray-200 absolute right-0 w-full min-w-max py-1 mt-2">
-          <div className="bg-white dark:bg-gray-900 py-2 px-2 shadow-md rounded-lg transition border border-gray-200 dark:border-gray-800 max-h-[80vh] space-y-5 overflow-y-scroll">
+          <div className="bg-white dark:bg-gray-900 py-3 px-5 shadow-md rounded-lg transition border border-gray-200 dark:border-gray-800 max-h-[80vh] space-y-6 overflow-y-scroll">
             {notifications?.map((notification: any) => (
-              <div key={notification?.id}>{notification?.type}</div>
+              <div
+                key={notification?.id}
+                className="flex items-center space-x-2"
+              >
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={notification?.dispatcher?.profile?.avatar as string}
+                  alt={`@${notification?.dispatcher?.username}'s avatar'`}
+                />
+                <div>
+                  {notification?.type === 'POSTLIKE' && (
+                    <PostLike notification={notification} />
+                  )}
+                  <div className="text-xs text-gray-400">
+                    {timeago.format(notification?.createdAt)}
+                  </div>
+                </div>
+              </div>
             ))}
           </div>
         </Popover.Panel>
--- src/graphql/resolvers/Notification/NotificationResolver.ts
+++ src/graphql/resolvers/Notification/NotificationResolver.ts
@@ -7,8 +7,11 @@ builder.prismaObject(db.notification, {
   fields: (t) => ({
     id: t.exposeID('id'),
     type: t.exposeString('type'),
+    entityId: t.exposeString('entityId', { nullable: true }),
     receiver: t.relation('receiver'),
-    dispatcher: t.relation('dispatcher')
+    dispatcher: t.relation('dispatcher'),
+    createdAt: t.expose('createdAt', { type: 'DateTime' }),
+    updatedAt: t.expose('updatedAt', { type: 'DateTime' })
   })
 })
 
