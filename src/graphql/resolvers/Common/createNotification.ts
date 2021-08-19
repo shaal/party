b705b1fa88b9b8b04c3c5260b3e9485ed724c7a0
Yogi <yoginth@hey.com>
2021-08-19 10:05:03 +0530

    add: entityId

--- src/graphql/resolvers/Common/createNotification.ts
+++ src/graphql/resolvers/Common/createNotification.ts
@@ -5,6 +5,7 @@ import { db } from '../../../utils/prisma'
 export const createNotification = async (
   receiverId: string,
   dispatcherId: string,
+  entityId: string | null,
   type: NotificationType
 ) => {
   if (receiverId !== dispatcherId) {
@@ -12,6 +13,7 @@ export const createNotification = async (
       data: {
         receiverId,
         dispatcherId,
+        entityId,
         type: type as NotificationType
       }
     })
