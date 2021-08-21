05af980db090082607b1dc51f6409e5c96119418
Yogi <yoginth@hey.com>
2021-08-21 19:53:54 +0530

    update: imports

--- src/components/Post/Reply/Replies.tsx
+++ src/components/Post/Reply/Replies.tsx
@@ -3,11 +3,11 @@ import { ReplyIcon } from '@heroicons/react/outline'
 import React from 'react'
 import useInView from 'react-cool-inview'
 
+import { Post, Reply } from '~/__generated__/schema.generated'
 import PostShimmer from '~/components/shared/Shimmer/PostShimmer'
 import { EmptyState } from '~/components/ui/EmptyState'
 import { ErrorMessage } from '~/components/ui/ErrorMessage'
 
-import { Post, Reply } from '../../../__generated__/schema.generated'
 import { RepliesQuery } from './__generated__/Replies.generated'
 import SingleReply from './SingleReply'
 
