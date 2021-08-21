6a082cde2289cefb76f5a55ba603daeb720b7fb6
Yogi <yoginth@hey.com>
2021-08-21 20:04:35 +0530

    fix: Lint errors

--- src/pages/settings/password.tsx
+++ src/pages/settings/password.tsx
@@ -1,6 +1,7 @@
+import { GetServerSideProps } from 'next'
+
 import ChangePassword from '~/components/User/Settings/ChangePassword'
 import { authenticatedRoute } from '~/utils/redirects'
-import { GetServerSideProps } from 'next'
 
 export const getServerSideProps: GetServerSideProps = authenticatedRoute
 
