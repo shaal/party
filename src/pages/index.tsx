754a6b1f42f28d6412742a4fbdb681faff0489c8
Yogi <yoginth@hey.com>
2021-08-21 19:48:34 +0530

    update: imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
+import Home, { HOME_QUERY as query } from '~/components/Home'
 import { preloadQuery } from '~/utils/apollo'
-
-import Home, { HOME_QUERY as query } from '../components/Home'
 
 export const getServerSideProps = async (ctx: any) => {
   return preloadQuery(ctx, { query })
