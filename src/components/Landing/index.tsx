3d9f17c2b53a26948445f4d37e1e55cbea50ccfd
Yogi <yoginth@hey.com>
2021-08-22 21:21:42 +0530

    WIP

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,13 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
+
+import { Button } from '../ui/Button'
 
 const Landing: React.FC = () => {
   return (
-    <div className="flex flex-grow items-center justify-center">
+    <div className="flex flex-grow items-center justify-center justify-items-center">
       <div className="max-w-md w-full">
-        <h2 className="text-center text-3xl font-extrabold">
-          <img className="mx-auto h-14" src="/logo.svg" alt="Devparty" />
-          <div className="mt-5 font-serif">The social media for developers</div>
-        </h2>
+        <div className="space-y-6 mx-auto">
+          <img className="h-14" src="/logo.svg" alt="Devparty" />
+          <div className="font-serif font-extrabold text-3xl">
+            The social media for developers
+          </div>
+          <div className="space-x-4 flex">
+            <div className="hidden sm:block">
+              <Link href="/signup" passHref>
+                <Button size="lg" variant="primary">
+                  Signup
+                </Button>
+              </Link>
+            </div>
+            <div>
+              <Link href="/login" passHref>
+                <Button size="lg" variant="success">
+                  Login
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
