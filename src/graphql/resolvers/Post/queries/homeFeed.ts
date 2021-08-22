b7c4afca207dc0c4a25385476df011ffb5001ca3
Yogi <yoginth@hey.com>
2021-08-22 17:45:40 +0530

    remove: Session check for homeFeed

--- src/graphql/resolvers/Post/queries/homeFeed.ts
+++ src/graphql/resolvers/Post/queries/homeFeed.ts
@@ -8,41 +8,29 @@ export const homeFeed = async (
   where: WherePostsInput | null | undefined,
   session: Session | null | undefined
 ) => {
-  if (session) {
-    const following = await prisma.user.findUnique({
-      where: { id: session?.userId },
-      select: { following: { select: { id: true } } }
-    })
+  const following = await prisma.user.findUnique({
+    where: { id: session?.userId },
+    select: { following: { select: { id: true } } }
+  })
 
-    return await prisma.post.findMany({
-      ...query,
-      where: {
-        type: where?.type === 'ALL' ? undefined : (where?.type as PostType),
-        user: {
-          // @ts-ignore
-          id: {
-            in: [
-              // @ts-ignore
-              ...following.following.map((user) => user.id),
-              session?.userId
-            ]
-          },
-          spammy: false
-        }
-      },
-      orderBy: {
-        createdAt: 'desc'
+  return await prisma.post.findMany({
+    ...query,
+    where: {
+      type: where?.type === 'ALL' ? undefined : (where?.type as PostType),
+      user: {
+        // @ts-ignore
+        id: {
+          in: [
+            // @ts-ignore
+            ...following.following.map((user) => user.id),
+            session?.userId
+          ]
+        },
+        spammy: false
       }
-    })
-  } else {
-    return await prisma.post.findMany({
-      ...query,
-      where: {
-        type: where?.type === 'ALL' ? undefined : (where?.type as PostType)
-      },
-      orderBy: {
-        createdAt: 'desc'
-      }
-    })
-  }
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
 }
