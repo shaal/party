8009329a0fb92f5321c09b6cbe58873b8907f780
Yogi <yoginth@hey.com>
2021-08-22 12:12:18 +0530

    add: Landing

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+import { GridItemEight, GridItemFour, GridLayout } from '../GridLayout'
+
+const Landing: React.FC = () => {
+  return (
+    <Fragment>
+      <GridLayout>
+        <GridItemEight>Landing WIP</GridItemEight>
+        <GridItemFour>Landing WIP</GridItemFour>
+      </GridLayout>
+    </Fragment>
+  )
+}
+
+export default Landing
