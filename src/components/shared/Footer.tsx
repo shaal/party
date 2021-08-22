758dbc877059025d0a8ad4b881f04512125d722d
Yogi <yoginth@hey.com>
2021-08-22 20:08:36 +0530

    update: footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -15,7 +15,7 @@ const Footer: React.FC = () => {
   }
 
   return (
-    <footer className="mt-4 leading-7 text-sm sticky top-20">
+    <footer className="mt-4 leading-7 text-sm sticky top-20 flex flex-wrap">
       <span className="text-gray-500 dark:text-gray-300 font-bold pr-3">
         © Devparty
       </span>
@@ -52,31 +52,29 @@ const Footer: React.FC = () => {
       >
         GitLab
       </a>
-      <div className="flex mt-1">
-        <a
-          className="pr-3 hover:font-bold"
-          href="https://vercel.com/?utm_source=Devparty&utm_campaign=oss"
-          target="_blank"
-          rel="noreferrer"
-        >
-          ▲ Powered by Vercel
+      <a
+        className="pr-3 hover:font-bold"
+        href="https://vercel.com/?utm_source=Devparty&utm_campaign=oss"
+        target="_blank"
+        rel="noreferrer"
+      >
+        ▲ Powered by Vercel
+      </a>
+      {currentUser?.isStaff && (
+        <a onClick={toggleStaffMode} className="cursor-pointer">
+          {staffMode ? (
+            <div className="flex items-center space-x-1 text-red-500">
+              <ShieldCheckIcon className="h-4 w-4" />
+              <div>Staff mode on</div>
+            </div>
+          ) : (
+            <div className="flex items-center space-x-1 text-green-600">
+              <ShieldExclamationIcon className="h-4 w-4" />
+              <div>Staff mode off</div>
+            </div>
+          )}
         </a>
-        {currentUser?.isStaff && (
-          <a onClick={toggleStaffMode} className="cursor-pointer">
-            {staffMode ? (
-              <div className="flex items-center space-x-1 text-red-500">
-                <ShieldCheckIcon className="h-4 w-4" />
-                <div>Staff mode on</div>
-              </div>
-            ) : (
-              <div className="flex items-center space-x-1 text-green-600">
-                <ShieldExclamationIcon className="h-4 w-4" />
-                <div>Staff mode off</div>
-              </div>
-            )}
-          </a>
-        )}
-      </div>
+      )}
     </footer>
   )
 }
