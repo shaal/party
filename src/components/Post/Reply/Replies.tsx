a7b391b908a115e45948549e5ea14f7fb93fa325
Yogi <yoginth@hey.com>
2021-08-22 18:54:36 +0530

    add: replies to relayStylePagination

---
+++ src/components/Post/Reply/Replies.tsx
@@ -0,0 +1,110 @@
+import { gql, useQuery } from '@apollo/client'
+import { ReplyIcon } from '@heroicons/react/outline'
+import React from 'react'
+import useInView from 'react-cool-inview'
+
+import { Post } from '~/__generated__/schema.generated'
+import PostShimmer from '~/components/shared/Shimmer/PostShimmer'
+import { EmptyState } from '~/components/ui/EmptyState'
+import { ErrorMessage } from '~/components/ui/ErrorMessage'
+
+import SinglePost from '../SinglePost'
+import { RepliesQuery } from './__generated__/Replies.generated'
+
+export const REPLIES_QUERY = gql`
+  query RepliesQuery($after: String, $postId: ID!) {
+    post(id: $postId) {
+      id
+      replies(first: 10, after: $after) {
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        edges {
+          node {
+            id
+            body
+            createdAt
+            hasLiked
+            likesCount
+            user {
+              id
+              username
+              profile {
+                id
+                name
+                avatar
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+interface Props {
+  post: Post
+}
+
+const Replies: React.FC<Props> = ({ post }) => {
+  const { data, loading, error, fetchMore } = useQuery<RepliesQuery>(
+    REPLIES_QUERY,
+    {
+      variables: {
+        after: null,
+        postId: post?.id
+      },
+      pollInterval: 10_000
+    }
+  )
+  const replies = data?.post?.replies?.edges?.map((edge) => edge?.node)
+  const pageInfo = data?.post?.replies?.pageInfo
+
+  const { observe } = useInView({
+    threshold: 1,
+    onChange: ({ observe, unobserve }) => {
+      unobserve()
+      observe()
+    },
+    onEnter: () => {
+      if (pageInfo?.hasNextPage) {
+        fetchMore({
+          variables: {
+            after: pageInfo?.endCursor
+          }
+        })
+      }
+    }
+  })
+
+  if (loading)
+    return (
+      <div className="space-y-3">
+        <PostShimmer />
+        <PostShimmer />
+        <PostShimmer />
+      </div>
+    )
+
+  return (
+    <div>
+      <ErrorMessage title="Failed to load posts" error={error} />
+      <div className="space-y-3">
+        {replies?.length === 0 ? (
+          <EmptyState
+            message="Be the first one to reply!"
+            icon={<ReplyIcon className="h-8 w-8" />}
+          />
+        ) : (
+          replies?.map((reply: any) => (
+            <SinglePost key={reply?.id} post={reply} />
+          ))
+        )}
+        <div ref={observe}></div>
+      </div>
+    </div>
+  )
+}
+
+export default Replies
