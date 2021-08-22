ed919d7a847931adc95d70011e442c40290c91e0
Yogi <yoginth@hey.com>
2021-08-22 18:21:57 +0530

    add: parentId check

--- src/graphql/resolvers/Post/mutations/createPost.ts
+++ src/graphql/resolvers/Post/mutations/createPost.ts
@@ -24,6 +24,19 @@ export const createPost = async (
     }
   }
 
+  let parentId = null
+
+  if (input.parentId) {
+    const parent = await prisma.post.findUnique({
+      where: { id: input.parentId }
+    })
+    if (parent) {
+      parentId = parent?.id
+    } else {
+      throw new Error('Incorrect parent ID')
+    }
+  }
+
   return await prisma.post.create({
     ...query,
     data: {
@@ -37,7 +50,7 @@ export const createPost = async (
       topics: {
         create: parseTopics(getTopics(input.body))
       },
-      parentId: input.parentId
+      parentId
     }
   })
 }
