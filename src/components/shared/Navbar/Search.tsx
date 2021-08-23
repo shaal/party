78cc0f191b93637b9af8db3fc7838e22cb4ef35b
Yogi <yoginth@hey.com>
2021-08-23 11:56:24 +0530

    add: Popover

--- src/components/shared/Navbar/Search.tsx
+++ src/components/shared/Navbar/Search.tsx
@@ -1,5 +1,4 @@
 import { gql, useLazyQuery } from '@apollo/client'
-import { Popover, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
 
 import { SearchPostsQuery } from './__generated__/Search.generated'
@@ -22,6 +21,7 @@ const Search: React.FC = () => {
     useLazyQuery<SearchPostsQuery>(SEARCH_POSTS_QUERY)
 
   const handleSearch = (evt: any) => {
+    console.log(evt.target.value)
     searchPosts({
       variables: {
         keyword: evt.target.value
@@ -31,31 +31,12 @@ const Search: React.FC = () => {
 
   return (
     <Fragment>
-      <Popover className="relative">
-        <Popover.Button className="flex">
-          <input
-            className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 focus:border-indigo-500 focus:ring-indigo-400 outline-none rounded-lg shadow-sm w-full py-1.5"
-            type="text"
-            placeholder="Search Devparty..."
-            onChange={handleSearch}
-          />
-        </Popover.Button>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Popover.Panel className="text-black dark:text-gray-200 absolute right-0 w-full min-w-max py-1 mt-2">
-            <div className="bg-white dark:bg-gray-900 py-3 px-5 shadow-md rounded-lg transition border border-gray-200 dark:border-gray-800 max-h-[80vh] space-y-6 overflow-y-scroll">
-              Hello
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </Popover>
+      <input
+        className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 focus:border-indigo-500 focus:ring-indigo-400 outline-none rounded-lg shadow-sm w-full py-1.5"
+        type="text"
+        placeholder="Search Devparty..."
+        onChange={handleSearch}
+      />
     </Fragment>
   )
 }
