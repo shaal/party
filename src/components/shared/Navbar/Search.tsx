4c73ffecaf439f5ebb31f209dfbab5e98db1c7c1
Yogi <yoginth@hey.com>
2021-08-23 11:31:08 +0530

    add: handleSearch

--- src/components/shared/Navbar/Search.tsx
+++ src/components/shared/Navbar/Search.tsx
@@ -1,9 +1,14 @@
 const Search: React.FC = () => {
+  const handleSearch = (evt: any) => {
+    console.log(evt.target.value)
+  }
+
   return (
     <input
       className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 focus:border-indigo-500 focus:ring-indigo-400 outline-none rounded-lg shadow-sm w-full py-1.5"
       type="text"
       placeholder="Search Devparty..."
+      onChange={handleSearch}
     />
   )
 }
