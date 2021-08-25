7887494ddf8b6640b8ec3c0392b878ed7434c958
Yogi <yoginth@hey.com>
2021-08-25 18:26:53 +0530

    add: toastOptions const

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,32 +12,30 @@ import { useApollo } from '~/utils/apollo'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialClientState)
+  const toastOptions = {
+    success: {
+      className: 'border border-green-500',
+      iconTheme: {
+        primary: '#10B981',
+        secondary: 'white'
+      }
+    },
+    error: {
+      className: 'border border-red-500',
+      iconTheme: {
+        primary: '#EF4444',
+        secondary: 'white'
+      }
+    },
+    loading: { className: 'border border-gray-300' }
+  }
 
   return (
     <ApolloProvider client={client}>
       <ThemeProvider defaultTheme="light" attribute="class">
         <DefaultSeo defaultTitle="Devparty" titleTemplate="%s | Devparty" />
         <NProgress />
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            success: {
-              className: 'border border-green-500',
-              iconTheme: {
-                primary: '#10B981',
-                secondary: 'white'
-              }
-            },
-            error: {
-              className: 'border border-red-500',
-              iconTheme: {
-                primary: '#EF4444',
-                secondary: 'white'
-              }
-            },
-            loading: { className: 'border border-gray-300' }
-          }}
-        />
+        <Toaster position="top-right" toastOptions={toastOptions} />
         <DefaultLayout>
           <Component {...pageProps} />
         </DefaultLayout>
