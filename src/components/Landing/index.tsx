114d4a1080d40a64d30ad3bb717e574dc4d3e609
Yogi <yoginth@hey.com>
2021-08-26 19:29:24 +0530

    update: Landing page

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -6,31 +6,27 @@ import { Button } from '../ui/Button'
 
 const Landing: React.FC = () => {
   return (
-    <div>
-      <div className="container mx-auto max-w-screen-2xl flex-grow py-8 lg:px-16 md:px-10 sm:px-5 px-0">
-        <div className="grid grid-cols-12 lg:gap-10">
-          <div className="lg:col-span-7 md:col-span-12 col-span-12">
-            <div className="space-y-6 mx-auto">
-              <div className="font-serif font-extrabold text-3xl">
-                The social media for developers
-              </div>
-              <div className="space-x-4 flex">
-                <div className="hidden sm:block">
-                  <Link href="/signup" passHref>
-                    <Button
-                      size="lg"
-                      variant="primary"
-                      className="flex items-center space-x-1.5"
-                    >
-                      <ArrowCircleRightIcon className="h-5 w-5" />
-                      <div>Get started</div>
-                    </Button>
-                  </Link>
-                </div>
-              </div>
+    <div className="flex flex-grow justify-center">
+      <div className="grid grid-cols-12 w-full">
+        <div className="lg:col-span-7 md:col-span-12 col-span-12 bg-indigo-500"></div>
+        <div className="lg:col-span-5 md:col-span-12 col-span-12 py-8 lg:px-16 md:px-10 sm:px-5 px-3">
+          <div className="space-y-6 mx-auto">
+            <div className="font-serif font-extrabold text-3xl">
+              The social media for developers
+            </div>
+            <div className="space-x-4 flex">
+              <Link href="/signup" passHref>
+                <Button
+                  size="lg"
+                  variant="primary"
+                  className="flex items-center space-x-1.5"
+                >
+                  <ArrowCircleRightIcon className="h-5 w-5" />
+                  <div>Get started</div>
+                </Button>
+              </Link>
             </div>
           </div>
-          <div className="lg:col-span-5 md:col-span-12 col-span-12">hi</div>
         </div>
       </div>
     </div>
