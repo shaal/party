3af92258767befc2f5b2c23a21d5dab765f969b9
Yogi <yoginth@hey.com>
2021-08-26 10:52:34 +0530

    add: Followers page

---
+++ src/components/User/Followers/index.tsx
@@ -0,0 +1,53 @@
+import { useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
+import React, { Fragment } from 'react'
+
+import { User } from '~/__generated__/schema.generated'
+import DetailsShimmer from '~/components/shared/Shimmer/DetailsShimmer'
+import { ErrorMessage } from '~/components/ui/ErrorMessage'
+
+import { GridItemEight, GridItemFour, GridLayout } from '../../GridLayout'
+import { ViewUserQuery } from '../__generated__/ViewUser.generated'
+import Details from '../Details'
+import { VIEW_USER_QUERY } from '../ViewUser'
+import FollowersList from './list'
+
+export const USER_FOLLOWERS_QUERY = VIEW_USER_QUERY
+
+const Followers: React.FC = () => {
+  const router = useRouter()
+  const { data, loading, error } = useQuery<ViewUserQuery>(
+    USER_FOLLOWERS_QUERY,
+    {
+      variables: {
+        username: router.query.username!.slice(1)
+      },
+      skip: !router.isReady
+    }
+  )
+
+  return (
+    <Fragment>
+      {data?.user?.profile?.cover ? (
+        <img
+          className="object-cover bg-gradient-to-r from-blue-400 to-purple-400 h-60 w-full"
+          src={data?.user?.profile?.cover as string}
+          alt={`@${data?.user?.username}'s cover`}
+        />
+      ) : (
+        <div className="bg-gradient-to-r from-blue-400 to-purple-400 h-60 w-full" />
+      )}
+      <GridLayout>
+        <GridItemFour>
+          <ErrorMessage title="Failed to load post" error={error} />
+          {loading ? <DetailsShimmer /> : <Details user={data?.user as User} />}
+        </GridItemFour>
+        <GridItemEight>
+          <FollowersList user={data?.user as User} />
+        </GridItemEight>
+      </GridLayout>
+    </Fragment>
+  )
+}
+
+export default Followers
---
+++ src/components/User/Followers/list.tsx
@@ -0,0 +1,105 @@
+import { gql, useQuery } from '@apollo/client'
+import { UsersIcon } from '@heroicons/react/outline'
+import React from 'react'
+import useInView from 'react-cool-inview'
+
+import { User } from '~/__generated__/schema.generated'
+import { ErrorMessage } from '~/components/ui/ErrorMessage'
+
+import UserProfile from '../../shared/UserProfile'
+import { Card, CardBody } from '../../ui/Card'
+import { EmptyState } from '../../ui/EmptyState'
+import { FollowersQuery } from './__generated__/list.generated'
+
+export const FOLLOWERS_QUERY = gql`
+  query FollowersQuery($after: String, $userId: ID!) {
+    user(id: $userId) {
+      followers(first: 5, after: $after) {
+        totalCount
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        edges {
+          node {
+            id
+            username
+            hasFollowed
+            profile {
+              id
+              name
+              avatar
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+interface Props {
+  user: User
+}
+
+const FollowersList: React.FC<Props> = ({ user }) => {
+  const { data, loading, error, fetchMore } = useQuery<FollowersQuery>(
+    FOLLOWERS_QUERY,
+    {
+      variables: {
+        after: null,
+        userId: user?.id
+      }
+    }
+  )
+  const followers = data?.user?.followers?.edges?.map((edge) => edge?.node)
+  const pageInfo = data?.user?.followers?.pageInfo
+
+  const { observe } = useInView({
+    threshold: 1,
+    onChange: ({ observe, unobserve }) => {
+      unobserve()
+      observe()
+    },
+    onEnter: () => {
+      if (pageInfo?.hasNextPage) {
+        fetchMore({
+          variables: {
+            after: pageInfo?.endCursor ? pageInfo?.endCursor : null
+          }
+        })
+      }
+    }
+  })
+
+  if (loading) return <div>Loading followers...</div>
+
+  return (
+    <div>
+      <ErrorMessage title="Failed to load posts" error={error} />
+      <div className="space-y-3">
+        {followers?.length === 0 ? (
+          <EmptyState
+            message={
+              <div>
+                <span className="font-bold mr-1">@{user?.username}</span>
+                <span>doesn’t have any followers yet.</span>
+              </div>
+            }
+            icon={<UsersIcon className="h-8 w-8" />}
+          />
+        ) : (
+          followers?.map((user: any) => (
+            <Card key={user?.id}>
+              <CardBody>
+                <UserProfile user={user} showFollow />
+              </CardBody>
+            </Card>
+          ))
+        )}
+        <span ref={observe}></span>
+      </div>
+    </div>
+  )
+}
+
+export default FollowersList
