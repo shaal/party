375f49b13d6d71f195f9bdd3bf3f9c144231282e
Yogi <yoginth@hey.com>
2021-08-27 09:41:23 +0530

    add: searchProducts

--- src/components/shared/Navbar/Search.tsx
+++ src/components/shared/Navbar/Search.tsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react'
 
 import {
   SearchPostsQuery,
+  SearchProductsQuery,
   SearchUsersQuery
 } from './__generated__/Search.generated'
 
@@ -32,11 +33,26 @@ export const SEARCH_USERS_QUERY = gql`
   }
 `
 
+export const SEARCH_PRODUCTS_QUERY = gql`
+  query SearchProductsQuery($keyword: String!) {
+    searchProduct(first: 5, keyword: $keyword) {
+      edges {
+        node {
+          id
+          slug
+        }
+      }
+    }
+  }
+`
+
 const Search: React.FC = () => {
   const [searchPosts, { loading: postsLoading, data: posts }] =
     useLazyQuery<SearchPostsQuery>(SEARCH_POSTS_QUERY)
   const [searchUsers, { loading: usersLoading, data: users }] =
     useLazyQuery<SearchUsersQuery>(SEARCH_USERS_QUERY)
+  const [searchProducts, { loading: productsLoading, data: products }] =
+    useLazyQuery<SearchProductsQuery>(SEARCH_PRODUCTS_QUERY)
 
   const handleSearch = (evt: any) => {
     console.log(evt.target.value)
@@ -46,6 +62,11 @@ const Search: React.FC = () => {
       }
     })
     searchUsers({
+      variables: {
+        keyword: evt.target.value
+      }
+    })
+    searchProducts({
       variables: {
         keyword: evt.target.value
       }
