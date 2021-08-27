1dc2b36799301550a8005e6e08c1d96be073809d
Yogi <yoginth@hey.com>
2021-08-27 03:01:08 +0000

    update: Landing page

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -9,8 +9,10 @@ const Landing: React.FC = () => {
     <div className="flex flex-grow justify-center">
       <div className="grid grid-cols-12 w-full">
         <div
-          className="lg:col-span-7 md:col-span-12 col-span-12 bg-indigo-500 md:block hidden"
-          style={{ backgroundImage: `url('https://svgur.com/i/_Zt.svg')` }}
+          className="lg:col-span-7 md:col-span-12 col-span-12 bg-indigo-600 md:block hidden"
+          style={{
+            backgroundImage: `url('https://cloudflare-ipfs.com/ipfs/bafybeicsg73girok5jgd23wau7xyoq7fquvmr5qss6ojeycxkqelmhsnlm/topography.svg')`
+          }}
         ></div>
         <div className="lg:col-span-5 md:col-span-12 col-span-12 py-8 lg:px-14 md:px-10 sm:px-5 px-5 flex flex-col">
           <div className="space-y-7 flex-grow">
