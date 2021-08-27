1a3c993e8186a3290acf85d0bd5605006100c756
Yogi <yoginth@hey.com>
2021-08-27 11:50:01 +0530

    add: PageLoading

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,12 +1,11 @@
 import { gql, useQuery } from '@apollo/client'
 import React from 'react'
 
-import PostShimmer from '~/components/shared/Shimmer/PostShimmer'
-import UserProfileShimmer from '~/components/shared/Shimmer/UserProfileShimmer'
 import { Card, CardBody } from '~/components/ui/Card'
 import { ErrorMessage } from '~/components/ui/ErrorMessage'
 
-import { GridItemEight, GridItemFour, GridLayout } from '../GridLayout'
+import { GridItemEight, GridLayout } from '../GridLayout'
+import { PageLoading } from '../ui/PageLoading'
 import { NotificationsQuery } from './__generated__/index.generated'
 
 export const NOTIFICATIONS_QUERY = gql`
@@ -25,21 +24,7 @@ const Notifications: React.FC = () => {
   const { data, loading, error } =
     useQuery<NotificationsQuery>(NOTIFICATIONS_QUERY)
 
-  if (loading)
-    return (
-      <GridLayout>
-        <GridItemEight>
-          <PostShimmer />
-        </GridItemEight>
-        <GridItemFour>
-          <Card>
-            <CardBody>
-              <UserProfileShimmer showFollow />
-            </CardBody>
-          </Card>
-        </GridItemFour>
-      </GridLayout>
-    )
+  if (loading) return <PageLoading message="Loading notifications..." />
 
   return (
     <GridLayout>
