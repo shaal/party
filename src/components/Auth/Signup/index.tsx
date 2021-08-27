53693fbfae07fee1678271e45b55bcb38b633308
Yogi <yoginth@hey.com>
2021-08-27 11:25:11 +0530

    update: Polish auth pages

--- src/components/Auth/Signup/index.tsx
+++ src/components/Auth/Signup/index.tsx
@@ -8,7 +8,7 @@ const Signup: React.FC = () => {
     <div className="flex flex-grow">
       <div className="grid grid-cols-12 w-full">
         <div
-          className="lg:col-span-7 md:col-span-12 col-span-12 bg-indigo-600 md:block hidden"
+          className="lg:col-span-7 md:col-span-12 col-span-12 bg-indigo-600 dark:bg-indigo-700 md:block hidden"
           style={{
             backgroundImage: `url('https://cloudflare-ipfs.com/ipfs/bafybeicsg73girok5jgd23wau7xyoq7fquvmr5qss6ojeycxkqelmhsnlm/topography.svg')`
           }}
