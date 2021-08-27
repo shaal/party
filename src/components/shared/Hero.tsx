972171d70275272c1c38d9d05845980a4a9776ce
Yogi <yoginth@hey.com>
2021-08-27 11:35:51 +0530

    add: Hero comp

---
+++ src/components/shared/Hero.tsx
@@ -0,0 +1,12 @@
+const Hero: React.FC = () => {
+  return (
+    <div
+      className="lg:col-span-7 md:col-span-12 col-span-12 bg-indigo-600 dark:bg-indigo-700 md:block hidden"
+      style={{
+        backgroundImage: `url('https://cloudflare-ipfs.com/ipfs/bafybeicsg73girok5jgd23wau7xyoq7fquvmr5qss6ojeycxkqelmhsnlm/topography.svg')`
+      }}
+    ></div>
+  )
+}
+
+export default Hero
