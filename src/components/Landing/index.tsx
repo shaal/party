a5378eab66d284969af150ed847eba1cfebfe797
Yogi <yoginth@hey.com>
2021-08-27 03:09:48 +0000

    add: LandingFooter comp

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleRightIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import React from 'react'
 
+import LandingFooter from '../shared/LandingFooter'
 import { Button } from '../ui/Button'
 
 const Landing: React.FC = () => {
@@ -46,35 +47,7 @@ const Landing: React.FC = () => {
               </Link>
             </div>
           </div>
-          <footer className="leading-7 flex flex-wrap">
-            <span className="text-gray-500 dark:text-gray-300 font-bold pr-3">
-              © Devparty
-            </span>
-            <span className="pr-3">
-              <Link href="/">About</Link>
-            </span>
-            <span className="pr-3">
-              <Link href="/">Terms</Link>
-            </span>
-            <span className="pr-3">
-              <Link href="/">Privacy</Link>
-            </span>
-            <a
-              href="https://gitlab.com/yo/devparty"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Status
-            </a>
-          </footer>
-          <a
-            className="mt-2 hover:font-bold"
-            href="https://vercel.com/?utm_source=Devparty&utm_campaign=oss"
-            target="_blank"
-            rel="noreferrer"
-          >
-            ▲ Powered by Vercel
-          </a>
+          <LandingFooter />
         </div>
       </div>
     </div>
---
+++ src/components/shared/LandingFooter.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import { Fragment } from 'react'
+
+const LandingFooter: React.FC = () => {
+  return (
+    <Fragment>
+      <footer className="leading-7 flex flex-wrap">
+        <span className="text-gray-500 dark:text-gray-300 font-bold pr-3">
+          © Devparty
+        </span>
+        <span className="pr-3">
+          <Link href="/">About</Link>
+        </span>
+        <span className="pr-3">
+          <Link href="/">Terms</Link>
+        </span>
+        <span className="pr-3">
+          <Link href="/">Privacy</Link>
+        </span>
+        <a
+          href="https://gitlab.com/yo/devparty"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Status
+        </a>
+      </footer>
+      <a
+        className="mt-2 hover:font-bold"
+        href="https://vercel.com/?utm_source=Devparty&utm_campaign=oss"
+        target="_blank"
+        rel="noreferrer"
+      >
+        ▲ Powered by Vercel
+      </a>
+    </Fragment>
+  )
+}
+
+export default LandingFooter
