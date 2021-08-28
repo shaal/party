17027a4b6f2aeee9e638fbe65b214fe9d7016c21
Yogi <yoginth@hey.com>
2021-08-28 19:32:53 +0530

    update: ChangePasswordForm

---
+++ src/components/User/Settings/Security/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import {
+  GridItemEight,
+  GridItemFour,
+  GridLayout
+} from '~/components/GridLayout'
+import { Card, CardBody } from '~/components/ui/Card'
+
+import Sidebar from '../Sidebar'
+import ChangePasswordForm from './Form'
+
+const SecuritySettings: React.FC = () => {
+  return (
+    <GridLayout>
+      <GridItemFour>
+        <Sidebar />
+      </GridItemFour>
+      <GridItemEight>
+        <Card>
+          <CardBody>
+            <ChangePasswordForm />
+          </CardBody>
+        </Card>
+      </GridItemEight>
+    </GridLayout>
+  )
+}
+
+export default SecuritySettings
