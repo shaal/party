b40974c1df6dd52f99d5d2afbdff9eb4de4d3dc6
Yogi <yoginth@hey.com>
2021-09-02 18:18:51 +0530

    add: React version in staffbar

--- src/components/shared/Navbar/StaffBar.tsx
+++ src/components/shared/Navbar/StaffBar.tsx
@@ -8,6 +8,12 @@ const StaffBar: React.FC = () => {
     <div className="bg-gray-200 dark:bg-black px-3 py-1 flex justify-between text-sm">
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-1">
+          React.js{' '}
+          <span className="text-xs font-bold bg-gray-300 dark:bg-gray-900 py-0.5 px-1.5 rounded-md">
+            v{React.version}
+          </span>
+        </div>
+        <div className="flex items-center gap-1">
           Next.js{' '}
           <span className="text-xs font-bold bg-gray-300 dark:bg-gray-900 py-0.5 px-1.5 rounded-md">
             v{packageJson?.dependencies?.next.replace('^', '')}
