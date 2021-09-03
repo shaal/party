a7e3d675d86a81ff0781cb6580725f50dd361aaa
Yogi <yoginth@hey.com>
2021-09-03 09:32:15 +0530

    add: wakatimeActivity mvp

--- src/graphql/resolvers/Integration/wakatimeActivity.ts
+++ src/graphql/resolvers/Integration/wakatimeActivity.ts
@@ -1,3 +1,13 @@
+import { db } from '~/utils/prisma'
+
 export const wakatimeActivity = async (userId: string) => {
-  return userId
+  const integration = await db.integration.findFirst({
+    where: { userId }
+  })
+  const response = await fetch(
+    `https://wakatime.com/api/v1/users/yoginth/all_time_since_today?api_key=${integration?.wakatimeAPIKey}`
+  )
+  const reader = await response.json()
+
+  return reader.data.text
 }
