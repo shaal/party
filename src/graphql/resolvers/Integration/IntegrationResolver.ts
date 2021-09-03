ccecc88c3601d4c18f55380e9eb9b3c9619e853b
Yogi <yoginth@hey.com>
2021-09-03 09:18:14 +0530

    add: wakatimeActivity mvp

---
+++ src/graphql/resolvers/Integration/IntegrationResolver.ts
@@ -0,0 +1,37 @@
+import { builder } from '~/graphql/builder'
+import { db } from '~/utils/prisma'
+
+import { wakatimeActivity } from './wakatimeActivity'
+
+builder.prismaObject('Integration', {
+  findUnique: (integration) => ({ id: integration.id }),
+  fields: (t) => ({
+    id: t.exposeID('id', {}),
+    wakatimeActivity: t.field({
+      type: 'String',
+      nullable: true,
+      resolve: async (root, args, { session }) => {
+        return await wakatimeActivity(session?.userId as string)
+      }
+    }),
+
+    // Timestamps
+    createdAt: t.expose('createdAt', { type: 'DateTime' }),
+    updatedAt: t.expose('updatedAt', { type: 'DateTime' })
+  })
+})
+
+builder.queryField('integration', (t) =>
+  t.prismaField({
+    type: 'Integration',
+    args: {
+      userId: t.arg.id({})
+    },
+    nullable: true,
+    resolve: async (query, root, { userId }) => {
+      return await db.integration.findFirst({
+        where: { userId }
+      })
+    }
+  })
+)
---
+++ src/graphql/resolvers/Integration/wakatimeActivity.ts
@@ -0,0 +1,3 @@
+export const wakatimeActivity = async (userId: string) => {
+  return userId
+}
