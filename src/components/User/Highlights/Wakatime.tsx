4e5752a3742168c3c5fdb299118f8c327d1a3468
Yogi <yoginth@hey.com>
2021-09-04 17:04:50 +0530

    add: Wakatime logo

--- src/components/User/Highlights/Wakatime.tsx
+++ src/components/User/Highlights/Wakatime.tsx
@@ -3,6 +3,7 @@ import { ClockIcon } from '@heroicons/react/outline'
 import { Fragment } from 'react'
 
 import { User } from '~/__generated__/schema.generated'
+import { Tooltip } from '~/components/ui/Tooltip'
 
 import { Card } from '../../ui/Card'
 import { WakatimeIntegrationsQuery } from './__generated__/Wakatime.generated'
@@ -34,7 +35,23 @@ const Wakatime: React.FC<Props> = ({ user }) => {
     <Fragment>
       {data?.wakatime?.hours && (
         <Card className="p-3 space-y-1 bg-blue-100 border-blue-300">
-          <div>Hours coded last 30 days</div>
+          <div className="flex items-center justify-between">
+            <div>Hours coded last 30 days</div>
+            <Tooltip content="Go to WakaTime">
+              <a
+                className="font-bold"
+                href="https://wakatime.com?utm_source=devparty"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="h-4 w-4"
+                  src="/brand/wakatime-dark.svg"
+                  alt="Wakatime Logo"
+                />
+              </a>
+            </Tooltip>
+          </div>
           <div className="flex items-center space-x-1.5">
             <ClockIcon className="h-4 w-4" />
             <div className="font-bold font-mono">{data?.wakatime?.hours}</div>
