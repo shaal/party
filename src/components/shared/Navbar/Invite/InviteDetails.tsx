470cc580539f9b0386f9a1ec94305eb39940dc3a
Yogi <yoginth@hey.com>
2021-09-06 11:24:38 +0530

    add: InviteCodeQuery MVP

--- src/components/shared/Navbar/Invite/InviteDetails.tsx
+++ src/components/shared/Navbar/Invite/InviteDetails.tsx
@@ -1,5 +1,29 @@
+import { gql, useQuery } from '@apollo/client'
+
+import { InviteCodeQuery } from './__generated__/InviteDetails.generated'
+
+export const INVITE_CODE_QUERY = gql`
+  query InviteCodeQuery {
+    me {
+      invite {
+        id
+        code
+        usedTimes
+      }
+    }
+  }
+`
+
 const InviteDetails: React.FC = () => {
-  return <div>Hello</div>
+  const { data, loading } = useQuery<InviteCodeQuery>(INVITE_CODE_QUERY)
+
+  if (loading) return <div>Loading Invite Code...</div>
+
+  return (
+    <div>
+      <div>{data?.me?.invite?.code}</div>
+    </div>
+  )
 }
 
 export default InviteDetails
