f85930175ec357f202e9e25dcf5042a0ab6f3382
Yogi <yoginth@hey.com>
2021-09-06 12:00:32 +0530

    rename: To Join Waitlist

--- src/components/Auth/Signup/index.tsx
+++ src/components/Auth/Signup/index.tsx
@@ -23,7 +23,7 @@ const Signup: React.FC = () => {
               alt="Hero"
             />
             <div className="space-y-2">
-              <div className="font-extrabold text-4xl">Signup</div>
+              <div className="font-extrabold text-4xl">Join Waitlist</div>
               <div className="linkify">
                 Already have an account?{' '}
                 <Link href="/login">
