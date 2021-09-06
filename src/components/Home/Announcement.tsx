d5b443cffdede18d21d552d5cc892522ef0c5665
Yogi <yoginth@hey.com>
2021-09-06 10:16:10 +0530

    update: Announcement

--- src/components/Home/Announcement.tsx
+++ src/components/Home/Announcement.tsx
@@ -6,7 +6,7 @@ const Announcement: React.FC = () => {
   return (
     <Card className="mb-4 border-yellow-400 px-5 py-3">
       <div className="text-yellow-700">
-        Devparty is currently in Preview mode, your data might be lost
+        Devparty is currently in <b>Preview mode</b>, your data might be lost.
       </div>
     </Card>
   )
