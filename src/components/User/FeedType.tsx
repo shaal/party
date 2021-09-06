69dec26e945c405d3ef671e2aab3ea12c3ef00fd
Yogi <yoginth@hey.com>
2021-09-06 09:54:35 +0530

    remove: Post count

--- src/components/User/FeedType.tsx
+++ src/components/User/FeedType.tsx
@@ -15,12 +15,7 @@ const FeedType: React.FC<Props> = ({ user, setFeedType }) => {
         onClick={() => setFeedType('POST')}
       >
         <CollectionIcon className="h-4 w-4" />
-        <div className="flex item-center space-x-2">
-          <div>Posts</div>
-          <div className="bg-gray-200 dark:bg-gray-800 px-2 rounded-full text-gray-800 dark:text-gray-200 font-bold">
-            {user?.posts?.totalCount}
-          </div>
-        </div>
+        <div>Posts</div>
       </button>
       <button
         className="flex items-center space-x-2 rounded-lg px-3 py-1 text-pink-500 hover:bg-pink-100 hover:text-pink-500 dark:hover:bg-opacity-20 hover:bg-opacity-100"
