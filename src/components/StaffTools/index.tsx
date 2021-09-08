20b76e4b483965a6d3d569ff022747cb140e2547
Yogi <yoginth@hey.com>
2021-09-08 14:39:26 +0530

    add: Basic stats

--- src/components/StaffTools/index.tsx
+++ src/components/StaffTools/index.tsx
@@ -3,21 +3,31 @@ import React from 'react'
 
 import { GridItemEight, GridItemFour, GridLayout } from '../GridLayout'
 import { Card, CardBody } from '../ui/Card'
+import { ErrorMessage } from '../ui/ErrorMessage'
+import { PageLoading } from '../ui/PageLoading'
 import { StaffToolsDashboardQuery } from './__generated__/index.generated'
 import Sidebar from './Sidebar'
 
 export const STAFF_TOOLS_DASHBOARD_QUERY = gql`
   query StaffToolsDashboardQuery {
-    me {
-      id
+    stats {
+      products
+      users
+      posts
+      likes
+      notifications
+      topics
     }
   }
 `
 
 const StaffToolsDashboard: React.FC = () => {
   const { data, loading, error } = useQuery<StaffToolsDashboardQuery>(
-    STAFF_TOOLS_DASHBOARD_QUERY
+    STAFF_TOOLS_DASHBOARD_QUERY,
+    { pollInterval: 2000 }
   )
+
+  if (loading) return <PageLoading message="Loading dashboard..." />
 
   return (
     <GridLayout>
@@ -26,7 +36,15 @@ const StaffToolsDashboard: React.FC = () => {
       </GridItemFour>
       <GridItemEight>
         <Card>
-          <CardBody>TBD</CardBody>
+          <CardBody>
+            <ErrorMessage title="Failed to stats" error={error} />
+            <div>{data?.stats?.users} Users</div>
+            <div>{data?.stats?.products} Products</div>
+            <div>{data?.stats?.posts} Posts</div>
+            <div>{data?.stats?.likes} Likes</div>
+            <div>{data?.stats?.topics} Topics</div>
+            <div>{data?.stats?.notifications} Notifications</div>
+          </CardBody>
         </Card>
       </GridItemEight>
     </GridLayout>
