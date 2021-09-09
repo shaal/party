891d66445a5ce43c0971a96a4e186524edbf856b
Yogi <yoginth@hey.com>
2021-09-09 11:05:46 +0530

    use: React latest

--- src/components/shared/Navbar/StaffBar.tsx
+++ src/components/shared/Navbar/StaffBar.tsx
@@ -1,4 +1,3 @@
-import { Disclosure } from '@headlessui/react'
 import { DatabaseIcon, ShieldCheckIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import React from 'react'
@@ -7,23 +6,11 @@ const StaffBar: React.FC = () => {
   const reactVersion = React.version.split('-')
   return (
     <div className="bg-gray-200 dark:bg-black px-3 py-1 flex justify-between text-sm">
-      <div className="flex items-center space-x-4">
-        <div className="flex items-center space-x-1">
-          <div>React.js</div>
-          <Disclosure>
-            <span className="flex text-xs font-bold bg-gray-300 dark:bg-gray-900 py-0.5 px-1.5 rounded-md">
-              <Disclosure.Button className="font-bold">
-                v{reactVersion[0]}
-              </Disclosure.Button>
-              <Disclosure.Panel>
-                <span className="mx-0.5">·</span>
-                <span>{reactVersion[1]}</span>
-                <span className="mx-0.5">·</span>
-                <span>{reactVersion[2]}</span>
-              </Disclosure.Panel>
-            </span>
-          </Disclosure>
-        </div>
+      <div className="flex items-center gap-1">
+        React.js{' '}
+        <span className="text-xs font-bold bg-gray-300 dark:bg-gray-900 py-0.5 px-1.5 rounded-md">
+          v{React.version}
+        </span>
       </div>
       <div className="flex items-center gap-4">
         <a
