f270d3e0a0d99fee33990691679e9f6209e6f959
Yogi <yoginth@hey.com>
2021-09-09 16:58:03 +0530

    update: Adopt design for mobile view in product profile

--- src/components/Product/Details.tsx
+++ src/components/Product/Details.tsx
@@ -1,6 +1,6 @@
 import Linkify from 'linkifyjs/react'
 import Link from 'next/link'
-import { useContext } from 'react'
+import { Fragment, useContext } from 'react'
 
 import { Product } from '~/__generated__/schema.generated'
 import { Button } from '~/components/ui/Button'
@@ -16,34 +16,36 @@ const Details: React.FC<Props> = ({ product }) => {
   const { currentUser, staffMode } = useContext(AppContext)
 
   return (
-    <div className="space-y-5">
-      <img
-        src={product?.avatar as string}
-        className="rounded-lg h-40 w-40 ring-8 ring-gray-50 dark:ring-black"
-        alt={`#${product?.slug}'s avatar`}
-      />
-      <div>
-        <div className="text-2xl font-bold flex items-center gap-1.5">
-          {product?.name}
+    <Fragment>
+      <div className="px-5 sm:px-0 space-y-5">
+        <img
+          src={product?.avatar as string}
+          className="rounded-lg h-28 w-28 sm:h-40 sm:w-40 ring-8 ring-gray-50 dark:ring-black"
+          alt={`#${product?.slug}'s avatar`}
+        />
+        <div>
+          <div className="text-2xl font-bold flex items-center gap-1.5">
+            {product?.name}
+          </div>
+          <div className="text-xl">{product?.slug}</div>
         </div>
-        <div className="text-xl">{product?.slug}</div>
+        {currentUser?.id !== product?.user?.id ? (
+          <div>Follow</div>
+        ) : (
+          <Link href={`/products/${product?.slug}/settings`} passHref>
+            <Button size="md" variant="success">
+              Edit Product
+            </Button>
+          </Link>
+        )}
+        {product?.description && (
+          <div className="linkify">
+            <Linkify>{product?.description}</Linkify>
+          </div>
+        )}
       </div>
-      {currentUser?.id !== product?.user?.id ? (
-        <div>Follow</div>
-      ) : (
-        <Link href={`/products/${product?.slug}/settings`} passHref>
-          <Button size="md" variant="success">
-            Edit Product
-          </Button>
-        </Link>
-      )}
-      {product?.description && (
-        <div className="linkify">
-          <Linkify>{product?.description}</Linkify>
-        </div>
-      )}
       {currentUser?.isStaff && staffMode && <ProductMod product={product} />}
-    </div>
+    </Fragment>
   )
 }
 
