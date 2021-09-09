aae3c9f72a6b9cb46835dd42d5d22d72034d7749
Yogi <yoginth@hey.com>
2021-09-09 09:59:01 +0530

    remove: preact and use react alpha

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
-  },
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     BASE_URL: process.env.BASE_URL
