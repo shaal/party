63ea7de23b6281771bbd2c6ebbda0329f952518f
Yogi <yoginth@hey.com>
2021-09-09 07:52:11 +0530

    rename: To communities

--- prisma/migrations/20210909021756_init/migration.sql
+++ prisma/migrations/20210909021756_init/migration.sql
@@ -134,7 +134,7 @@ CREATE TABLE `Product` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Group` (
+CREATE TABLE `Community` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
@@ -143,7 +143,7 @@ CREATE TABLE `Group` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Group_slug_key`(`slug`),
+    UNIQUE INDEX `Community_slug_key`(`slug`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -175,12 +175,12 @@ CREATE TABLE `Integration` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `_GroupToUser` (
+CREATE TABLE `_CommunityToUser` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `_GroupToUser_AB_unique`(`A`, `B`),
-    INDEX `_GroupToUser_B_index`(`B`)
+    UNIQUE INDEX `_CommunityToUser_AB_unique`(`A`, `B`),
+    INDEX `_CommunityToUser_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
