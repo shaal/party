7a127151fb2f5dca0938abfc6c135e1a75760406
Yogi <yoginth@hey.com>
2021-09-11 09:17:43 +0530

    add: who to follow mvp

--- src/graphql/resolvers/User/queries/getWhoToFollow.ts
+++ src/graphql/resolvers/User/queries/getWhoToFollow.ts
@@ -6,10 +6,21 @@ export const getWhoToFollow = async (
   query: any,
   session: Session | null | undefined
 ) => {
+  const following = await db.user.findUnique({
+    where: { id: session?.userId },
+    select: { following: { select: { id: true } } }
+  })
+
   return await db.user.findMany({
     ...query,
     take: 5,
-    where: { spammy: false, id: { not: session?.userId } },
+    where: {
+      spammy: false,
+      id: {
+        // @ts-ignore
+        notIn: [...following.following.map((user) => user.id), session?.userId]
+      }
+    },
     orderBy: { createdAt: 'desc' }
   })
 }
