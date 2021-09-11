8a63b131a0381a4aa6b06f61dcffdf70c28e14e8
Yogi <yoginth@hey.com>
2021-09-11 11:41:10 +0530

    update: More posts card

--- src/components/Post/MorePosts.tsx
+++ src/components/Post/MorePosts.tsx
@@ -19,6 +19,7 @@ export const MORE_POSTS_BY_USER_QUERY = gql`
             username
             profile {
               id
+              name
               avatar
             }
           }
@@ -37,7 +38,7 @@ const MorePosts: React.FC<Props> = ({ post }) => {
     MORE_POSTS_BY_USER_QUERY,
     {
       variables: {
-        wheere: {
+        where: {
           userId: post?.user?.id,
           type: 'QUESTION'
         }
@@ -45,16 +46,25 @@ const MorePosts: React.FC<Props> = ({ post }) => {
       skip: !post?.user?.id
     }
   )
+  const posts = data?.morePostsByUser?.edges?.map((edge) => edge?.node)
 
   if (loading) return <div>Loading More Posts</div>
 
   return (
     <Card>
-      <CardBody>
+      <CardBody className="space-y-5">
         <ErrorMessage title="Failed to load more posts" error={error} />
-        {data?.morePostsByUser?.edges?.map((post: any) => (
-          <div key={post?.node?.id}>
-            <div>{post?.node?.title}</div>
+        {posts?.map((post: any) => (
+          <div key={post?.id} className="space-y-2">
+            <div className="font-bold">{post?.title}</div>
+            <div className="flex items-start space-x-1 text-sm">
+              <img
+                className="h-5 w-5 rounded-full"
+                src={post?.user?.profile?.avatar}
+                alt={`@${post?.user?.profile?.avatar}'s avatar'`}
+              />
+              <div>{post?.user?.profile?.name}</div>
+            </div>
           </div>
         ))}
       </CardBody>
