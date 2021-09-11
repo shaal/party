65068a133aee5a94ad254cba5c9cf6cf898ca130
Yogi <yoginth@hey.com>
2021-09-11 16:40:23 +0530

    add: New badge check

--- src/components/User/Badges.tsx
+++ src/components/User/Badges.tsx
@@ -1,9 +1,11 @@
 import { gql, useQuery } from '@apollo/client'
 import { PlusIcon } from '@heroicons/react/outline'
+import { useContext } from 'react'
 
 import { User } from '~/__generated__/schema.generated'
 
 import { Button } from '../ui/Button'
+import AppContext from '../utils/AppContext'
 import { UserProductsQuery } from './__generated__/OwnedProducts.generated'
 
 export const USER_PRODUCTS_QUERY = gql`
@@ -26,6 +28,7 @@ interface Props {
 }
 
 const Badges: React.FC<Props> = ({ user }) => {
+  const { currentUser } = useContext(AppContext)
   const { data, loading } = useQuery<UserProductsQuery>(USER_PRODUCTS_QUERY, {
     variables: {
       where: { userId: user?.id }
@@ -34,13 +37,13 @@ const Badges: React.FC<Props> = ({ user }) => {
 
   return (
     <div className="space-y-2">
-      {data?.products?.edges?.length !== 0 && (
-        <div className="font-bold">Badges</div>
-      )}
-      <div className="flex flex-wrap gap-1.5 w-3/4">
-        <Button variant="secondary" outline>
-          <PlusIcon className="h-5 w-5" />
-        </Button>
+      <div className="font-bold">Badges</div>
+      <div>
+        {currentUser?.id === user?.id && (
+          <Button variant="secondary" outline>
+            <PlusIcon className="h-5 w-5" />
+          </Button>
+        )}
       </div>
     </div>
   )
