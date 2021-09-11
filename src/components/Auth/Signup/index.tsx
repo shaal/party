8cf563f6ed8cb5279f5265c61d0d7aeeaeaac7d4
Yogi <yoginth@hey.com>
2021-09-11 08:49:01 +0530

    update: To emojis folder #88

--- src/components/Auth/Signup/index.tsx
+++ src/components/Auth/Signup/index.tsx
@@ -18,7 +18,7 @@ const Signup: React.FC = () => {
         <div className="lg:col-span-5 md:col-span-12 col-span-12 py-8 lg:px-14 md:px-10 sm:px-5 px-5 flex flex-col">
           <div className="space-y-7 flex-grow">
             <img
-              src="https://assets.devparty.io/images/handshake.png"
+              src="https://assets.devparty.io/images/emojis/handshake.png"
               className="w-20"
               alt="Handshake Emoji"
             />
--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -14,7 +14,7 @@ const Landing: React.FC = () => {
         <div className="lg:col-span-5 md:col-span-12 col-span-12 py-8 lg:px-14 md:px-10 sm:px-5 px-5 flex flex-col">
           <div className="space-y-7 flex-grow">
             <img
-              src="https://assets.devparty.io/images/wave.png"
+              src="https://assets.devparty.io/images/emojis/wave.png"
               className="w-20"
               alt="Waving Emoji"
             />
