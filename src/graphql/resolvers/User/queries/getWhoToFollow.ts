5acfecf3d65fd711bf943711f02a019327a51f5c
Yogi <yoginth@hey.com>
2021-09-11 09:08:55 +0530

    add: take 5

--- src/graphql/resolvers/User/queries/getWhoToFollow.ts
+++ src/graphql/resolvers/User/queries/getWhoToFollow.ts
@@ -3,6 +3,7 @@ import { db } from '~/utils/prisma'
 export const getWhoToFollow = async (query: any) => {
   return await db.user.findMany({
     ...query,
+    take: 5,
     where: { spammy: false },
     orderBy: { createdAt: 'desc' }
   })
