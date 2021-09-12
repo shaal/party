7c8f77b845a2cebfc6cb3b6df1370ce7ab6eca54
Yogi <yoginth@hey.com>
2021-09-12 02:23:31 +0000

    add: Card

--- src/components/Pages/Terms.tsx
+++ src/components/Pages/Terms.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 
 import Footer from '../shared/Footer'
+import { Card, CardBody } from '../ui/Card'
 
 const Terms: React.FC = () => {
   return (
@@ -19,84 +20,90 @@ const Terms: React.FC = () => {
       </div>
       <div className="relative">
         <div className="flex justify-center">
-          <div className="bg-white dark:bg-gray-900 mb-5 relative mx-auto -top-14 rounded-lg max-w-3/4 md:w-3/4 p-8 shadow-lg">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-            <div className="mt-5 font-semibold text-xl">1. Terms</div>
-            <p className="mt-2">
-              Terms By accessing this Website, accessible from taskord.com
-              and/or www.taskord.com, you are agreeing to be bound by these
-              Website Terms and Conditions of Use and agree that you are
-              responsible for the agreement with any applicable local laws. If
-              you disagree with any of these terms, you are prohibited from
-              accessing this site. The materials contained in this Website are
-              protected by copyright and trademark law.
-            </p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-            <div className="mt-5 font-semibold text-xl">1. Terms</div>
-            <p className="my-2">
-              Terms By accessing this Website, accessible from taskord.com
-              and/or www.taskord.com, you are agreeing to be bound by these
-              Website Terms and Conditions of Use and agree that you are
-              responsible for the agreement with any applicable local laws. If
-              you disagree with any of these terms, you are prohibited from
-              accessing this site. The materials contained in this Website are
-              protected by copyright and trademark law.
-            </p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-            <div className="mt-5 font-semibold text-xl">1. Terms</div>
-            <p className="my-2">
-              Terms By accessing this Website, accessible from taskord.com
-              and/or www.taskord.com, you are agreeing to be bound by these
-              Website Terms and Conditions of Use and agree that you are
-              responsible for the agreement with any applicable local laws. If
-              you disagree with any of these terms, you are prohibited from
-              accessing this site. The materials contained in this Website are
-              protected by copyright and trademark law.
-            </p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-            <div className="mt-5 font-semibold text-xl">1. Terms</div>
-            <p className="my-2">
-              Terms By accessing this Website, accessible from taskord.com
-              and/or www.taskord.com, you are agreeing to be bound by these
-              Website Terms and Conditions of Use and agree that you are
-              responsible for the agreement with any applicable local laws. If
-              you disagree with any of these terms, you are prohibited from
-              accessing this site. The materials contained in this Website are
-              protected by copyright and trademark law.
-            </p>
-          </div>
+          <Card className="relative mx-auto -top-14 rounded-lg max-w-3/4 md:w-3/4 shadow-lg">
+            <CardBody className="!p-8">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+              <div className="mt-5 font-semibold text-xl">1. Terms</div>
+              <p className="mt-2">
+                Terms By accessing this Website, accessible from taskord.com
+                and/or www.taskord.com, you are agreeing to be bound by these
+                Website Terms and Conditions of Use and agree that you are
+                responsible for the agreement with any applicable local laws. If
+                you disagree with any of these terms, you are prohibited from
+                accessing this site. The materials contained in this Website are
+                protected by copyright and trademark law.
+              </p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+              <div className="mt-5 font-semibold text-xl">1. Terms</div>
+              <p className="my-2">
+                Terms By accessing this Website, accessible from taskord.com
+                and/or www.taskord.com, you are agreeing to be bound by these
+                Website Terms and Conditions of Use and agree that you are
+                responsible for the agreement with any applicable local laws. If
+                you disagree with any of these terms, you are prohibited from
+                accessing this site. The materials contained in this Website are
+                protected by copyright and trademark law.
+              </p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+              <div className="mt-5 font-semibold text-xl">1. Terms</div>
+              <p className="my-2">
+                Terms By accessing this Website, accessible from taskord.com
+                and/or www.taskord.com, you are agreeing to be bound by these
+                Website Terms and Conditions of Use and agree that you are
+                responsible for the agreement with any applicable local laws. If
+                you disagree with any of these terms, you are prohibited from
+                accessing this site. The materials contained in this Website are
+                protected by copyright and trademark law.
+              </p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+              <div className="mt-5 font-semibold text-xl">1. Terms</div>
+              <p className="my-2">
+                Terms By accessing this Website, accessible from taskord.com
+                and/or www.taskord.com, you are agreeing to be bound by these
+                Website Terms and Conditions of Use and agree that you are
+                responsible for the agreement with any applicable local laws. If
+                you disagree with any of these terms, you are prohibited from
+                accessing this site. The materials contained in this Website are
+                protected by copyright and trademark law.
+              </p>
+            </CardBody>
+          </Card>
         </div>
         <div className="flex justify-center pb-4">
           <Footer />
