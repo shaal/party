a1323b4436cadb695591c1de351bbfec52f65573
Yogi <yoginth@hey.com>
2021-09-12 12:13:44 +0530

    update: Terms page

--- src/components/Pages/Terms.tsx
+++ src/components/Pages/Terms.tsx
@@ -1,14 +1,13 @@
 import React, { Fragment } from 'react'
 
 import Footer from '../shared/Footer'
-import { Card, CardBody } from '../ui/Card'
 
 const Terms: React.FC = () => {
   return (
     <Fragment>
-      <div className="flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-400 h-64 w-full">
+      <div className="flex items-center justify-center bg-brand-400 h-44 w-full">
         <div className="relative text-center">
-          <h1 className="text-3xl md:text-4xl font-semibold -mt-5 text-white">
+          <h1 className="text-3xl md:text-4xl font-semibold text-white">
             Terms and Conditions
           </h1>
           <div className="flex justify-center mt-4">
@@ -20,8 +19,8 @@ const Terms: React.FC = () => {
       </div>
       <div className="relative">
         <div className="flex justify-center">
-          <Card className="relative mx-auto -mt-14 rounded-lg max-w-3/4 md:w-3/4">
-            <CardBody className="!p-8 prose max-w-none text-gray-900 dark:text-gray-200">
+          <div className="relative mx-auto rounded-lg max-w-3/4 md:w-3/4">
+            <div className="!p-8 prose max-w-none text-gray-900 dark:text-gray-200">
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
@@ -110,8 +109,8 @@ const Terms: React.FC = () => {
                 accessing this site. The materials contained in this Website are
                 protected by copyright and trademark law.
               </p>
-            </CardBody>
-          </Card>
+            </div>
+          </div>
         </div>
         <div className="flex justify-center pt-2 pb-6">
           <Footer />
