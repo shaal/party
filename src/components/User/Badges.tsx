10d27a211f48c105b8fe9c55d32fe2949c9a3595
Yogi <yoginth@hey.com>
2021-09-12 13:07:37 +0530

    add: User badges query

---
+++ src/components/User/Badges.tsx
@@ -0,0 +1,44 @@
+import { gql, useQuery } from '@apollo/client'
+
+import { User } from '~/__generated__/schema.generated'
+
+import { UserBadgesQuery } from './__generated__/Badges.generated'
+
+export const USER_BADGES_QUERY = gql`
+  query UserBadgesQuery($id: ID) {
+    user(id: $id) {
+      id
+      badges {
+        edges {
+          node {
+            id
+            name
+            icon
+          }
+        }
+      }
+    }
+  }
+`
+
+interface Props {
+  user: User
+}
+
+const Badges: React.FC<Props> = ({ user }) => {
+  const { data, loading } = useQuery<UserBadgesQuery>(USER_BADGES_QUERY, {
+    variables: {
+      id: user?.id
+    },
+    skip: !user?.id
+  })
+
+  return (
+    <div className="space-y-2">
+      <div className="font-bold">Badges</div>
+      <div>TBD</div>
+    </div>
+  )
+}
+
+export default Badges
