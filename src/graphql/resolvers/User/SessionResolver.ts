ddc16fd100ce3d487b56756b2a126a528569acdb
Yogi <yoginth@hey.com>
2021-09-12 08:31:00 +0530

    add: SessionResolver

---
+++ src/graphql/resolvers/User/SessionResolver.ts
@@ -0,0 +1,33 @@
+import { builder } from '~/graphql/builder'
+
+import { getSessions } from './queries/getSessions'
+
+builder.prismaObject('Session', {
+  findUnique: (user) => ({ id: user.id }),
+  fields: (t) => ({
+    id: t.exposeID('id', {}),
+    isStaff: t.exposeBoolean('isStaff', {}),
+
+    // Timestamps
+    createdAt: t.expose('createdAt', { type: 'DateTime' }),
+    updatedAt: t.expose('updatedAt', { type: 'DateTime' }),
+    expiresAt: t.expose('expiresAt', { type: 'DateTime' }),
+
+    // Relations
+    user: t.relation('user')
+  })
+})
+
+builder.queryField('sessions', (t) =>
+  t.prismaConnection({
+    type: 'Session',
+    cursor: 'id',
+    defaultSize: 20,
+    maxSize: 100,
+    authScopes: { user: true },
+    nullable: true,
+    resolve: async (query, root, args, { session }) => {
+      return await getSessions(query, session)
+    }
+  })
+)
