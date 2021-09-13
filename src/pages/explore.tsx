2af1cb224239a6064fbe9dd7e02e8f7dd8cb32b5
Yogi <yoginth@hey.com>
2021-09-13 14:03:29 +0530

    update: Expose explore page

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,11 +1,7 @@
 import Explore, { EXPLORE_QUERY as query } from '~/components/Explore'
 import { preloadQuery } from '~/utils/apollo'
-import { authenticatedRoute } from '~/utils/redirects'
 
 export const getServerSideProps = async (ctx: any) => {
-  const auth = await authenticatedRoute(ctx)
-  if ('redirect' in auth) return auth
-
   return preloadQuery(ctx, { query })
 }
 
