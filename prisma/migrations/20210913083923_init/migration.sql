7c3efed1a9b26595349d5257ba7d28723b068790
Yogi <yoginth@hey.com>
2021-09-13 14:09:39 +0530

    add: attach topics with users

--- prisma/migrations/20210913083923_init/migration.sql
+++ prisma/migrations/20210913083923_init/migration.sql
@@ -211,6 +211,15 @@ CREATE TABLE `_BadgeToUser` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `_TopicToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_TopicToUser_AB_unique`(`A`, `B`),
+    INDEX `_TopicToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `_follows` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
