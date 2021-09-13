04e90b88e14abb2d3fa9ca8d227f149d3d7607b4
Yogi <yoginth@hey.com>
2021-09-13 13:35:10 +0530

    mvp: Explore page

---
+++ src/components/Explore/Feed.tsx
@@ -0,0 +1,88 @@
+import { gql, useQuery } from '@apollo/client'
+import { CollectionIcon } from '@heroicons/react/outline'
+import React from 'react'
+import useInView from 'react-cool-inview'
+
+import { EmptyState } from '~/components/ui/EmptyState'
+import { ErrorMessage } from '~/components/ui/ErrorMessage'
+
+import SinglePost, { PostFragment } from '../Post/SinglePost'
+import PostsShimmer from '../shared/Shimmer/PostsShimmer'
+import { ExploreFeedQuery } from './__generated__/Feed.generated'
+
+export const EXPLORE_FEED_QUERY = gql`
+  query ExploreFeedQuery($after: String, $where: WhereHomeFeedInput) {
+    homeFeed(first: 10, after: $after, where: $where) {
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      edges {
+        node {
+          ...PostFragment
+        }
+      }
+    }
+  }
+  ${PostFragment}
+`
+
+interface Props {
+  feedType?: string
+}
+
+const ExploreFeed: React.FC<Props> = ({ feedType }) => {
+  const { data, loading, error, fetchMore } = useQuery<ExploreFeedQuery>(
+    EXPLORE_FEED_QUERY,
+    {
+      variables: {
+        after: null,
+        where: {
+          type: feedType
+        }
+      }
+    }
+  )
+  const posts = data?.homeFeed?.edges?.map((edge) => edge?.node)
+  const pageInfo = data?.homeFeed?.pageInfo
+
+  const { observe } = useInView({
+    threshold: 1,
+    onChange: ({ observe, unobserve }) => {
+      unobserve()
+      observe()
+    },
+    onEnter: () => {
+      if (pageInfo?.hasNextPage) {
+        fetchMore({
+          variables: {
+            after: pageInfo?.endCursor
+          }
+        })
+      }
+    }
+  })
+
+  if (loading) return <PostsShimmer />
+
+  return (
+    <div>
+      <ErrorMessage title="Failed to load posts" error={error} />
+      <div className="space-y-3">
+        {posts?.length === 0 ? (
+          <EmptyState
+            message="No posts found, follow some users!"
+            icon={<CollectionIcon className="h-8 w-8" />}
+          />
+        ) : (
+          posts?.map((post: any) => (
+            <SinglePost key={post?.id} post={post} showParent />
+          ))
+        )}
+        {pageInfo?.hasNextPage && <span ref={observe}></span>}
+      </div>
+    </div>
+  )
+}
+
+export default ExploreFeed
