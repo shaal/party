c484050d5f732ac694c2e686105a3e9c02f36cca
Yogi <yoginth@hey.com>
2021-09-14 18:45:25 +0530

    rename: root to parent

--- src/graphql/resolvers/Integration/IntegrationResolver.ts
+++ src/graphql/resolvers/Integration/IntegrationResolver.ts
@@ -10,12 +10,12 @@ builder.prismaObject('Integration', {
     wakatimeAPIKey: t.field({
       type: 'String',
       nullable: true,
-      resolve: async (root, args, { session }) => {
-        if (!session || session.userId !== root.userId) {
+      resolve: async (parent, args, { session }) => {
+        if (!session || session.userId !== parent.userId) {
           return null
         }
         const integration = await db.integration.findUnique({
-          where: { id: root.id }
+          where: { id: parent.id }
         })
         return integration?.wakatimeAPIKey
       }
@@ -23,16 +23,17 @@ builder.prismaObject('Integration', {
     spotifyRefreshToken: t.field({
       type: 'String',
       nullable: true,
-      resolve: async (root, args, { session }) => {
-        if (!session || session.userId !== root.userId) {
+      resolve: async (parent, args, { session }) => {
+        if (!session || session.userId !== parent.userId) {
           return null
         }
         const integration = await db.integration.findUnique({
-          where: { id: root.id }
+          where: { id: parent.id }
         })
         return integration?.spotifyRefreshToken
       }
     }),
+
     // Timestamps
     createdAt: t.expose('createdAt', { type: 'DateTime' }),
     updatedAt: t.expose('updatedAt', { type: 'DateTime' })
@@ -44,7 +45,7 @@ builder.queryField('integration', (t) =>
     type: 'Integration',
     args: { userId: t.arg.id({ required: false }) },
     nullable: true,
-    resolve: async (query, root, { userId }, { session }) => {
+    resolve: async (query, parent, { userId }, { session }) => {
       return await db.integration.findFirst({
         where: { userId: userId ? userId : session?.userId }
       })
@@ -63,7 +64,7 @@ builder.mutationField('editIntegration', (t) =>
   t.prismaField({
     type: 'Integration',
     args: { input: t.arg({ type: EditIntegrationInput }) },
-    resolve: async (query, root, { input }, { session }) => {
+    resolve: async (query, parent, { input }, { session }) => {
       return await editIntegration(input, session)
     }
   })
