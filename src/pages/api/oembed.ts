ae9975905e954cbee1fb0c5cdcf7d4e7c68ab32d
Yogi <yoginth@hey.com>
2021-09-14 21:46:47 +0530

    add: data caching

--- src/pages/api/oembed.ts
+++ src/pages/api/oembed.ts
@@ -1,3 +1,4 @@
+import cacheData from 'memory-cache'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { unfurl } from 'unfurl.js'
 
@@ -5,7 +6,15 @@ const oembed = async (req: NextApiRequest, res: NextApiResponse) => {
   const { url } = req.query
   if (url) {
     const stringifiedURL = url.toString()
-    return res.status(200).send(await unfurl(stringifiedURL))
+    const value = cacheData.get(url)
+    if (value) {
+      return res.status(200).send(value)
+    } else {
+      const hours = 24
+      const data = await unfurl(stringifiedURL)
+      cacheData.put(url, data, hours * 1000 * 60 * 60)
+      return res.status(200).send(data)
+    }
   } else {
     return res.status(400).send({
       status: 'error',
