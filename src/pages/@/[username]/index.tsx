1df678bfd790d1d6317ffa21707526233cb5cb02
Yogi <yoginth@hey.com>
2021-09-14 16:21:45 +0530

    fix: 404 not working

--- src/pages/@/[username]/index.tsx
+++ src/pages/@/[username]/index.tsx
@@ -2,14 +2,10 @@ import ViewUser, { VIEW_USER_QUERY as query } from '~/components/User/ViewUser'
 import { preloadQuery } from '~/utils/apollo'
 
 export const getServerSideProps = async (ctx: any) => {
-  const yo = await preloadQuery(ctx, {
+  return await preloadQuery(ctx, {
     query,
     variables: { where: { username: ctx.params!.username } }
   })
-
-  console.log(yo)
-
-  return yo
 }
 
 export default ViewUser
