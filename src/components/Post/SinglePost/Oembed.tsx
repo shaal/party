2a5037f15204a283ccf02bc5d526676de83a273c
Yogi <yoginth@hey.com>
2021-09-14 20:52:41 +0530

    add: basic oembed

---
+++ src/components/Post/SinglePost/Oembed.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import { Card, CardBody } from '~/components/ui/Card'
+import { Spinner } from '~/components/ui/Spinner'
+
+interface Props {
+  loading: boolean
+  oembed: any
+}
+
+const Oembed: React.FC<Props> = ({ loading, oembed }) => {
+  if (loading)
+    return (
+      <Card>
+        <CardBody>
+          <Spinner className="mx-auto" size="sm" />
+        </CardBody>
+      </Card>
+    )
+
+  return (
+    <Card>
+      <CardBody className="flex items-center justify-between space-x-5">
+        <div className="text-sm space-y-2">
+          <div className="font-bold flex items-center space-x-2">
+            <img
+              className="h-4 w-4 rounded-md"
+              src={oembed?.favicon}
+              alt="Favicon"
+            />
+            <div className="line-clamp-1">{oembed?.title}</div>
+          </div>
+          <div className="line-clamp-2 text-gray-500">
+            {oembed?.description}
+          </div>
+        </div>
+        {oembed?.open_graph?.images[0]?.url && (
+          <div>
+            <img
+              className="rounded-lg w-[50rem]"
+              src={oembed?.open_graph?.images[0]?.url}
+              alt="Image"
+            />
+          </div>
+        )}
+      </CardBody>
+    </Card>
+  )
+}
+
+export default Oembed
--- src/components/Post/SinglePost/Type/Post.tsx
+++ src/components/Post/SinglePost/Type/Post.tsx
@@ -1,12 +1,13 @@
 import * as linkify from 'linkifyjs'
 import hashtag from 'linkifyjs/plugins/hashtag'
 import Linkify from 'linkifyjs/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Post } from '~/__generated__/schema.generated'
 import { linkifyOptions } from '~/components/utils/linkifyOptions'
 
 import Attachments from '../Attachments'
+import Oembed from '../Oembed'
 
 interface Props {
   post: Post
@@ -15,12 +16,22 @@ interface Props {
 hashtag(linkify)
 
 const PostType: React.FC<Props> = ({ post }) => {
+  const [oembed, setOembed] = useState()
+  const [oembedLoading, setOembedLoading] = useState<boolean>(false)
+  useEffect(() => {
+    setOembedLoading(true)
+    fetch('/api/oembed?url=https://github.com/Noovolari/leapp')
+      .then((response) => response.json())
+      .then((data) => setOembed(data))
+      .finally(() => setOembedLoading(false))
+  }, [])
   return (
     <div className="space-y-3">
       <div className="linkify space-y-3 inline-flex">
         <Linkify options={linkifyOptions}>{post?.body}</Linkify>
       </div>
       {post?.attachments && <Attachments attachments={post?.attachments} />}
+      <Oembed loading={oembedLoading} oembed={oembed} />
     </div>
   )
 }
