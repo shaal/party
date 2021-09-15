64f85eedf3ae54cd5927692064c8c45c54bec363
Yogi <yoginth@hey.com>
2021-09-15 18:02:00 +0530

    add: waitlist check

--- src/pages/waitlist.tsx
+++ src/pages/waitlist.tsx
@@ -1,3 +1,8 @@
+import { GetServerSideProps } from 'next'
+
 import Waitlist from '~/components/Pages/Waitlist'
+import { unauthenticatedRoute } from '~/utils/redirects'
+
+export const getServerSideProps: GetServerSideProps = unauthenticatedRoute
 
 export default Waitlist
