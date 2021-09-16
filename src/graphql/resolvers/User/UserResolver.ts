140d09580bc01c3ce8eed7793ab427cdedc6f81b
Yogi <yoginth@hey.com>
2021-09-16 19:51:14 +0530

    add: hide inWaitlist users

--- src/graphql/resolvers/User/UserResolver.ts
+++ src/graphql/resolvers/User/UserResolver.ts
@@ -86,9 +86,9 @@ builder.queryField('user', (t) =>
     args: { username: t.arg.id() },
     nullable: true,
     resolve: async (query, parent, { username }) => {
-      return await db.user.findUnique({
+      return await db.user.findFirst({
         ...query,
-        where: { username },
+        where: { username, inWaitlist: false },
         rejectOnNotFound: true
       })
     }
