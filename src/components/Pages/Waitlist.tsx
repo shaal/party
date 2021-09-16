3243fc4afb8f36b0efc948aaca42dd3aad57b08f
Yogi <yoginth@hey.com>
2021-09-16 15:29:11 +0530

    remove: Waitlist page

--- src/components/Pages/Waitlist.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Waitlist: React.FC = () => {
-  return <div>Waitlist WIP</div>
-}
-
-export default Waitlist
--- src/pages/waitlist.tsx
+++
@@ -1,7 +0,0 @@
-import Waitlist from '@components/Pages/Waitlist'
-import { unauthenticatedRoute } from '@utils/redirects'
-import { GetServerSideProps } from 'next'
-
-export const getServerSideProps: GetServerSideProps = unauthenticatedRoute
-
-export default Waitlist
