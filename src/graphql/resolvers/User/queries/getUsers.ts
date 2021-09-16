8521b9e1d7e05bf2d202fd3881de1740b283c852
Yogi <yoginth@hey.com>
2021-09-16 19:51:55 +0530

    add: hide inWaitlist users

--- src/graphql/resolvers/User/queries/getUsers.ts
+++ src/graphql/resolvers/User/queries/getUsers.ts
@@ -3,7 +3,7 @@ import { db } from '@utils/prisma'
 export const getUsers = async (query: any) => {
   return await db.user.findMany({
     ...query,
-    where: { spammy: false },
+    where: { spammy: false, inWaitlist: false },
     orderBy: { createdAt: 'desc' }
   })
 }
