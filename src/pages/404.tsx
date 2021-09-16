5813ab33a7a399c60a54cfb28fc6733387c0b9bd
Yogi <yoginth@hey.com>
2021-09-16 14:02:25 +0000

    update: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@components/ui/Button'
 import { HomeIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import { NextSeo } from 'next-seo'
@@ -13,12 +14,14 @@ export default function Custom404() {
           className="h-60"
         />
         <div className="py-10 text-center">
-          <h1 className="text-3xl mb-4 font-bold">Opps, Lost?! </h1>
+          <h1 className="text-3xl mb-4 font-bold">Oops, Lost‽</h1>
           <div className="mb-4">This page could not be found.</div>
-          <Link href="/signup">
-            <a className="inline-flex items-center justify-center w-full px-6 py-2 no-underline bg-indigo-600 border border-indigo-600 rounded-lg cursor-pointer md:w-auto lg:mt-0 hover:bg-indigo-700 hover:border-indigo-700 text-white hover:text-white focus-within:bg-indigo-700 sm:text-lg md:text-xl">
-              <HomeIcon className="w-5 h-5 mr-2" />
-              Go Home
+          <Link href="/signup" passHref>
+            <a>
+              <Button className="flex item-center mx-auto" size="lg">
+                <HomeIcon className="w-5 h-5 mr-2" />
+                <div>Go to home</div>
+              </Button>
             </a>
           </Link>
         </div>
