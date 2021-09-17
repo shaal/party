3f7e8cf8e4aeea219c04ec8ce77e2d77e136a79f
Yogi <yoginth@hey.com>
2021-09-17 20:22:01 +0530

    remove: PS login

--- src/components/shared/Navbar/StaffBar.tsx
+++ src/components/shared/Navbar/StaffBar.tsx
@@ -1,4 +1,4 @@
-import { DatabaseIcon, ShieldCheckIcon } from '@heroicons/react/outline'
+import { ShieldCheckIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import React from 'react'
 
@@ -12,14 +12,6 @@ const StaffBar: React.FC = () => {
         </span>
       </div>
       <div className="flex items-center gap-4">
-        <a
-          href="https://app.planetscale.com/yo/devparty"
-          title="Planetscale"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <DatabaseIcon className="h-4 w-4" />
-        </a>
         <Link href="/stafftools">
           <a title="Staff panel">
             <ShieldCheckIcon className="h-4 w-4" />
