2b605e946d86b9c41126487642a36e3c7d0be863
Yogi <yoginth@hey.com>
2021-09-17 11:51:36 +0530

    update: FieldError message

--- src/components/ui/Form.tsx
+++ src/components/ui/Form.tsx
@@ -41,7 +41,12 @@ export const FieldError: React.FC<FieldErrorProps> = ({ name }) => {
 
   if (!error) return null
 
-  return <div className="text-sm text-red-500 font-bold">{error.message}</div>
+  return (
+    <div className="text-sm mt-1 flex items-center space-x-1 text-red-500 font-bold">
+      <div className="text-xs">😱</div>
+      <div>{error.message}</div>
+    </div>
+  )
 }
 
 interface Props<T extends FieldValues = any>
