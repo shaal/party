c65fcdfaa0d79c849dbf90cc41dbf944878e7602
Yogi <yoginth@hey.com>
2021-09-17 13:06:45 +0530

    add: handler for join and signup

--- src/graphql/resolvers/User/mutations/joinWaitlist.ts
+++ src/graphql/resolvers/User/mutations/joinWaitlist.ts
@@ -9,29 +9,40 @@ export const joinWaitlist = async (query: any, input: JoinWaitlistInput) => {
     throw new Error(`Username "${input.username}" is reserved by Devparty.`)
   }
 
-  const user = await db.user.create({
-    ...query,
-    data: {
-      username: input.username,
-      email: input.email,
-      hashedPassword: await hashPassword(input.password),
-      inWaitlist: true,
-      profile: {
-        create: {
-          name: input.username,
-          avatar: `https://avatar.tobi.sh/${await md5(input.email)}.svg`
-        }
-      },
-      invite: {
-        create: {
-          code: await (await md5(input.email + Math.random()))
-            .slice(0, 12)
-            .toLowerCase()
-        }
-      },
-      integrations: { create: {} }
-    }
-  })
+  try {
+    const user = await db.user.create({
+      ...query,
+      data: {
+        username: input.username,
+        email: input.email,
+        hashedPassword: await hashPassword(input.password),
+        inWaitlist: true,
+        profile: {
+          create: {
+            name: input.username,
+            avatar: `https://avatar.tobi.sh/${await md5(input.email)}.svg`
+          }
+        },
+        invite: {
+          create: {
+            code: await (await md5(input.email + Math.random()))
+              .slice(0, 12)
+              .toLowerCase()
+          }
+        },
+        integrations: { create: {} }
+      }
+    })
 
-  return user
+    return user
+  } catch (error: PrismaClientKnownRequestError | any) {
+    if (error.code === 'P2002') {
+      if (error.meta.target === 'users_username_key')
+        throw new Error('Username is already taken!')
+      if (error.meta.target === 'users_email_key')
+        throw new Error('Email is already taken!')
+    }
+
+    throw new Error('Something went wrong!')
+  }
 }
--- src/graphql/resolvers/User/mutations/signUp.ts
+++ src/graphql/resolvers/User/mutations/signUp.ts
@@ -18,35 +18,44 @@ export const signUp = async (query: any, input: SignupInput, req: any) => {
     throw new Error('Invite code is invalid or expired.')
   }
 
-  const user = await db.user.create({
-    ...query,
-    data: {
-      username: input.username,
-      email: input.email,
-      hashedPassword: await hashPassword(input.password),
-      profile: {
-        create: {
-          name: input.username,
-          avatar: `https://avatar.tobi.sh/${await md5(input.email)}.svg`
-        }
-      },
-      invite: {
-        create: {
-          code: await (await md5(input.email + Math.random()))
-            .slice(0, 12)
-            .toUpperCase()
-        }
-      },
-      integrations: { create: {} }
+  try {
+    const user = await db.user.create({
+      ...query,
+      data: {
+        username: input.username,
+        email: input.email,
+        hashedPassword: await hashPassword(input.password),
+        profile: {
+          create: {
+            name: input.username,
+            avatar: `https://avatar.tobi.sh/${await md5(input.email)}.svg`
+          }
+        },
+        invite: {
+          create: {
+            code: await (await md5(input.email + Math.random()))
+              .slice(0, 12)
+              .toUpperCase()
+          }
+        },
+        integrations: { create: {} }
+      }
+    })
+
+    await createSession(req, user)
+
+    await db.invite.updateMany({
+      where: { code: input.invite },
+      data: { usedTimes: { increment: 1 } }
+    })
+
+    return user
+  } catch (error: PrismaClientKnownRequestError | any) {
+    console.log(error)
+    if (error.code === 'P2002') {
+      throw new Error('Username is already taken!')
     }
-  })
 
-  await createSession(req, user)
-
-  await db.invite.updateMany({
-    where: { code: input.invite },
-    data: { usedTimes: { increment: 1 } }
-  })
-
-  return user
+    throw new Error('Something went wrong!')
+  }
 }
