d18c4aeb941d624182ad112258c17b325e378867
Yogi <yoginth@hey.com>
2021-09-19 17:24:22 +0530

    add: NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     BASE_URL: process.env.BASE_URL,
-    GIT_COMMIT_REF: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF
+    GIT_COMMIT_REF: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
+    GIT_COMMIT_SHA: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
   }
 }
