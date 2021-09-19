6497b202dc9d9da84853fe7b928d12e331068ab3
Yogi <yoginth@hey.com>
2021-09-19 13:31:15 +0530

    refactor: SSR

--- src/pages/@/[username]/index.tsx
+++ src/pages/@/[username]/index.tsx
@@ -2,7 +2,7 @@ import ViewUser, { VIEW_USER_QUERY as query } from '@components/User/ViewUser'
 import { preloadQuery } from '@utils/apollo'
 
 export const getServerSideProps = async (ctx: any) => {
-  return await preloadQuery(ctx, {
+  return preloadQuery(ctx, {
     query,
     variables: { username: ctx.params!.username }
   })
