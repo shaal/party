d42421b98739944079abc6b88e47b01bc1da1150
Yogi <yoginth@hey.com>
2021-09-20 17:58:07 +0530

    add: Preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+
+    return config
+  },
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     BASE_URL: process.env.BASE_URL,
