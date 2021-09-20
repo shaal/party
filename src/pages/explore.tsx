6cb66fdd3bd4ca3572bfd5d0c7fd38daa2b7b958
Yogi <yoginth@hey.com>
2021-09-20 12:26:35 +0530

    replace: to EXPLORE_FEED_QUERY

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,11 +1,15 @@
-import Explore, { GET_EXPLORE_USER_QUERY as query } from '@components/Explore'
+import Explore from '@components/Explore'
+import { EXPLORE_FEED_QUERY } from '@components/Explore/Feed'
 import { preloadQuery } from '@utils/apollo'
 import { GetServerSidePropsContext } from 'next'
 
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
-  return preloadQuery(context, { query })
+  return preloadQuery(context, {
+    query: EXPLORE_FEED_QUERY,
+    variables: { after: null }
+  })
 }
 
 export default Explore
