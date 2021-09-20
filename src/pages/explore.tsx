0828159ad798260e357836c5b0e355d8355fb56f
Yogi <yoginth@hey.com>
2021-09-20 09:30:34 +0530

    add: CURRENT_USER_QUERY during preload

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,3 +1,4 @@
+import { CURRENT_USER_QUERY } from '@components/DefaultLayout'
 import Explore, { GET_EXPLORE_USER_QUERY as query } from '@components/Explore'
 import { preloadQuery } from '@utils/apollo'
 import { GetServerSidePropsContext } from 'next'
@@ -5,6 +6,7 @@ import { GetServerSidePropsContext } from 'next'
 export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
+  preloadQuery(context, { query: CURRENT_USER_QUERY })
   return preloadQuery(context, { query })
 }
 
