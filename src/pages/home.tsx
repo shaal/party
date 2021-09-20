a99bc36971667ee17fef7eb0ef22e01e8e3134ff
Yogi <yoginth@hey.com>
2021-09-20 16:18:42 +0530

    test: SSR

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,3 @@
 import Home from '@components/Home'
-import { HOME_FEED_QUERY } from '@components/Home/Feed'
-import { preloadQuery } from '@utils/apollo'
-import { authenticatedRoute } from '@utils/redirects'
-import { GetServerSidePropsContext } from 'next'
-
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const auth = await authenticatedRoute(context)
-  if ('redirect' in auth) return auth
-
-  return preloadQuery(context, {
-    query: HOME_FEED_QUERY,
-    variables: { after: null, type: 'ALL' }
-  })
-}
 
 export default Home
