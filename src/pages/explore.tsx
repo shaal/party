728e25331bb62e4a5fd5230f1192eeed2e2d1071
Yogi <yoginth@hey.com>
2021-09-20 16:37:04 +0530

    test: SSR

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,3 +1,15 @@
 import Explore from '@components/Explore'
+import { EXPLORE_FEED_QUERY } from '@components/Explore/Feed'
+import { preloadQuery } from '@utils/apollo'
+import { GetServerSidePropsContext } from 'next'
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  return preloadQuery(context, {
+    query: EXPLORE_FEED_QUERY,
+    variables: { after: null }
+  })
+}
 
 export default Explore
--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,3 +1,17 @@
-import Notifications from '@components/Notification'
+import Notifications, {
+  NOTIFICATIONS_QUERY as query
+} from '@components/Notification'
+import { preloadQuery } from '@utils/apollo'
+import { authenticatedRoute } from '@utils/redirects'
+import { GetServerSidePropsContext } from 'next'
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const auth = await authenticatedRoute(context)
+  if ('redirect' in auth) return auth
+
+  return preloadQuery(context, { query })
+}
 
 export default Notifications
