3fa005addafffd3ba506b6cad7e93314bde94f68
Yogi <yoginth@hey.com>
2021-09-22 15:32:09 +0530

    add: Spinner while checking the task

--- src/components/Post/SinglePost/Type/Task.tsx
+++ src/components/Post/SinglePost/Type/Task.tsx
@@ -2,6 +2,7 @@ import 'linkify-plugin-hashtag'
 import 'linkify-plugin-mention'
 
 import { gql, useMutation } from '@apollo/client'
+import { Spinner } from '@components/ui/Spinner'
 import { TaskCheckbox } from '@components/ui/TaskCheckbox'
 import { linkifyOptions } from '@components/utils/linkifyOptions'
 import Linkify from 'linkify-react'
@@ -20,7 +21,10 @@ interface Props {
 }
 
 const TaskType: React.FC<Props> = ({ task }) => {
-  const [editPost] = useMutation<EditPostMutation, EditPostMutationVariables>(
+  const [editPost, { loading }] = useMutation<
+    EditPostMutation,
+    EditPostMutationVariables
+  >(
     gql`
       mutation EditPostMutation($input: EditPostInput!) {
         editPost(input: $input) {
@@ -47,7 +51,11 @@ const TaskType: React.FC<Props> = ({ task }) => {
   return (
     <div className="linkify space-y-3">
       <div className="flex items-center gap-2.5">
-        <TaskCheckbox checked={task?.done} onChange={toggleTaskStatus} />
+        {loading ? (
+          <Spinner size="sm" className="mr-0.5" />
+        ) : (
+          <TaskCheckbox checked={task?.done} onChange={toggleTaskStatus} />
+        )}
         <Linkify tagName="div" options={linkifyOptions}>
           {task?.body}
         </Linkify>
