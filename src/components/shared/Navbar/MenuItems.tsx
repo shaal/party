639f0dd081eb81c1d5d1faf36eb47485e91a77ec
Yogi <yoginth@hey.com>
2021-09-23 21:02:21 +0530

    add: messages to menu

--- src/components/shared/Navbar/MenuItems.tsx
+++ src/components/shared/Navbar/MenuItems.tsx
@@ -90,21 +90,23 @@ const MenuItems: React.FC<Props> = ({ currentUser }) => {
                   <div>Your Profile</div>
                 </div>
               </Menu.Item>
-              <Menu.Item
-                as={NextLink}
-                href="/messages"
-                className={({ active }: any) =>
-                  clsx(
-                    { 'bg-gray-100 dark:bg-gray-800': active },
-                    'block px-4 py-1.5 text-sm text-gray-700 dark:text-gray-200 m-2 rounded-lg cursor-pointer'
-                  )
-                }
-              >
-                <div className="flex items-center space-x-1.5">
-                  <ChatAlt2Icon className="h-4 w-4" />
-                  <div>Messages</div>
-                </div>
-              </Menu.Item>
+              {staffMode && (
+                <Menu.Item
+                  as={NextLink}
+                  href="/messages"
+                  className={({ active }: any) =>
+                    clsx(
+                      { 'bg-gray-100 dark:bg-gray-800': active },
+                      'block px-4 py-1.5 text-sm text-gray-700 dark:text-gray-200 m-2 rounded-lg cursor-pointer'
+                    )
+                  }
+                >
+                  <div className="flex items-center space-x-1.5">
+                    <ChatAlt2Icon className="h-4 w-4" />
+                    <div>Messages</div>
+                  </div>
+                </Menu.Item>
+              )}
               <Menu.Item
                 as={NextLink}
                 href="/settings/profile"
