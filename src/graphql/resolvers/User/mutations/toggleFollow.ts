90e8192efcdfeec5b95bdbd644c58fd19849ecfe
Yogi <yoginth@hey.com>
2021-09-23 10:28:30 +0530

    add: Basic error handling in toggleFollow

--- src/graphql/resolvers/User/mutations/toggleFollow.ts
+++ src/graphql/resolvers/User/mutations/toggleFollow.ts
@@ -5,35 +5,39 @@ import { hasFollowed } from '../queries/hasFollowed'
 import { User } from '.prisma/client'
 
 export const toggleFollow = async (currentUserId: string, userId: string) => {
-  // Unfollow
-  if (await hasFollowed(currentUserId, userId)) {
-    return await db.user.update({
+  try {
+    // Unfollow
+    if (await hasFollowed(currentUserId, userId)) {
+      return await db.user.update({
+        where: { id: userId },
+        data: {
+          followedBy: {
+            disconnect: {
+              id: currentUserId
+            }
+          }
+        }
+      })
+    }
+
+    // Follow
+    const user: User = await db.user.update({
       where: { id: userId },
       data: {
         followedBy: {
-          disconnect: {
+          connect: {
             id: currentUserId
           }
         }
       }
     })
-  }
 
-  // Follow
-  const user: User = await db.user.update({
-    where: { id: userId },
-    data: {
-      followedBy: {
-        connect: {
-          id: currentUserId
-        }
-      }
+    if (userId !== currentUserId) {
+      createNotification(currentUserId, userId, userId, 'FOLLOW')
     }
-  })
 
-  if (userId !== currentUserId) {
-    createNotification(currentUserId, userId, userId, 'FOLLOW')
+    return user
+  } catch (error) {
+    throw new Error('Something went wrong!')
   }
-
-  return user
 }
