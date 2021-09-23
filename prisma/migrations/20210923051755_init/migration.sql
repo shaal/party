b440375ee36ea304f296eca2859e87778479ce6c
Yogi <yoginth@hey.com>
2021-09-23 10:48:08 +0530

    add: logs table

--- prisma/migrations/20210923051755_init/migration.sql
+++ prisma/migrations/20210923051755_init/migration.sql
@@ -171,6 +171,18 @@ CREATE TABLE `integrations` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `logs` (
+    `id` VARCHAR(191) NOT NULL,
+    `action` ENUM('POSTLIKE') NOT NULL,
+    `entityId` VARCHAR(191),
+    `ipAddress` VARCHAR(191),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `_subscribed` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
