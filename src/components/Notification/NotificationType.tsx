dc566d84f0a589338c62ebd2e75034916ca0ce3c
Yogi <yoginth@hey.com>
2021-09-23 08:25:46 +0000

    fix: Notification type

--- src/components/Notification/NotificationType.tsx
+++ src/components/Notification/NotificationType.tsx
@@ -14,27 +14,27 @@ const NotificationType: React.FC<Props> = ({ isRead, setIsRead }) => {
         className={clsx(
           {
             'text-blue-500 bg-blue-100 dark:bg-opacity-20 bg-opacity-100':
-              isRead
+              !isRead
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-blue-500 hover:bg-blue-100 hover:text-blue-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setIsRead(true)}
+        onClick={() => setIsRead(false)}
       >
         <InboxIcon className="h-4 w-4" />
-        <div className={isRead ? '' : `hidden sm:block`}>Inbox</div>
+        <div className={!isRead ? '' : `hidden sm:block`}>Inbox</div>
       </button>
       <button
         className={clsx(
           {
             'text-green-500 bg-green-100 dark:bg-opacity-20 bg-opacity-100':
-              !isRead
+              isRead
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-green-500 hover:bg-green-100 hover:text-green-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setIsRead(false)}
+        onClick={() => setIsRead(true)}
       >
         <CheckCircleIcon className="h-4 w-4" />
-        <div className={!isRead ? '' : `hidden sm:block`}>Done</div>
+        <div className={isRead ? '' : `hidden sm:block`}>Done</div>
       </button>
     </div>
   )
