e7206b6262686436a80428fe35e59379cbc03861
Yogi <yoginth@hey.com>
2021-09-24 12:28:22 +0530

    use: cuid

--- src/components/User/Tips/index.tsx
+++ src/components/User/Tips/index.tsx
@@ -1,11 +1,14 @@
 import { gql, useQuery } from '@apollo/client'
+import Slug from '@components/shared/Slug'
 import { Button } from '@components/ui/Button'
+import { Modal } from '@components/ui/Modal'
 import { CashIcon } from '@heroicons/react/outline'
+import { useState } from 'react'
 import { User } from 'src/__generated__/schema.generated'
 
 import { UserTipsQuery } from './__generated__/index.generated'
 
-export const USER_TIPS_QUERY = gql`
+const USER_TIPS_QUERY = gql`
   query UserTipsQuery($username: String!) {
     user(username: $username) {
       id
@@ -27,6 +30,7 @@ interface Props {
 }
 
 const Tips: React.FC<Props> = ({ user }) => {
+  const [showModal, setShowModal] = useState<boolean>(false)
   const { data, loading } = useQuery<UserTipsQuery>(USER_TIPS_QUERY, {
     variables: { username: user?.username },
     skip: !user?.username
@@ -35,15 +39,34 @@ const Tips: React.FC<Props> = ({ user }) => {
   if (loading) return null
 
   return (
-    <>
+    <div>
       <Button
         className="text-sm"
         icon={<CashIcon className="h-4 w-4" />}
         outline
+        onClick={() => setShowModal(!showModal)}
       >
         Tip
       </Button>
-    </>
+      <Modal
+        onClose={() => setShowModal(!showModal)}
+        title={
+          <div className="flex items-center space-x-1.5">
+            <div>Send dev tips to</div>
+            <Slug slug={user?.username} prefix="@" />
+          </div>
+        }
+        show={showModal}
+      >
+        <div className="p-5">
+          {loading ? (
+            <div>Loading dev tips...</div>
+          ) : (
+            <div className="font-bold">TBD</div>
+          )}
+        </div>
+      </Modal>
+    </div>
   )
 }
 
