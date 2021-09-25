57823adc462e7fc9a21752d31fa5ff85babaa78c
Yogi <yoginth@hey.com>
2021-09-25 11:42:15 +0000

    add: cacheRequest to pages

--- src/pages/stafftools/users.tsx
+++ src/pages/stafftools/users.tsx
@@ -2,6 +2,7 @@ import StaffToolsUsers, {
   STAFF_TOOLS_USERS_QUERY as query
 } from '@components/StaffTools/Users'
 import { preloadQuery } from '@utils/apollo'
+import { cacheRequest } from '@utils/cache'
 import { staffRoute } from '@utils/redirects'
 import { GetServerSidePropsContext } from 'next'
 
@@ -9,9 +10,8 @@ export const getServerSideProps = async (
   context: GetServerSidePropsContext
 ) => {
   const staff = await staffRoute(context)
-  if ('redirect' in staff) {
-    return staff
-  }
+  if ('redirect' in staff) return staff
+  cacheRequest(context)
 
   return preloadQuery(context, { query })
 }
