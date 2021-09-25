bbadfed79a4ab591fe946ce374196b0bb4a3fbe9
Yogi <yoginth@hey.com>
2021-09-25 13:44:02 +0000

    add: imagekitURL

--- src/components/shared/UserProfileLarge.tsx
+++ src/components/shared/UserProfileLarge.tsx
@@ -1,6 +1,7 @@
 import { Tooltip } from '@components/ui/Tooltip'
 import Follow from '@components/User/Follow'
 import AppContext from '@components/utils/AppContext'
+import { imagekitURL } from '@components/utils/imagekitURL'
 import { BadgeCheckIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import React, { useContext } from 'react'
@@ -20,7 +21,7 @@ const UserProfileLarge: React.FC<Props> = ({ user, showFollow = false }) => {
     <div className="flex justify-between items-center space-x-5">
       <div className="flex space-x-3 items-center">
         <img
-          src={user?.profile?.avatar as string}
+          src={imagekitURL(user?.profile?.avatar as string, 100, 100)}
           className="h-14 w-14 rounded-full bg-gray-200"
           alt={`@/${user?.username}'s avatar`}
         />
