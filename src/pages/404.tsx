53bd6e6f0cdbbdf82736ede67bcc45c0f51f6e20
Yogi <yoginth@hey.com>
2021-09-25 05:04:13 +0000

    update: SEO

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import DevpartySEO from '@components/shared/SEO'
 import { Button } from '@components/ui/Button'
 import { HomeIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
@@ -5,7 +6,7 @@ import Link from 'next/link'
 export default function Custom404() {
   return (
     <>
-      <NextSeo title="Page Not Found" />
+      <DevpartySEO title="Page Not Found" />
       <div className="flex flex-col h-full justify-start pt-10 items-center">
         <img
           src="https://assets.devparty.io/images/gifs/nyan-cat.gif"
@@ -17,8 +18,11 @@ export default function Custom404() {
           <div className="mb-4">This page could not be found.</div>
           <Link href="/signup" passHref>
             <a>
-              <Button className="flex item-center mx-auto" size="lg">
-                <HomeIcon className="w-5 h-5 mr-2" />
+              <Button
+                className="flex item-center mx-auto"
+                size="lg"
+                icon={<HomeIcon className="w-4 h-4" />}
+              >
                 <div>Go to home</div>
               </Button>
             </a>
