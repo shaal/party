e482c6a3529f24416f0b05d3fee47f56da97a3a3
Yogi <yoginth@hey.com>
2021-09-26 12:35:48 +0530

    update: Home route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,7 @@
 import Landing from '@components/Landing'
-import { CURRENT_USER_QUERY } from '@components/SiteLayout'
-import { preloadQuery } from '@utils/apollo'
 import { unauthenticatedRoute } from '@utils/redirects'
-import { GetServerSidePropsContext } from 'next'
+import { GetServerSideProps } from 'next'
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const auth = await unauthenticatedRoute(context)
-  if ('redirect' in auth) return auth
-
-  return preloadQuery(context, { query: CURRENT_USER_QUERY })
-}
+export const getServerSideProps: GetServerSideProps = unauthenticatedRoute
 
 export default Landing
