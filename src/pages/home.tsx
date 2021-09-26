aee9d94e9be5552cfe0df90955ee091ce6805344
Yogi <yoginth@hey.com>
2021-09-26 12:31:43 +0530

    remove: cacheRequest for home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,6 @@
 import Home from '@components/Home'
 import { HOME_FEED_QUERY } from '@components/Home/Feed'
 import { preloadQuery } from '@utils/apollo'
-import { cacheRequest } from '@utils/cache'
 import { authenticatedRoute } from '@utils/redirects'
 import { GetServerSidePropsContext } from 'next'
 
@@ -10,7 +9,6 @@ export const getServerSideProps = async (
 ) => {
   const auth = await authenticatedRoute(context)
   if ('redirect' in auth) return auth
-  cacheRequest(context)
 
   return preloadQuery(context, {
     query: HOME_FEED_QUERY,
