31ccabc8e65b8be45f5c21e785259608b2bf13ec
Yogi <yoginth@hey.com>
2021-09-26 20:07:38 +0530

    remove: Preact

--- next.config.js
+++ next.config.js
@@ -3,17 +3,6 @@ module.exports = {
   poweredByHeader: false,
   generateEtags: false,
   experimental: { esmExternals: true },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom': 'preact/compat'
-      })
-    }
-
-    return config
-  },
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     BASE_URL: process.env.BASE_URL,
