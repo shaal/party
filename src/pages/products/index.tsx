6fc9aa9b3cd336e28771f894b4920eea4d46ffc2
Yogi <yoginth@hey.com>
2021-09-26 14:41:47 +0530

    remove: SSR

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,13 +1,3 @@
-import Products, { PRODUCTS_QUERY as query } from '@components/Product/Products'
-import { preloadQuery } from '@utils/apollo'
-import { cacheRequest } from '@utils/cache'
-import { GetServerSidePropsContext } from 'next'
-
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  cacheRequest(context)
-  return preloadQuery(context, { query })
-}
+import Products from '@components/Product/Products'
 
 export default Products
