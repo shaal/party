71f37637400b0ec430e61deecbc3b50729aa6dd6
Yogi <yoginth@hey.com>
2021-09-26 17:30:42 +0530

    add: authenticatedRoute for home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,7 @@
 import Home from '@components/Home'
+import { authenticatedRoute } from '@utils/redirects'
+import { GetServerSideProps } from 'next'
+
+export const getServerSideProps: GetServerSideProps = authenticatedRoute
 
 export default Home
