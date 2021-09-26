cbdd415210d1209d16693dc45f80640a570a0b0d
Yogi <yoginth@hey.com>
2021-09-26 17:37:25 +0530

    add: Preact

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ module.exports = {
   poweredByHeader: false,
   generateEtags: false,
   experimental: { esmExternals: true },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat'
+      })
+    }
+
+    return config
+  },
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     BASE_URL: process.env.BASE_URL,
