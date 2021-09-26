a542889f8510a66d6742c04e33c21d0f943e232f
Yogi <yoginth@hey.com>
2021-09-26 14:32:33 +0530

    remove: SSR for home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,3 @@
 import Home from '@components/Home'
-import { HOME_FEED_QUERY } from '@components/Home/Feed'
-import { preloadQuery } from '@utils/apollo'
-import { cacheRequest } from '@utils/cache'
-import { authenticatedRoute } from '@utils/redirects'
-import { GetServerSidePropsContext } from 'next'
-
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const auth = await authenticatedRoute(context)
-  if ('redirect' in auth) return auth
-  cacheRequest(context)
-
-  return preloadQuery(context, {
-    query: HOME_FEED_QUERY,
-    variables: { after: null, type: 'ALL' }
-  })
-}
 
 export default Home
