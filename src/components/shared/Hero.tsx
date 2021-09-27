b733abeb6800584c7589286e9f76ce19f4ff066f
Yogi <yoginth@hey.com>
2021-09-27 20:53:24 +0530

    update: patterns url

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -3,7 +3,7 @@ const Hero: React.FC = () => {
     <div
       className="lg:col-span-7 md:col-span-12 col-span-12 bg-brand-600 dark:bg-brand-700 md:block hidden"
       style={{
-        backgroundImage: `url('https://assets.devparty.io/images/heropatterns/topography.svg')`
+        backgroundImage: `url('https://assets.devparty.io/images/patterns/topography.svg')`
       }}
     ></div>
   )
