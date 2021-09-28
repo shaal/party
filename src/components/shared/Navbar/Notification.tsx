a5b5e24d23721ad665a59e4123c5fbf22cdad0e7
Yogi <yoginth@hey.com>
2021-09-28 03:12:27 +0000

    remove: Lucide

--- src/components/shared/Navbar/Notification.tsx
+++ src/components/shared/Navbar/Notification.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from '@apollo/client'
-import { Zap } from 'lucide-react'
+import { LightningBoltIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 
 import { NotificationCountQuery } from './__generated__/Notification.generated'
@@ -18,7 +18,7 @@ const Notification: React.FC = () => {
   return (
     <Link href="/notifications">
       <a className="flex items-start">
-        <Zap size={22} />
+        <LightningBoltIcon className="h-6 w-6" />
         {(data?.me?.notificationsCount as number) > 0 && (
           <div className="h-2 w-2 rounded-full bg-red-500" />
         )}
