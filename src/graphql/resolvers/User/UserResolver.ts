0130731bf439523dd701127b2a0d69312d077c6b
Yogi <yoginth@hey.com>
2021-09-30 17:58:38 +0530

    add: resolve integrations in user profile

--- src/graphql/resolvers/User/UserResolver.ts
+++ src/graphql/resolvers/User/UserResolver.ts
@@ -42,6 +42,7 @@ builder.prismaObject('User', {
     // Relations
     profile: t.relation('profile'),
     tip: t.relation('tip', { nullable: true }),
+    integrations: t.relation('integrations', { nullable: true }),
     badges: t.relatedConnection('badges', { cursor: 'id', totalCount: true }),
     topics: t.relatedConnection('topics', { cursor: 'id', totalCount: true }),
     posts: t.relatedConnection('posts', {
