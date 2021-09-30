fe5e1b8bb744e56247cd3fbe90287a050a1ab47f
Yogi <yoginth@hey.com>
2021-09-30 02:20:20 +0000

    skip: config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6'
-export const nftaddress = '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318'
+export const nftmarketaddress = '0x610178dA211FEF7D417bC0e6FeD39F05609AD788'
+export const nftaddress = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e'
