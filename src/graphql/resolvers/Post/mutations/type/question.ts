52a56881ab06ba13f77bff1777fb3a42dd9577f3
Yogi <yoginth@hey.com>
2021-09-30 12:25:27 +0530

    update: Split into multiple types for create post

---
+++ src/graphql/resolvers/Post/mutations/type/question.ts
@@ -0,0 +1,37 @@
+import { getMentions } from '@graphql/utils/getMentions'
+import { getTopics } from '@graphql/utils/getTopics'
+import { parseTopics } from '@graphql/utils/parseTopics'
+import { Session } from '@prisma/client'
+import { db } from '@utils/prisma'
+import { CreatePostInput } from 'src/__generated__/schema.generated'
+
+import { processMentions } from '../processMentions'
+
+export const question = async (
+  query: any,
+  input: CreatePostInput,
+  session: Session | null | undefined,
+  parentId: string | null | undefined
+) => {
+  const question = await db.post.create({
+    ...query,
+    data: {
+      userId: session!.userId,
+      title: input.title,
+      body: input.body,
+      attachments: input.attachments ? input.attachments : undefined,
+      type: 'QUESTION',
+      productId: input.productId ? input.productId : null,
+      topics: {
+        create: parseTopics(getTopics(input.body))
+      },
+      parentId
+    }
+  })
+
+  if (getMentions(question.body)?.length > 0) {
+    await processMentions(question, session)
+  }
+
+  return question
+}
---
+++ src/graphql/resolvers/Post/mutations/type/reply.ts
@@ -0,0 +1,47 @@
+import { createNotification } from '@graphql/resolvers/Notification/mutations/createNotification'
+import { getMentions } from '@graphql/utils/getMentions'
+import { getTopics } from '@graphql/utils/getTopics'
+import { parseTopics } from '@graphql/utils/parseTopics'
+import { PostType, Session } from '@prisma/client'
+import { db } from '@utils/prisma'
+import { CreatePostInput } from 'src/__generated__/schema.generated'
+
+import { processMentions } from '../processMentions'
+
+export const reply = async (
+  query: any,
+  input: CreatePostInput,
+  session: Session | null | undefined,
+  parentId: string | null | undefined
+) => {
+  const reply = await db.post.create({
+    ...query,
+    data: {
+      userId: session!.userId,
+      title: input.title,
+      body: input.body,
+      done: input.done,
+      attachments: input.attachments ? input.attachments : undefined,
+      type: input.type as PostType,
+      productId: input.productId ? input.productId : null,
+      topics: {
+        create: parseTopics(getTopics(input.body))
+      },
+      parentId
+    }
+  })
+
+  const parent = await db.post.findUnique({
+    where: { id: reply?.parentId! }
+  })
+
+  if (getMentions(reply.body)?.length > 0) {
+    await processMentions(reply, session)
+  }
+
+  if (session!.userId !== parent?.userId) {
+    createNotification(session!.userId, parent?.userId, reply?.id, 'POST_REPLY')
+  }
+
+  return reply
+}
