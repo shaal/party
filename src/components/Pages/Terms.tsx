ec02629e2643b1e472946143e92dbc47555bd390
Yogi <yoginth@hey.com>
2021-10-01 11:50:20 +0000

    update: Terms

--- src/components/Pages/Terms.tsx
+++ src/components/Pages/Terms.tsx
@@ -43,25 +43,113 @@ const Terms: React.FC = () => {
                 1. Agreement to these Terms
               </h2>
               <p>
-                By using the Services, you agree to be bound by these Terms. If
-                you don’t agree to these Terms, do not use the Services. If you
-                are accessing and using the Services on behalf of a company
-                (such as your employer) or other legal entity, you represent and
-                warrant that you have the authority to bind that company or
-                other legal entity to these Terms. In that case, "you" and
-                "your" will refer to that company or other legal entity.
+                By accessing this Website, accessible from devparty.io and/or
+                www.devparty.io, you are agreeing to be bound by these Website
+                Terms and Conditions of Use and agree that you are responsible
+                for the agreement with any applicable local laws. If you
+                disagree with any of these terms, you are prohibited from
+                accessing this site. The materials contained in this Website are
+                protected by copyright and trademark law.
+              </p>
+              <h2 className="text-black dark:text-white">2. Use License</h2>
+              <p>
+                Permission is granted to temporarily download one copy of the
+                materials on Devparty's Website for personal, non-commercial
+                transitory viewing only. This is the grant of a license, not a
+                transfer of title, and under this license, you may not:
+              </p>
+              <ul>
+                <li>modify or copy the materials;</li>
+                <li>
+                  use the materials for any commercial purpose or any public
+                  display;
+                </li>
+                <li>
+                  attempt to reverse engineer any software contained on
+                  Devparty's Website or Devparty app itself;
+                </li>
+                <li>
+                  remove any copyright or other proprietary notations from the
+                  materials; or
+                </li>
+                <li>
+                  transferring the materials to another person or "mirror" the
+                  materials on any other server.
+                </li>
+              </ul>
+              <p>
+                This will let Devparty terminate upon violations of any of these
+                restrictions. Upon termination, your viewing right will also be
+                terminated and you should destroy any downloaded materials in
+                your possession whether it is printed or electronic format.
+                These Terms of Service have been created by the sole developers
+                and maintainers of this project.
+              </p>
+              <h2 className="text-black dark:text-white">3. Disclaimer</h2>
+              <p>
+                All the materials on Devparty's Website are provided "as is".
+                Devparty makes no warranties, may it be expressed or implied,
+                therefore negates all other warranties. Furthermore, Devparty
+                does not make any representations concerning the accuracy or
+                reliability of the use of the materials on its Website or
+                otherwise relating to such materials or any sites linked to this
+                Website.
+              </p>
+              <h2 className="text-black dark:text-white">4. Limitations</h2>
+              <p>
+                Devparty or its suppliers will not be held accountable for any
+                damages that will arise with the use or inability to use the
+                materials on Devparty's Website, even if Devparty or an
+                authorized representative of this Website has been notified,
+                orally or written, of the possibility of such damage. Some
+                jurisdiction does not allow limitations on implied warranties or
+                limitations of liability for incidental damages, these
+                limitations may not apply to you.
               </p>
               <h2 className="text-black dark:text-white">
-                2. Changes to the Terms or Services
+                5. Revisions and Errata
               </h2>
               <p>
-                We may modify the Terms at any time, at our sole discretion. If
-                we do so, we will let you know either by posting the modified
-                Terms on the Site or through other communications. If you
-                continue to use the Services after such a change, you are
-                indicating that you agree to the modified Terms. We may also
-                change or discontinue all or any part of the Services, at any
-                time and without notice, at our sole discretion.
+                The materials appearing on Devparty's Website may include
+                technical, typographical, or photographic errors. Devparty will
+                not promise that any of the materials on this Website are
+                accurate, complete, or current. Devparty may change the
+                materials contained on its Website at any time without notice.
+                Devparty does not make any commitment to update the materials.
+              </p>
+              <h2 className="text-black dark:text-white">6. Links</h2>
+              <p>
+                Devparty has not reviewed all of the sites linked to its Website
+                and is not responsible for the contents of any such linked site.
+                The presence of any link does not imply endorsement by Devparty
+                of the site. The use of any linked website is at the user's own
+                risk.
+              </p>
+              <h2 className="text-black dark:text-white">
+                7. Site Terms of Use Modifications
+              </h2>
+              <p>
+                Devparty may revise these Terms of Use for its Website at any
+                time without prior notice. By using this Website, you are
+                agreeing to be bound by the current version of these Terms and
+                Conditions of Use.
+              </p>
+              <h2 className="text-black dark:text-white">8. Your Privacy</h2>
+              <p>
+                Please check our{' '}
+                <a
+                  href="https://devparty.io/privacy"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Privacy Policy
+                </a>{' '}
+                for information about how we take care and protect your privacy.
+              </p>
+              <h2 className="text-black dark:text-white">9. Governing Law</h2>
+              <p>
+                Any claim related to Devparty's Website shall be governed by the
+                laws of in without regards to its conflict of law provisions.
               </p>
             </div>
           </div>
