16827947230570698182d06e17e1c2cce5dfaf4d
Yogi <me@yogi.codes>
2021-10-03 19:55:15 +0530

    add: jsdoc for cacheRequest

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,5 +1,10 @@
 import { GetServerSidePropsContext } from 'next'
 
+/**
+ * Add cache requests to the HTTP response
+ * @param context - Next.js server side props context
+ * @returns add cache header to the requests
+ */
 export async function cacheRequest(context: GetServerSidePropsContext) {
   return context.res.setHeader(
     'Cache-Control',
