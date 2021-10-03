57d27edcd32283032933c45effbc59179cc58ae4
Yogi <me@yogi.codes>
2021-10-03 11:31:58 +0000

    fix: IPFS

--- src/components/utils/uploadToIPFS.ts
+++ src/components/utils/uploadToIPFS.ts
@@ -1,19 +1,17 @@
-import { create } from 'ipfs-http-client'
-
 export async function uploadToIPFS(data: any) {
-  const ipfs = create({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https'
+  const formData = new FormData()
+  formData.append('file', data, 'img')
+  const upload = await fetch('https://ipfs.infura.io:5001/api/v0/add', {
+    method: 'post',
+    body: formData
   })
-
-  const { path }: { path: string } = await ipfs.add(data)
+  const response = await upload.json()
 
   return {
     type: data.type,
     url:
       data.type === 'video/mp4'
-        ? `https://ipfs.io/ipfs/${path}`
-        : `https://cloudflare-ipfs.com/ipfs/${path}`
+        ? `https://ipfs.io/ipfs/${response.Hash}`
+        : `https://cloudflare-ipfs.com/ipfs/${response.Hash}`
   }
 }
