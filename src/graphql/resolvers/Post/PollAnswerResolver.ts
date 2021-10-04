73aa6c2039114d886a5c5eed7c9ab1f2c3111666
Yogi <me@yogi.codes>
2021-10-04 19:38:15 +0530

    add: hasVoted

--- src/graphql/resolvers/Post/PollAnswerResolver.ts
+++ src/graphql/resolvers/Post/PollAnswerResolver.ts
@@ -1,12 +1,20 @@
 import { builder } from '@graphql/builder'
 
 import { answerPoll } from './mutations/answerPoll'
+import { hasVoted } from './queries/hasVoted'
 
 builder.prismaObject('PollAnswer', {
   findUnique: (answer) => ({ id: answer.id }),
   fields: (t) => ({
     id: t.exposeID('id'),
     title: t.exposeString('title'),
+    hasVoted: t.field({
+      type: 'Boolean',
+      resolve: async (parent, args, { session }) => {
+        if (!session) return false
+        return await hasVoted(session?.userId as string, parent.id)
+      }
+    }),
     voters: t.relatedConnection('voters', { cursor: 'id', totalCount: true }),
 
     // Relations
