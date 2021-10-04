6d52c8c1fccce6843b1c09998f1bb156bb229e7a
Yogi <me@yogi.codes>
2021-10-04 10:26:01 +0000

    update: migs

--- prisma/migrations/20211004102554_init/migration.sql
+++ prisma/migrations/20211004102554_init/migration.sql
@@ -124,6 +124,7 @@ CREATE TABLE `poll` (
 -- CreateTable
 CREATE TABLE `poll_answers` (
     `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
     `pollId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
