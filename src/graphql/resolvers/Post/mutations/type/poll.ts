e24aa3629afb13553f0e610a74dc17c043f5c082
Yogi <me@yogi.codes>
2021-10-04 05:19:24 +0000

    add: Poll resolver

---
+++ src/graphql/resolvers/Post/mutations/type/poll.ts
@@ -0,0 +1,33 @@
+import { getMentions } from '@graphql/utils/getMentions'
+import { getTopics } from '@graphql/utils/getTopics'
+import { parseTopics } from '@graphql/utils/parseTopics'
+import { Session } from '@prisma/client'
+import { db } from '@utils/prisma'
+import { CreatePostInput } from 'src/__generated__/schema.generated'
+
+import { processMentions } from '../processMentions'
+
+export const poll = async (
+  query: any,
+  input: CreatePostInput,
+  session: Session | null | undefined
+) => {
+  const poll = await db.post.create({
+    ...query,
+    data: {
+      userId: session!.userId,
+      body: input.body,
+      type: 'POLL',
+      productId: input.productId ? input.productId : null,
+      topics: {
+        create: parseTopics(getTopics(input.body))
+      }
+    }
+  })
+
+  if (getMentions(poll.body)?.length > 0) {
+    await processMentions(poll, session)
+  }
+
+  return poll
+}
