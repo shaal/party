db48e7f0ec399206f3eeb974df9c4d2da73026e7
Yogi <me@yogi.codes>
2021-10-05 18:23:41 +0530

    add: Web3 auth

--- src/components/Post/NewPost/Type/NFT.tsx
+++ src/components/Post/NewPost/Type/NFT.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { Button } from '@components/ui/Button'
+import React, { useState } from 'react'
+import Web3 from 'web3'
+import Web3Modal from 'web3modal'
 
 const NFTType: React.FC = () => {
-  return <div>Hello</div>
+  const [ethAddress, setEthAddress] = useState<string>()
+
+  const connectWallet = async () => {
+    const web3Modal = new Web3Modal()
+    const connection = await web3Modal.connect()
+    const web3 = new Web3(connection)
+
+    // @ts-ignore
+    setEthAddress(web3?.currentProvider?.selectedAddress)
+  }
+
+  return (
+    <div className="space-y-2">
+      <div>
+        Put some text here! Put some text here! Put some text here! Put some
+        text here! Put some text here! Put some text here! Put some text here!
+        Put some text here! Put some text here!
+      </div>
+      {!ethAddress && (
+        <Button type="button" onClick={connectWallet}>
+          Load NFTs
+        </Button>
+      )}
+    </div>
+  )
 }
 
 export default NFTType
