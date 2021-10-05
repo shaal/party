443da43dc65131056b0f8047154f74688290e7e4
Yogi <me@yogi.codes>
2021-10-05 18:50:38 +0530

    add: createNFT query

--- src/components/Post/NewPost/Type/NFT.tsx
+++ src/components/Post/NewPost/Type/NFT.tsx
@@ -1,19 +1,46 @@
+import { gql, useMutation } from '@apollo/client'
 import { Button } from '@components/ui/Button'
 import { Card, CardBody } from '@components/ui/Card'
+import { useRouter } from 'next/router'
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 import useSWR from 'swr'
 import Web3 from 'web3'
 import Web3Modal from 'web3modal'
 
+import {
+  CreateNftMutation,
+  CreateNftMutationVariables
+} from './__generated__/NFT.generated'
+
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 const NFTType: React.FC = () => {
+  const router = useRouter()
   const [ethAddress, setEthAddress] = useState<string>()
-  const { data, error } = useSWR(
+  const { data } = useSWR(
     `https://${
       process.env.NODE_ENV === 'production' ? 'testnets-api' : 'testnets-api'
     }.opensea.io/api/v1/assets?format=json&limit=9&offset=0&order_direction=desc&owner=${ethAddress}`,
     fetcher
+  )
+  const [createNFT] = useMutation<
+    CreateNftMutation,
+    CreateNftMutationVariables
+  >(
+    gql`
+      mutation CreateNFTMutation($input: CreatePostInput!) {
+        createPost(input: $input) {
+          id
+        }
+      }
+    `,
+    {
+      onCompleted(data) {
+        toast.success('NFT has been posted successfully!')
+        router.push(`/posts/${data?.createPost?.id}`)
+      }
+    }
   )
 
   const connectWallet = async () => {
@@ -34,7 +61,17 @@ const NFTType: React.FC = () => {
               <div key={asset?.id}>
                 <div
                   className="cursor-pointer"
-                  onClick={() => alert(asset?.token_id)}
+                  onClick={() =>
+                    createNFT({
+                      variables: {
+                        input: {
+                          body: asset?.name,
+                          address: asset?.asset_contract?.address,
+                          tokenId: asset?.token_id
+                        }
+                      }
+                    })
+                  }
                 >
                   <img
                     className="object-cover h-38 w-38 rounded-lg border"
