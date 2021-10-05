d3fc1b6520c721571ccad72f940da60e4f9f7159
Yogi <me@yogi.codes>
2021-10-05 02:48:22 +0000

    update: Git commit

--- src/components/Post/SinglePost/Type/Commit.tsx
+++ src/components/Post/SinglePost/Type/Commit.tsx
@@ -1,6 +1,14 @@
 import 'linkify-plugin-hashtag'
 import 'linkify-plugin-mention'
 
+import { Tooltip } from '@components/ui/Tooltip'
+import { imagekitURL } from '@components/utils/imagekitURL'
+import { useGitCommit } from '@components/utils/useGitCommit'
+import {
+  DocumentAddIcon,
+  DocumentIcon,
+  ShieldCheckIcon
+} from '@heroicons/react/outline'
 import React from 'react'
 import { Post } from 'src/__generated__/schema.generated'
 
@@ -9,19 +17,21 @@ interface Props {
 }
 
 const CommitType: React.FC<Props> = ({ post }) => {
-  // if (loading) return <div>Loading Commit...</div>
+  const { commit, slug, isLoading, isError } = useGitCommit(post?.body)
+
+  if (isLoading) return <div>Loading Commit...</div>
 
   return (
     <div className="space-y-2">
-      {/* <div className="linkify">
+      <div className="linkify">
         <a
           className="flex items-center space-x-2"
-          href={commit?.url as string}
+          href={commit?.html_url as string}
           target="_blank"
           rel="noreferrer"
         >
-          <div className="text-lg font-bold">{commit?.message}</div>
-          {commit?.verified && (
+          <div className="text-lg font-bold">{commit?.commit?.message}</div>
+          {commit?.commit?.verification?.verified && (
             <Tooltip content="This commit was signed by the committer">
               <div className="text-xs text-green-500 border border-green-500 rounded-full px-1.5 font-light flex items-center space-x-0.5">
                 <ShieldCheckIcon className="h-3 w-4" />
@@ -34,10 +44,10 @@ const CommitType: React.FC<Props> = ({ post }) => {
       <div>
         <a
           className="flex items-center space-x-1 text-sm text-gray-600 dark:text-gray-300"
-          href={`https://github.com/${commit?.repoSlug}`}
+          href={`https://github.com/${slug}`}
         >
           <DocumentAddIcon className="h-4 w-4" />
-          <div>{commit?.repoSlug}</div>
+          <div>{slug}</div>
         </a>
       </div>
       <div className="pt-2">
@@ -49,41 +59,41 @@ const CommitType: React.FC<Props> = ({ post }) => {
                 <DocumentIcon className="h-4 w-4" />
                 <div className="flex items-center space-x-1">
                   <div className="font-bold">
-                    {commit?.changed}{' '}
-                    {(commit?.changed as number) > 1 ? 'files' : 'file'}
+                    {commit?.files?.length}{' '}
+                    {(commit?.files?.length as number) > 1 ? 'files' : 'file'}
                   </div>
                   <div>changed</div>
                 </div>
               </div>
             </div>
             <div className="flex items-center space-x-3">
-              <div className="text-green-500">+{commit?.additions}</div>
-              <div className="text-red-500">-{commit?.deletions}</div>
+              <div className="text-green-500">+{commit?.stats?.additions}</div>
+              <div className="text-red-500">-{commit?.stats?.deletions}</div>
             </div>
           </div>
           <div className="flex items-center space-x-1.5">
             <div>
               <img
                 className="h-5 w-5 rounded-md"
-                src={imagekitURL(commit?.authorAvatar as string, 50, 50)}
-                alt={`@${commit?.authorUsername}'s avatar'`}
+                src={imagekitURL(commit?.author?.avatar_url as string, 50, 50)}
+                alt={`@${commit?.author?.login}'s avatar'`}
               />
             </div>
             <div className="flex items-center space-x-1">
               <a
                 className="font-bold"
-                href={`https://github.com/${commit?.authorUsername}`}
+                href={`https://github.com/${commit?.author?.login}`}
                 target="_blank"
                 rel="noreferrer"
               >
-                {commit?.authorUsername}
+                {commit?.author?.login}
               </a>
               <div>authored</div>
             </div>
           </div>
         </div>
         <div className="border-t" />
-      </div> */}
+      </div>
     </div>
   )
 }
