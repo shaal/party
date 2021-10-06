2a3ac7d55a7146be0061099c58a61b3783b26e5d
Yogi <me@yogi.codes>
2021-10-06 06:34:43 +0000

    add: jsdoc to getBadges

--- src/graphql/resolvers/Badge/queries/getBadges.ts
+++ src/graphql/resolvers/Badge/queries/getBadges.ts
@@ -1,5 +1,10 @@
 import { db } from '@utils/prisma'
 
+/**
+ * Return all badges
+ * @param query - Contains an include object to pre-load data needed to resolve nested parts.
+ * @returns all badges
+ */
 export const getBadges = async (query: any) => {
   return await db.badge.findMany({
     ...query,
