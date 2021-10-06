70d165c8bcb60c339e0e4c5e5a20444071210b0d
Yogi <me@yogi.codes>
2021-10-06 03:55:35 +0000

    add: STATIC_ASSETS

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 import React from 'react'
 
+export const STATIC_ASSETS = 'https://assets.devparty.io/images'
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
 export const BASE_URL = process.env.BASE_URL
@@ -7,5 +8,4 @@ export const REACT_VERSION = React.version
 export const GIT_COMMIT_SHA = process.env.GIT_COMMIT_SHA?.slice(0, 7)
 export const GIT_COMMIT_REF = process.env.GIT_COMMIT_REF
 export const POLLING_INTERVAL = 1000 * 60 // 1 minute
-
 export const ERROR_MESSAGE = 'Something went wrong!'
