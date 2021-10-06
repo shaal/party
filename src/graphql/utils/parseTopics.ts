3b46b4222445024b7dc6c2d80c8e6934bd4806b2
Yogi <me@yogi.codes>
2021-10-06 06:31:54 +0000

    add: jsdoc to parseTopics

--- src/graphql/utils/parseTopics.ts
+++ src/graphql/utils/parseTopics.ts
@@ -1,3 +1,8 @@
+/**
+ * Generated the prisma compatible parsed data to inserted
+ * @param topics - Array of topics generated in getTopics()
+ * @returns prisma compatible parsed data to inserted
+ */
 export const parseTopics = (topics: any) => {
   if (topics) {
     return topics.map((topic: string) => ({
