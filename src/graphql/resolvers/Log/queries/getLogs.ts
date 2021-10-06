3c72570dcb9b7d69c6ba0f272871d5e08b0b7878
Yogi <me@yogi.codes>
2021-10-06 06:43:39 +0000

    add: jsdoc to getLogs

--- src/graphql/resolvers/Log/queries/getLogs.ts
+++ src/graphql/resolvers/Log/queries/getLogs.ts
@@ -1,6 +1,12 @@
 import { Session } from '@prisma/client'
 import { db } from '@utils/prisma'
 
+/**
+ * Get logs for the current user
+ * @param query - Contains an include object to pre-load data needed to resolve nested parts.
+ * @param session - Current user session
+ * @returns the logs for the current user
+ */
 export const getLogs = async (
   query: any,
   session: Session | null | undefined
