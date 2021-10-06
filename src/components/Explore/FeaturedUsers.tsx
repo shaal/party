f9b9fe6ae798a1c08a30ff1cb0740e4f270b9534
Yogi <me@yogi.codes>
2021-10-06 05:09:04 +0000

    add: Featured users in explore page

---
+++ src/components/Explore/FeaturedUsers.tsx
@@ -0,0 +1,93 @@
+import { gql, useQuery } from '@apollo/client'
+import UserProfileShimmer from '@components/shared/Shimmer/UserProfileShimmer'
+import UserProfile from '@components/shared/UserProfile'
+import { Card, CardBody } from '@components/ui/Card'
+import { ErrorMessage } from '@components/ui/ErrorMessage'
+import { UsersIcon } from '@heroicons/react/outline'
+import { RefreshIcon, SparklesIcon } from '@heroicons/react/solid'
+import React from 'react'
+
+import { FeaturedUsersQuery } from './__generated__/FeaturedUsers.generated'
+
+const FEATURED_USERS_QUERY = gql`
+  query FeaturedUsersQuery {
+    featuredUsers {
+      edges {
+        node {
+          id
+          username
+          hasFollowed
+          isVerified
+          profile {
+            id
+            avatar
+            name
+          }
+        }
+      }
+    }
+  }
+`
+
+const FeaturedUsersCard = ({ children, refetch }: any) => {
+  return (
+    <div className="mb-4">
+      <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center gap-2">
+          <SparklesIcon className="h-4 w-4 text-yellow-500" />
+          <div>Featured users</div>
+        </div>
+        <button onClick={() => refetch()}>
+          <RefreshIcon className="h-5 w-5" />
+        </button>
+      </div>
+      <Card>
+        <CardBody>{children}</CardBody>
+      </Card>
+    </div>
+  )
+}
+
+const FeaturedUsers: React.FC = () => {
+  const { data, loading, error, refetch } = useQuery<FeaturedUsersQuery>(
+    FEATURED_USERS_QUERY,
+    {
+      notifyOnNetworkStatusChange: true
+    }
+  )
+  const users = data?.featuredUsers?.edges?.map((edge) => edge?.node)
+
+  if (loading)
+    return (
+      <FeaturedUsersCard>
+        <div className="space-y-4">
+          <UserProfileShimmer showFollow />
+          <UserProfileShimmer showFollow />
+          <UserProfileShimmer showFollow />
+          <UserProfileShimmer showFollow />
+          <UserProfileShimmer showFollow />
+        </div>
+      </FeaturedUsersCard>
+    )
+
+  return (
+    <FeaturedUsersCard refetch={refetch}>
+      <ErrorMessage title="Failed to load users" error={error} />
+      <div className="space-y-3">
+        {users?.length === 0 && (
+          <div className="grid justify-items-center space-y-2">
+            <div>
+              <UsersIcon className="h-5 w-5" />
+            </div>
+            <div>No users are featured</div>
+          </div>
+        )}
+        {users?.map((user: any) => (
+          <UserProfile key={user?.id} user={user} showFollow />
+        ))}
+      </div>
+    </FeaturedUsersCard>
+  )
+}
+
+export default FeaturedUsers
