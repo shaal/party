bca0da37d0f88318a3242ad875e8e4fea65864ad
Yogi <me@yogi.codes>
2021-10-06 03:58:18 +0000

    add: STATIC_ASSETS

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -1,9 +1,11 @@
+import { STATIC_ASSETS } from 'src/constants'
+
 const Hero: React.FC = () => {
   return (
     <div
       className="lg:col-span-7 md:col-span-12 col-span-12 bg-brand-600 dark:bg-brand-700 md:block hidden"
       style={{
-        backgroundImage: `url('https://assets.devparty.io/images/patterns/topography.svg')`
+        backgroundImage: `url('${STATIC_ASSETS}/patterns/topography.svg')`
       }}
     ></div>
   )
