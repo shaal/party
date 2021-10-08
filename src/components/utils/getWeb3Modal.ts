7e3c1064b8a18d591aaa27310129c0ff023fdb73
Yogi <me@yogi.codes>
2021-10-08 17:13:03 +0530

    remove: Wallet connect

--- src/components/utils/getWeb3Modal.ts
+++ src/components/utils/getWeb3Modal.ts
@@ -1,4 +1,3 @@
-import WalletConnectProvider from '@walletconnect/web3-provider'
 import Web3Modal from 'web3modal'
 
 interface Props {
@@ -9,16 +8,8 @@ const getWeb3Modal = ({ theme }: Props) => {
   const web3Modal = new Web3Modal({
     network: 'mainnet',
     cacheProvider: false,
-    providerOptions: {
-      walletconnect: {
-        display: { description: 'Use Rainbow & other popular wallets' },
-        package: WalletConnectProvider,
-        options: { infuraId: '3d19324a72854976a7160e0e2ebc9c2b' }
-      }
-    },
     theme
   })
-
   web3Modal.clearCachedProvider()
 
   return web3Modal
