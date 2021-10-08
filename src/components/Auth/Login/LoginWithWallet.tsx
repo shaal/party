1047482efeda3ac0e5921df3b49e7050a78215a1
Yogi <me@yogi.codes>
2021-10-08 06:15:31 +0000

    add: connecting

--- src/components/Auth/Login/LoginWithWallet.tsx
+++ src/components/Auth/Login/LoginWithWallet.tsx
@@ -4,7 +4,7 @@ import getWeb3Modal from '@components/utils/getWeb3Modal'
 import { useAuthRedirect } from '@components/utils/useAuthRedirect'
 import { ethers } from 'ethers'
 import { useTheme } from 'next-themes'
-import React from 'react'
+import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 
 import {
@@ -15,6 +15,7 @@ import {
 const LoginWithWallet: React.FC = () => {
   const { resolvedTheme } = useTheme()
   const authRedirect = useAuthRedirect()
+  const [loading, setLoading] = useState<boolean>(false)
   const [login, loginResult] = useMutation<
     LoginWithWalletMutation,
     LoginWithWalletMutationVariables
@@ -34,25 +35,32 @@ const LoginWithWallet: React.FC = () => {
   )
 
   const connectWallet = async () => {
-    const web3Modal = getWeb3Modal({ theme: resolvedTheme })
-    const web3 = new ethers.providers.Web3Provider(await web3Modal.connect())
-    const address = await web3.getSigner().getAddress()
-    const response = await fetch(`/api/nonce?address=${address}`)
-    const data = await response.json()
-    if (data.status === 'error') {
-      toast.error(data.message)
-    } else {
-      const signature = await web3
-        .getSigner()
-        .provider.send('personal_sign', [
-          `Sign into Devparty with this wallet. ${data?.nonce}`,
-          await web3.getSigner().getAddress()
-        ])
+    try {
+      setLoading(true)
+      const web3Modal = getWeb3Modal({ theme: resolvedTheme })
+      const web3 = new ethers.providers.Web3Provider(await web3Modal.connect())
+      const address = await web3.getSigner().getAddress()
+      const response = await fetch(`/api/nonce?address=${address}`)
+      const data = await response.json()
+      if (data.status === 'error') {
+        toast.error(data.message)
+        setLoading(false)
+      } else {
+        const signature = await web3
+          .getSigner()
+          .provider.send('personal_sign', [
+            `Sign into Devparty with this wallet. ${data?.nonce}`,
+            await web3.getSigner().getAddress()
+          ])
 
-      login({
-        variables: { input: { nonce: data?.nonce as string, signature } }
-      })
-      web3Modal.clearCachedProvider()
+        login({
+          variables: { input: { nonce: data?.nonce as string, signature } }
+        })
+        web3Modal.clearCachedProvider()
+        setLoading(false)
+      }
+    } catch {
+      setLoading(false)
     }
   }
 
@@ -63,9 +71,10 @@ const LoginWithWallet: React.FC = () => {
       variant="success"
       className=" w-full justify-center"
       onClick={connectWallet}
+      disabled={loading}
       outline
     >
-      Login with Wallet
+      {loading ? 'Connecting...' : 'Login with Wallet'}
     </Button>
   )
 }
