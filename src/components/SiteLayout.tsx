e051fae196427c555040d347594f20908cfdabc7
Yogi <me@yogi.codes>
2021-10-08 19:35:10 +0530

    add: More mixpanel event

--- src/components/SiteLayout.tsx
+++ src/components/SiteLayout.tsx
@@ -3,14 +3,13 @@ import mixpanel from 'mixpanel-browser'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
-import { IS_PRODUCTION } from 'src/constants'
 
 import { CurrentUserQuery } from './__generated__/SiteLayout.generated'
 import MobileFooter from './shared/MobileFooter'
 import Navbar from './shared/Navbar'
 import AppContext from './utils/AppContext'
 
-mixpanel.init(IS_PRODUCTION ? 'ebe03e94eac57d2b33456503c24c02da' : '000')
+mixpanel.init(true ? 'ebe03e94eac57d2b33456503c24c02da' : '000')
 
 export const CURRENT_USER_QUERY = gql`
   query CurrentUserQuery {
@@ -71,11 +70,7 @@ const SiteLayout: React.FC<Props> = ({ children }) => {
   }
 
   if (data?.me) {
-    mixpanel.identify(data?.me?.id)
-    mixpanel.people.set({
-      $username: data?.me?.username,
-      USER_ID: data?.me?.id
-    })
+    mixpanel.identify(data?.me?.username)
   } else {
     mixpanel.identify('Anonymous')
   }
