3e20513ca7245c1581e917e1fb8227cbfccd35a1
Yogi <me@yogi.codes>
2021-10-08 19:03:22 +0530

    rename: To getnonce

--- src/components/Auth/Login/LoginWithWallet.tsx
+++ src/components/Auth/Login/LoginWithWallet.tsx
@@ -47,7 +47,7 @@ const LoginWithWallet: React.FC = () => {
       const web3Modal = getWeb3Modal({ theme: resolvedTheme })
       const web3 = new ethers.providers.Web3Provider(await web3Modal.connect())
       const address = await web3.getSigner().getAddress()
-      const response = await fetch(`/api/nonce?address=${address}`)
+      const response = await fetch(`/api/getnonce?address=${address}`)
       const data = await response.json()
       if (data.status === 'error') {
         toast.error(data.message)
