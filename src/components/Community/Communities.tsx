c9085d785db4692887d9f651f08d55ba5dc7d502
Yogi <me@yogi.codes>
2021-10-09 14:07:41 +0000

    update: communities layout

--- src/components/Community/Communities.tsx
+++ src/components/Community/Communities.tsx
@@ -1,18 +1,20 @@
 import { gql, useQuery } from '@apollo/client'
-import { GridItemEight, GridItemFour, GridLayout } from '@components/GridLayout'
+import {
+  GridItemEight,
+  GridItemFour,
+  GridItemSix,
+  GridItemTwelve,
+  GridLayout
+} from '@components/GridLayout'
 import PostShimmer from '@components/shared/Shimmer/PostShimmer'
 import UserProfileShimmer from '@components/shared/Shimmer/UserProfileShimmer'
 import { Button } from '@components/ui/Button'
 import { Card, CardBody } from '@components/ui/Card'
-import { ErrorMessage } from '@components/ui/ErrorMessage'
 import { CubeIcon } from '@heroicons/react/outline'
-import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import React from 'react'
 
 import { CommunitiesQuery } from './__generated__/Communities.generated'
-
-const Footer = dynamic(() => import('@components/shared/Footer'))
 
 export const COMMUNITIES_QUERY = gql`
   query CommunitiesQuery {
@@ -50,32 +52,33 @@ const Communities: React.FC = () => {
     )
 
   return (
-    <GridLayout>
-      <GridItemEight>
-        <Card>
-          <CardBody className="space-y-6">
-            <ErrorMessage title="Failed to load communities" error={error} />
-            WIP
-          </CardBody>
-        </Card>
-      </GridItemEight>
-      <GridItemFour>
-        <Card>
-          <CardBody className="space-y-3">
-            <div>
-              Launch a new community to Devparty and socialize with your
-              favorite developers
-            </div>
-            <Link href="/communities/new" passHref>
-              <Button icon={<CubeIcon className="h-4 w-4" />}>
-                <div>Create new community</div>
-              </Button>
-            </Link>
-          </CardBody>
-        </Card>
-        <Footer />
-      </GridItemFour>
-    </GridLayout>
+    <>
+      <GridLayout className="flex-grow-0 pb-0">
+        <GridItemTwelve>
+          <Card>
+            <CardBody className="space-y-3">
+              <Link href="/communities/new" passHref>
+                <Button icon={<CubeIcon className="h-4 w-4" />}>
+                  <div>New community</div>
+                </Button>
+              </Link>
+            </CardBody>
+          </Card>
+        </GridItemTwelve>
+      </GridLayout>
+      <GridLayout className="flex-grow-0">
+        <GridItemSix>
+          <Card>
+            <CardBody className="space-y-3">WIP</CardBody>
+          </Card>
+        </GridItemSix>
+        <GridItemSix>
+          <Card>
+            <CardBody className="space-y-3">WIP</CardBody>
+          </Card>
+        </GridItemSix>
+      </GridLayout>
+    </>
   )
 }
 
