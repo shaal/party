ef352292a6e33c0be54aa0401e0d0525dc2b23ec
Yogi <me@yogi.codes>
2021-10-10 04:58:53 +0000

    remove: viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,10 +21,6 @@ class DevpartyDocument extends Document<Props> {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-          />
           <meta name="theme-color" content="#ffffff" />
           <link rel="icon" href="/favicon.svg" />
           <link
