e8af28c998425e2accad4ab0f45e8fa7bbc6acb9
Yogi <me@yogi.codes>
2021-10-11 19:47:21 +0530

    remove: mixpanel tracking

--- src/components/User/Settings/Profile/NFTAvatars/Modal.tsx
+++ src/components/User/Settings/Profile/NFTAvatars/Modal.tsx
@@ -1,7 +1,6 @@
 import { gql, useMutation } from '@apollo/client'
 import { Spinner } from '@components/ui/Spinner'
 import { CollectionIcon } from '@heroicons/react/outline'
-import mixpanel from 'mixpanel-browser'
 import { useRouter } from 'next/router'
 import toast from 'react-hot-toast'
 import { ERROR_MESSAGE, IS_PRODUCTION } from 'src/constants'
@@ -43,13 +42,9 @@ const NFTAvatarsModal: React.FC<Props> = ({ ethAddress }) => {
       }
     `,
     {
-      onError() {
-        mixpanel.track('user.profile.nft_avatars.update.failed')
-      },
       onCompleted(data) {
         toast.success('Avatar has been updated successfully!')
         router.push(`/@/${data?.editNFTAvatar?.username}`)
-        mixpanel.track('user.profile.nft_avatars.update.success')
       }
     }
   )
@@ -84,8 +79,7 @@ const NFTAvatarsModal: React.FC<Props> = ({ ethAddress }) => {
           <div key={asset?.id}>
             <div
               className="cursor-pointer"
-              onClick={() => {
-                mixpanel.track('user.profile.nft_avatars.update.click')
+              onClick={() =>
                 editNFTAvatar({
                   variables: {
                     input: {
@@ -94,7 +88,7 @@ const NFTAvatarsModal: React.FC<Props> = ({ ethAddress }) => {
                     }
                   }
                 })
-              }}
+              }
             >
               <img
                 className="object-cover h-36 w-36 rounded-lg border"
