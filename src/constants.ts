03d337d45a310cae58d2c76ef51ac1f6e9677bec
Yogi <me@yogi.codes>
2021-10-12 02:20:48 +0000

    add: OPENSEA_API_URL to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,27 @@
 import React from 'react'
 
-export const STATIC_ASSETS = 'https://assets.devparty.io/images'
+// Environments
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development'
-export const BASE_URL = process.env.BASE_URL
+
+// Versions
 export const REACT_VERSION = React.version
+
+// Git
 export const GIT_COMMIT_SHA = process.env.GIT_COMMIT_SHA?.slice(0, 7)
 export const GIT_COMMIT_REF = process.env.GIT_COMMIT_REF
+
+// Configs
 export const POLLING_INTERVAL = 1000 * 60 // 1 minute
+
+// Messages
 export const ERROR_MESSAGE = 'Something went wrong!'
 export const PUBLIC_SIGNING_MESSAGE =
   'Sign into Devparty with this wallet.\n\nNonce:'
+
+// URLs
+export const BASE_URL = process.env.BASE_URL
+export const STATIC_ASSETS = 'https://assets.devparty.io/images'
+export const OPENSEA_API_URL = `https://${
+  IS_PRODUCTION ? 'testnets-api' : 'testnets-api'
+}.opensea.io/api/v1`
