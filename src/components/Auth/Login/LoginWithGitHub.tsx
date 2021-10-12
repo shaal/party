f862c51bf5127a5f8a972812fdd997fdd61369ac
Yogi <me@yogi.codes>
2021-10-12 03:50:29 +0000

    fix: theme issues in login buttons

--- src/components/Auth/Login/LoginWithGitHub.tsx
+++ src/components/Auth/Login/LoginWithGitHub.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@components/ui/Button'
 import mixpanel from 'mixpanel-browser'
+import { useTheme } from 'next-themes'
 import React from 'react'
 import { STATIC_ASSETS } from 'src/constants'
 
 const LoginWithGitHub: React.FC = () => {
+  const { resolvedTheme } = useTheme()
+
   return (
     <a href="/api/auth/github">
       <Button
@@ -11,10 +14,12 @@ const LoginWithGitHub: React.FC = () => {
         type="button"
         variant="success"
         onClick={() => mixpanel.track('login.github.click')}
-        className="w-full justify-center text-[#0d1117] border-[#0d1117] hover:bg-[#dadada] focus:ring-[#0d1117]"
+        className="w-full justify-center text-[#0d1117] dark:text-white border-[#0d1117] dark:border-white hover:bg-[#dadada] focus:ring-[#0d1117]"
         icon={
           <img
-            src={`${STATIC_ASSETS}/brands/github-dark.svg`}
+            src={`${STATIC_ASSETS}/brands/${
+              resolvedTheme === 'dark' ? 'github-dark' : 'github-light'
+            }.svg`}
             className="h-4 w-4"
             alt="GitHub Logo"
           />
