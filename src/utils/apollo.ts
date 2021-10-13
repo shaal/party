4bd808d3da88429a0ef2610f82637bea8ce3606b
Yogi <me@yogi.codes>
2021-10-13 04:26:58 +0000

    add: more docs to apollo.ts

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -83,13 +83,25 @@ export function createApolloClient({ initialState, headers }: ClientOptions) {
     })
   }
 
+  /**
+   * If your page has Next.js data fetching methods that use Apollo Client,
+   * the initial state gets hydrated here
+   */
   if (initialState) {
+    // Get existing cache, loaded during client side data fetching
     const existingCache = nextClient.extract()
+
+    /**
+     * Restore the cache using the data passed from
+     * getStaticProps/getServerSideProps combined with the existing cached data
+     */
     nextClient.cache.restore({ ...existingCache, ...initialState })
   }
 
+  // For SSG and SSR always create a new Apollo Client
   if (ssrMode) return nextClient
 
+  // Create the Apollo Client once in the client
   if (!apolloClient) apolloClient = nextClient
 
   return nextClient
