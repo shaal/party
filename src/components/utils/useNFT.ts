812e40c865b3ace2260b0bf0d7e21381c2b72eac
Yogi <me@yogi.codes>
2021-10-13 04:29:47 +0000

    add: more JSDoc in hooks #130

--- src/components/utils/useNFT.ts
+++ src/components/utils/useNFT.ts
@@ -2,6 +2,12 @@ import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
+/**
+ * Get a single NFT from opensea
+ * @param address - NFT's hash address
+ * @param tokenId - NFT's Token ID
+ * @returns a NFT from opensea
+ */
 export const useNFT = (address: string, tokenId: string) => {
   const { data, error } = useSWR(
     `/api/nft?address=${address}&tokenId=${tokenId}`,
