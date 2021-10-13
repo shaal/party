13f0f56efa182ce1b79f91350dab4f3e5f8843ea
Yogi <me@yogi.codes>
2021-10-13 03:41:05 +0000

    add: more JSDoc in GraphQL #130

--- src/graphql/resolvers/Post/queries/hasAnswered.ts
+++ src/graphql/resolvers/Post/queries/hasAnswered.ts
@@ -1,5 +1,11 @@
 import { db } from '@utils/prisma'
 
+/**
+ * Check whether the user has answered a poll
+ * @param currentUserId - Current user ID
+ * @param answerId - Answer ID of the poll to check it is answered
+ * @returns the user whether they answered a poll
+ */
 export const hasAnswered = async (currentUserId: string, answerId: string) => {
   const user = await db.pollAnswer.findUnique({
     where: { id: answerId },
