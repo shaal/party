a93329812f2b98e51da786e8cb84889e74025c3b
Yogi <me@yogi.codes>
2021-10-13 04:14:35 +0000

    add: more JSDoc in GraphQL User #130

--- src/graphql/resolvers/User/queries/getSessions.ts
+++ src/graphql/resolvers/User/queries/getSessions.ts
@@ -2,6 +2,12 @@ import { db } from '@utils/prisma'
 
 import { Session } from '.prisma/client'
 
+/**
+ * Get list of current user's sessions
+ * @param query - Contains an include object to pre-load data needed to resolve nested parts.
+ * @param session - Current user's session
+ * @returns list of current user's sessions
+ */
 export const getSessions = async (
   query: any,
   session: Session | null | undefined
