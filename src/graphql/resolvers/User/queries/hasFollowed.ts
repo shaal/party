78055a92309064d533c4db33d78e915c848a06b2
Yogi <me@yogi.codes>
2021-10-13 04:16:42 +0000

    add: more JSDoc in GraphQL User #130

--- src/graphql/resolvers/User/queries/hasFollowed.ts
+++ src/graphql/resolvers/User/queries/hasFollowed.ts
@@ -1,5 +1,11 @@
 import { db } from '@utils/prisma'
 
+/**
+ * Check whether user is followed or not
+ * @param currentUserId - Current user ID
+ * @param userId - Target user ID
+ * @returns whether user is followed or not
+ */
 export const hasFollowed = async (currentUserId: string, userId: string) => {
   const user = await db.user.findUnique({
     where: { id: userId },
