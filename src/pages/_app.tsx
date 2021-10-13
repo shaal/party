92478a7307c9a9a8476f1ce74d19ee4d04eccc35
Yogi <me@yogi.codes>
2021-10-13 05:04:18 +0000

    add: viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,10 @@ const App = ({ Component, pageProps }: AppProps) => {
       <ThemeProvider defaultTheme="light" attribute="class">
         <Head>
           <title>Devparty</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          />
         </Head>
         <SiteLayout>
           <NextNprogress
