57d8830f6784a83ee2053ecab27fd3ccf8af29f0
Yogi <me@yogi.codes>
2021-10-13 03:38:39 +0000

    add: more JSDoc in GraphQL #130

--- src/graphql/resolvers/Post/queries/exploreFeed.ts
+++ src/graphql/resolvers/Post/queries/exploreFeed.ts
@@ -1,5 +1,10 @@
 import { db } from '@utils/prisma'
 
+/**
+ * Generated a feed for explore page
+ * @param query - Contains an include object to pre-load data needed to resolve nested parts.
+ * @returns a posts for explore page
+ */
 export const exploreFeed = async (query: any) => {
   return await db.post.findMany({
     ...query,
