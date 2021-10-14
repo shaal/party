c3fff91dd1f8722b26de273b356291282135a3e9
Yogi <me@yogi.codes>
2021-10-14 20:11:38 +0530

    update: Font sizes

--- src/components/Post/NewPost/SelectTarget.tsx
+++ src/components/Post/NewPost/SelectTarget.tsx
@@ -1,7 +1,8 @@
 import { gql, useQuery } from '@apollo/client'
+import { Button } from '@components/ui/Button'
 import AppContext from '@components/utils/AppContext'
 import { Popover, Transition } from '@headlessui/react'
-import { GlobeIcon } from '@heroicons/react/outline'
+import { ChevronDownIcon, GlobeIcon } from '@heroicons/react/outline'
 import { Fragment, useState } from 'react'
 import { useContext } from 'react'
 import { Product } from 'src/__generated__/schema.generated'
@@ -58,11 +59,19 @@ const SelectTarget: React.FC<Props> = ({ setSelectedProduct }) => {
   }
 
   return (
-    <div className="w-full max-w-sm px-4">
+    <div className="w-full max-w-sm px-2">
       <Popover>
         {({ open }) => (
           <>
-            <Popover.Button>Everywhere</Popover.Button>
+            <Popover.Button
+              as={Button}
+              className="text-sm flex items-center space-x-2"
+              size="sm"
+              icon={<ChevronDownIcon className="h-4 w-4" />}
+              outline
+            >
+              <div>Everywhere</div>
+            </Popover.Button>
             <Transition
               as={Fragment}
               enter="transition ease-out duration-200"
@@ -72,20 +81,20 @@ const SelectTarget: React.FC<Props> = ({ setSelectedProduct }) => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute z-10 mt-2 w-screen max-w-[13rem]">
+              <Popover.Panel className="absolute z-10 mt-2 w-screen max-w-[14rem]">
                 <div className="overflow-hidden rounded-lg shadow-md border">
-                  <div className="relative bg-white p-3 space-y-2">
+                  <div className="relative bg-white p-4 space-y-2">
                     <div className="font-bold">Where to post?</div>
                     <button className="flex items-center space-x-2 text-sm">
                       <GlobeIcon className="h-5 w-5 text-brand-500" />
                       <div>Everywhere</div>
                     </button>
                     <div className="space-y-2">
-                      <div className="text-sm font-bold">My Products</div>
-                      <div className="space-y-2 text-sm">
+                      <div className="font-bold">My Products</div>
+                      <div className="space-y-2">
                         {products?.map((product: any) => (
                           <div key={product?.name}>
-                            <button className="flex space-x-2">
+                            <button className="flex items-center space-x-2">
                               <img
                                 className="h-8 w-8 rounded"
                                 src={product?.avatar}
@@ -93,7 +102,7 @@ const SelectTarget: React.FC<Props> = ({ setSelectedProduct }) => {
                               />
                               <div className="text-left">
                                 <div className="font-bold">{product?.name}</div>
-                                <div className="text-xs">
+                                <div className="text-sm">
                                   <b>{product?.subscribers?.totalCount}</b>{' '}
                                   subscribers
                                 </div>
@@ -104,11 +113,11 @@ const SelectTarget: React.FC<Props> = ({ setSelectedProduct }) => {
                       </div>
                     </div>
                     <div className="space-y-2 pt-2">
-                      <div className="text-sm font-bold">My Communities</div>
-                      <div className="space-y-2 text-sm">
+                      <div className="font-bold">My Communities</div>
+                      <div className="space-y-2">
                         {communities?.map((community: any) => (
                           <div key={community?.name}>
-                            <button className="flex space-x-2">
+                            <button className="flex items-center space-x-2">
                               <img
                                 className="h-8 w-8 rounded"
                                 src={community?.avatar}
@@ -118,7 +127,7 @@ const SelectTarget: React.FC<Props> = ({ setSelectedProduct }) => {
                                 <div className="font-bold">
                                   {community?.name}
                                 </div>
-                                <div className="text-xs">
+                                <div className="text-sm">
                                   <b>{community?.members?.totalCount}</b>{' '}
                                   members
                                 </div>
