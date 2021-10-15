861b93048112d328a496654b6e0439039f72453a
Yogi <me@yogi.codes>
2021-10-15 05:53:12 +0000

    finish: SETTINGS_UPDATE

--- src/components/User/Settings/Logs/SingleLog.tsx
+++ src/components/User/Settings/Logs/SingleLog.tsx
@@ -1,6 +1,5 @@
 import Slug from '@components/shared/Slug'
-import { HeartIcon } from '@heroicons/react/outline'
-import Link from 'next/link'
+import { CogIcon } from '@heroicons/react/outline'
 import React from 'react'
 import { Log } from 'src/__generated__/schema.generated'
 import * as timeago from 'timeago.js'
@@ -26,10 +25,8 @@ const SingleLog: React.FC<Props> = ({ log }) => {
         <div className="linkify">
           {log?.action === 'SETTINGS_UPDATE' && (
             <div className="flex items-center space-x-1">
-              <HeartIcon className="h-4 w-4 text-pink-500" />
-              <div>
-                Liked a <Link href={`/posts/${log?.entityId}`}>post</Link>
-              </div>
+              <CogIcon className="h-4 w-4 text-gray-500" />
+              <div>updated user settings</div>
             </div>
           )}
         </div>
