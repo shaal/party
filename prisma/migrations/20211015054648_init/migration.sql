6b468c38a7474813150d21b07af21bea6504ec96
Yogi <me@yogi.codes>
2021-10-15 05:47:07 +0000

    update: migs

---
+++ prisma/migrations/20211015054648_init/migration.sql
@@ -0,0 +1,312 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `hashedPassword` VARCHAR(191) NOT NULL,
+    `spammy` BOOLEAN NOT NULL DEFAULT false,
+    `isVerified` BOOLEAN NOT NULL DEFAULT false,
+    `isStaff` BOOLEAN NOT NULL DEFAULT false,
+    `inWaitlist` BOOLEAN NOT NULL DEFAULT true,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `featuredAt` DATETIME(3),
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sessions` (
+    `id` VARCHAR(191) NOT NULL,
+    `isStaff` BOOLEAN NOT NULL DEFAULT false,
+    `ipAddress` VARCHAR(191),
+    `userAgent` VARCHAR(191),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `expiresAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `profiles` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `avatar` VARCHAR(191) NOT NULL,
+    `nftSource` VARCHAR(191),
+    `cover` VARCHAR(191) NOT NULL,
+    `coverBg` VARCHAR(191) NOT NULL,
+    `bio` VARCHAR(191),
+    `location` VARCHAR(191),
+    `website` VARCHAR(191),
+    `discord` VARCHAR(191),
+    `github` VARCHAR(191),
+    `twitter` VARCHAR(191),
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `profiles_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `invites` (
+    `id` VARCHAR(191) NOT NULL,
+    `code` VARCHAR(191),
+    `usedTimes` INTEGER NOT NULL DEFAULT 0,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `invites_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `tips` (
+    `id` VARCHAR(191) NOT NULL,
+    `cash` VARCHAR(191),
+    `paypal` VARCHAR(191),
+    `github` VARCHAR(191),
+    `buymeacoffee` VARCHAR(191),
+    `bitcoin` VARCHAR(191),
+    `ethereum` VARCHAR(191),
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `tips_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `posts` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191),
+    `body` TEXT NOT NULL,
+    `done` BOOLEAN NOT NULL DEFAULT false,
+    `type` ENUM('POST', 'TASK', 'QUESTION', 'POLL', 'COMMIT', 'NFT', 'REPLY') NOT NULL DEFAULT 'POST',
+    `attachments` JSON,
+    `hidden` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `productId` VARCHAR(191),
+    `communityId` VARCHAR(191),
+    `parentId` VARCHAR(191),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `nfts` (
+    `id` VARCHAR(191) NOT NULL,
+    `tokenId` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `postId` VARCHAR(191),
+
+    UNIQUE INDEX `nfts_postId_key`(`postId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `poll` (
+    `id` VARCHAR(191) NOT NULL,
+    `postId` VARCHAR(191),
+
+    UNIQUE INDEX `poll_postId_key`(`postId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `poll_answers` (
+    `id` VARCHAR(191) NOT NULL,
+    `index` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `pollId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `post_topics` (
+    `id` VARCHAR(191) NOT NULL,
+    `postId` VARCHAR(191),
+    `topicId` VARCHAR(191),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `topics` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191),
+    `description` VARCHAR(191),
+
+    UNIQUE INDEX `topics_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `likes` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `postId` VARCHAR(191),
+
+    UNIQUE INDEX `likes_userId_postId_key`(`userId`, `postId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `products` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191),
+    `avatar` VARCHAR(191),
+    `website` VARCHAR(191),
+    `producthunt` VARCHAR(191),
+    `discord` VARCHAR(191),
+    `github` VARCHAR(191),
+    `twitter` VARCHAR(191),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `ownerId` VARCHAR(191),
+
+    UNIQUE INDEX `products_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `communities` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `slug` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191),
+    `avatar` VARCHAR(191),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `ownerId` VARCHAR(191),
+
+    UNIQUE INDEX `communities_slug_key`(`slug`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `community_rules` (
+    `id` VARCHAR(191) NOT NULL,
+    `index` INTEGER NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191),
+    `communityId` VARCHAR(191),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `badges` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `notifications` (
+    `id` VARCHAR(191) NOT NULL,
+    `message` VARCHAR(191),
+    `isRead` BOOLEAN NOT NULL DEFAULT false,
+    `type` ENUM('POST_LIKE', 'POST_REPLY', 'USER_MENTION', 'USER_FOLLOW', 'USER_INVITE_FOLLOW', 'PRODUCT_SUBSCRIBE') NOT NULL,
+    `entityId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `receiverId` VARCHAR(191) NOT NULL,
+    `dispatcherId` VARCHAR(191) NOT NULL,
+    `likeId` VARCHAR(191),
+    `productId` VARCHAR(191),
+    `postId` VARCHAR(191),
+
+    UNIQUE INDEX `notifications_entityId_key`(`entityId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `integrations` (
+    `id` VARCHAR(191) NOT NULL,
+    `wakatimeAPIKey` VARCHAR(191),
+    `spotifyRefreshToken` VARCHAR(191),
+    `githubId` VARCHAR(191),
+    `ethAddress` VARCHAR(191),
+    `ethNonce` VARCHAR(191),
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `integrations_githubId_key`(`githubId`),
+    UNIQUE INDEX `integrations_ethAddress_key`(`ethAddress`),
+    UNIQUE INDEX `integrations_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `logs` (
+    `id` VARCHAR(191) NOT NULL,
+    `action` ENUM('SETTINGS_UPDATE') NOT NULL,
+    `entityId` VARCHAR(191),
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_ProductToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_ProductToUser_AB_unique`(`A`, `B`),
+    INDEX `_ProductToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_CommunityToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_CommunityToUser_AB_unique`(`A`, `B`),
+    INDEX `_CommunityToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_BadgeToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_BadgeToUser_AB_unique`(`A`, `B`),
+    INDEX `_BadgeToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_TopicToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_TopicToUser_AB_unique`(`A`, `B`),
+    INDEX `_TopicToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_PollAnswerToUser` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_PollAnswerToUser_AB_unique`(`A`, `B`),
+    INDEX `_PollAnswerToUser_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_follows` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_follows_AB_unique`(`A`, `B`),
+    INDEX `_follows_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
