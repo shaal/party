d540b6f88b6835106ded6fa59f06bd87ced90e9b
Yogi <me@yogi.codes>
2021-10-15 05:22:35 +0000

    remove: sentiment analyzer

--- src/components/Post/Mod.tsx
+++ src/components/Post/Mod.tsx
@@ -1,17 +1,12 @@
 import { Card, CardBody } from '@components/ui/Card'
 import { HashtagIcon } from '@heroicons/react/outline'
-import Sentiment from 'sentiment'
 import { Post } from 'src/__generated__/schema.generated'
-
-var sentiment = new Sentiment()
 
 interface Props {
   post: Post
 }
 
 const PostMod: React.FC<Props> = ({ post }) => {
-  const score = sentiment.analyze(post?.body).score
-
   return (
     <Card className="mt-5 border-yellow-400 !bg-yellow-300 !bg-opacity-20">
       <CardBody>
@@ -20,21 +15,6 @@ const PostMod: React.FC<Props> = ({ post }) => {
           <div className="flex items-center gap-1">
             <HashtagIcon className="h-4 w-4" />
             <span className="font-mono font-bold">{post?.id}</span>
-          </div>
-        </div>
-        <div className="border-t border-yellow-400 my-3"></div>
-        <div className="font-bold text-lg">Flags</div>
-        <div className="space-y-1.5 mt-3 text-sm font-bold">
-          <div className="space-x-2">
-            <span>Toxicity:</span>
-            {score < -5 && <span className="text-red-600">🤮 Yikes</span>}
-            {score >= -5 && score < 0 && (
-              <span className="text-red-600">🤢 Bad</span>
-            )}
-            {score >= 0 && score < 5 && (
-              <span className="text-green-600">😇 Good</span>
-            )}
-            {score > 5 && <span className="text-green-600">😇 Very Good</span>}
           </div>
         </div>
       </CardBody>
