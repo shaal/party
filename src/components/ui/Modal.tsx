2cde75d8441dd53b49627bb19485ed77e12632e1
Yogi <me@yogi.codes>
2021-10-15 03:18:25 +0000

    update: Modal style

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,5 +1,6 @@
+import { Dialog, Transition } from '@headlessui/react'
 import { XIcon } from '@heroicons/react/outline'
-import React, { useEffect } from 'react'
+import React, { Fragment } from 'react'
 
 interface Props {
   title: React.ReactNode
@@ -10,57 +11,54 @@ interface Props {
 }
 
 export const Modal: React.FC<Props> = (props) => {
-  const handler = (evt: KeyboardEvent) => {
-    if (evt.defaultPrevented) {
-      return
-    }
-    if (evt.key === 'Escape') {
-      props.onClose()
-    }
-    if (evt.key === 'Enter') {
-      if (props.onEnter) {
-        if (props.onEnter()) {
-          props.onClose()
-        }
-      } else {
-        props.onClose()
-      }
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', handler)
-    return () => {
-      window.removeEventListener('keydown', handler)
-    }
-  })
-
-  if (!props.show) return null
-
   return (
-    <div className="fixed top-0 left-0 bg-black bg-opacity-70 z-50 h-screen">
-      <div
-        className="w-screen h-screen align-middle"
-        style={{ display: 'table-cell' }}
+    <Transition.Root show={props.show} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-10 inset-0 overflow-y-auto"
+        onClose={props.onClose}
       >
-        <div
-          className="relative bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-800 rounded-lg max-w-lg mx-auto text-left"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className="flex items-center justify-between border-b dark:border-gray-800 px-5 py-3.5">
-            <div className="font-bold">{props.title}</div>
-            <div
-              className="cursor-pointer text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md p-1"
-              onClick={props.onClose}
-            >
-              <XIcon className="h-5 w-5" />
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          />
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="flex items-center justify-between border-b dark:border-gray-800 px-5 py-3.5">
+                <div className="font-bold">{props.title}</div>
+                <div
+                  className="cursor-pointer text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-md p-1"
+                  onClick={props.onClose}
+                >
+                  <XIcon className="h-5 w-5" />
+                </div>
+              </div>
+              {props.children}
             </div>
-          </div>
-          <div className="border-gray-200 dark:border-gray-800">
-            {props.children}
-          </div>
+          </Transition.Child>
         </div>
-      </div>
-    </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
