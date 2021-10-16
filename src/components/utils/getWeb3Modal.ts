6bfb1fae41477c8155c0ed3574260f7aaba89379
Yogi <me@yogi.codes>
2021-10-16 03:01:11 +0000

    add: WalletConnect

--- src/components/utils/getWeb3Modal.ts
+++ src/components/utils/getWeb3Modal.ts
@@ -1,3 +1,4 @@
+import WalletConnectProvider from '@walletconnect/web3-provider'
 import Web3Modal from 'web3modal'
 
 interface Props {
@@ -8,6 +9,13 @@ const getWeb3Modal = ({ theme }: Props) => {
   const web3Modal = new Web3Modal({
     network: 'mainnet',
     cacheProvider: false,
+    providerOptions: {
+      walletconnect: {
+        display: { description: 'Use Rainbow & other popular wallets' },
+        package: WalletConnectProvider,
+        options: { infuraId: '3d19324a72854976a7160e0e2ebc9c2b' }
+      }
+    },
     theme
   })
   web3Modal.clearCachedProvider()
