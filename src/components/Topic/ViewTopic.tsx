1178dc0a50e62171048134bfb6265346863bb29d
Yogi <me@yogi.codes>
2021-10-16 14:32:37 +0000

    fix: classname

--- src/components/Topic/ViewTopic.tsx
+++ src/components/Topic/ViewTopic.tsx
@@ -44,7 +44,7 @@ const ViewTopic: React.FC = () => {
 
   return (
     <GridLayout>
-      <GridItemEight>
+      <GridItemEight className="mb-4 lg:mb-0">
         <TopicFeed topic={topic?.name as string} />
       </GridItemEight>
       <GridItemFour>
