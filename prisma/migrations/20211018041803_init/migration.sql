6b530cda1ac7dda4c1e050c15e57d68f601f9da7
Yogi <yoginth@hey.com>
2021-10-18 04:18:39 +0000

    feat: add communityModerator / moderatedCommunities to DB

--- prisma/migrations/20211018041803_init/migration.sql
+++ prisma/migrations/20211018041803_init/migration.sql
@@ -267,6 +267,15 @@ CREATE TABLE `_ProductToUser` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
+CREATE TABLE `_communityModerator` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_communityModerator_AB_unique`(`A`, `B`),
+    INDEX `_communityModerator_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `_CommunityToUser` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
