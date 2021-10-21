195a6ae080ed97b2795fb4e5f37e921fa11107b5
Yogi <yoginth@hey.com>
2021-10-21 12:44:56 +0000

    feat: add Masquerading comp

--- src/components/shared/Navbar/Masquerading.tsx
+++ src/components/shared/Navbar/Masquerading.tsx
@@ -1,15 +1,18 @@
+import AppContext from '@components/utils/AppContext'
 import { SwitchHorizontalIcon } from '@heroicons/react/outline'
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Slug from '../Slug'
 
 const Masquerading: React.FC = () => {
+  const { currentUser } = useContext(AppContext)
   return (
     <div className="bg-yellow-200 dark:bg-yellow px-3 py-2 flex justify-between text-sm font-bold">
       <div className="mx-auto flex items-center space-x-2">
         <SwitchHorizontalIcon className="h-5 w-5" />
         <div>
-          You are masquerading into <Slug slug="filiptronicek" prefix="@" />
+          You are masquerading into{' '}
+          <Slug slug={currentUser?.username} prefix="@" />
           's account
         </div>
       </div>
