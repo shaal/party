f48395af9b0aaad51c1a27bc233f3e5a2de84abb
Yogi <yoginth@hey.com>
2021-10-23 15:28:10 +0000

    feat: add acceptCocAndTos and form

--- src/components/Onboarding/index.tsx
+++ src/components/Onboarding/index.tsx
@@ -1,17 +1,54 @@
+import { gql, useMutation } from '@apollo/client'
 import { Button } from '@components/UI/Button'
 import { Card, CardBody } from '@components/UI/Card'
+import { Form, useZodForm } from '@components/UI/Form'
 import AppContext from '@components/utils/AppContext'
 import { ArrowCircleRightIcon } from '@heroicons/react/outline'
+import mixpanel from 'mixpanel-browser'
 import { useRouter } from 'next/router'
 import React, { useContext } from 'react'
+import toast from 'react-hot-toast'
+import { boolean, object } from 'zod'
+
+import {
+  AcceptCocAndTosMutation,
+  AcceptCocAndTosMutationVariables
+} from './__generated__/index.generated'
+
+const acceptCOCAndTosSchema = object({
+  coc: boolean(),
+  tos: boolean()
+})
 
 const Onboarding: React.FC = () => {
   const { currentUser } = useContext(AppContext)
   const router = useRouter()
+  const [acceptCocAndTos] = useMutation<
+    AcceptCocAndTosMutation,
+    AcceptCocAndTosMutationVariables
+  >(
+    gql`
+      mutation AcceptCOCAndTOSMutation($input: AcceptCOCAndTOSInput!) {
+        acceptCocAndTos(input: $input) {
+          id
+        }
+      }
+    `,
+    {
+      onError(error) {
+        toast.error(error.message)
+        mixpanel.track('user.onboarding.accept_coc_and_tos.failed')
+      },
+      onCompleted() {
+        router.push('/onboarding/topics')
+        mixpanel.track('user.onboarding.accept_coc_and_tos.success')
+      }
+    }
+  )
 
-  const handleContinue = () => {
-    router.push('/onboarding/topics')
-  }
+  const form = useZodForm({
+    schema: acceptCOCAndTosSchema
+  })
 
   return (
     <div
@@ -31,27 +68,35 @@ const Onboarding: React.FC = () => {
           <div>A social network for developers and creators.</div>
         </div>
         <CardBody className="linkify space-y-2">
-          <div className="flex items-center space-x-2">
-            <input id="acceptCOC" type="checkbox" />
-            <label htmlFor="acceptCOC">
-              You agree to uphold our <a href="/">Code of Conduct</a>
-            </label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <input id="acceptCOC" type="checkbox" />
-            <label htmlFor="acceptCOC">
-              You agree to our <a href="/">Terms and Conditions</a>
-            </label>
-          </div>
-          <div className="pt-5">
-            <Button
-              className="mx-auto"
-              icon={<ArrowCircleRightIcon className="h-4 w-4" />}
-              onClick={handleContinue}
-            >
-              Continue
-            </Button>
-          </div>
+          <Form
+            form={form}
+            onSubmit={({ coc, tos }) => {
+              mixpanel.track('user.onboarding.accept_coc_and_tos.click')
+              acceptCocAndTos({ variables: { input: { coc, tos } } })
+            }}
+          >
+            <div className="flex items-center space-x-2">
+              <input id="acceptCOC" type="checkbox" {...form.register('coc')} />
+              <label htmlFor="acceptCOC">
+                You agree to uphold our <a href="/">Code of Conduct</a>
+              </label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <input id="acceptCOC" type="checkbox" {...form.register('tos')} />
+              <label htmlFor="acceptCOC">
+                You agree to our <a href="/">Terms and Conditions</a>
+              </label>
+            </div>
+            <div className="pt-5">
+              <Button
+                className="mx-auto"
+                icon={<ArrowCircleRightIcon className="h-4 w-4" />}
+                disabled
+              >
+                Continue
+              </Button>
+            </div>
+          </Form>
         </CardBody>
       </Card>
     </div>
