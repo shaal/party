d1663dedd4610f772f14ef145696c4d06f15bd17
Yogi <yoginth@hey.com>
2021-10-23 09:06:17 +0000

    feat: add card

--- src/components/Onboarding/index.tsx
+++ src/components/Onboarding/index.tsx
@@ -1,7 +1,17 @@
+import { Card, CardBody } from '@components/ui/Card'
 import React from 'react'
 
 const Onboarding: React.FC = () => {
-  return <div className="flex justify-center items-center h-[90vh]">WIP</div>
+  return (
+    <div className="flex justify-center items-center h-[90vh]">
+      <Card>
+        <div>
+          <div>Yogi — welcome to DEV Community!</div>
+        </div>
+        <CardBody>WIP</CardBody>
+      </Card>
+    </div>
+  )
 }
 
 export default Onboarding
