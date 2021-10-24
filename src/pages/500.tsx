f0ccb3f427aff44224cb0f64833094b9bee7ffc4
Yogi <yoginth@hey.com>
2021-10-24 02:57:13 +0000

    fix: center align 500 page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -6,35 +6,33 @@ import { ERROR_MESSAGE, STATIC_ASSETS } from 'src/constants'
 
 export default function Custom500() {
   return (
-    <>
+    <div className="page-center flex-col">
       <DevpartySEO title={`${ERROR_MESSAGE} · Devparty`} />
-      <div className="flex flex-col h-full justify-start pt-10 items-center">
-        <img
-          src={`${STATIC_ASSETS}/gifs/nyan-cat.gif`}
-          alt="Nyan Cat"
-          className="h-60"
-        />
-        <div className="py-10 text-center">
-          <h1 className="text-3xl mb-4 font-bold">
-            Looks like something went wrong!
-          </h1>
-          <div className="mb-4">
-            We track these errors automatically, but if the problem persists
-            feel free to contact us. In the meantime, try refreshing.
-          </div>
-          <Link href="/signup">
-            <a href="/signup">
-              <Button
-                className="flex item-center mx-auto"
-                size="lg"
-                icon={<HomeIcon className="w-4 h-4" />}
-              >
-                <div>Go to home</div>
-              </Button>
-            </a>
-          </Link>
+      <img
+        src={`${STATIC_ASSETS}/gifs/nyan-cat.gif`}
+        alt="Nyan Cat"
+        className="h-60"
+      />
+      <div className="py-10 text-center">
+        <h1 className="text-3xl mb-4 font-bold">
+          Looks like something went wrong!
+        </h1>
+        <div className="mb-4">
+          We track these errors automatically, but if the problem persists feel
+          free to contact us. In the meantime, try refreshing.
         </div>
+        <Link href="/signup">
+          <a href="/signup">
+            <Button
+              className="flex item-center mx-auto"
+              size="lg"
+              icon={<HomeIcon className="w-4 h-4" />}
+            >
+              <div>Go to home</div>
+            </Button>
+          </a>
+        </Link>
       </div>
-    </>
+    </div>
   )
 }
