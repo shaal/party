0b96f5363711be42147e433b541d6d856754a6df
Yogi <yoginth@hey.com>
2021-10-24 15:38:18 +0000

    fix: build generated file inside app

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     runtimeCaching
   },
+  experimental: { esmExternals: true },
   reactStrictMode: true,
   poweredByHeader: false,
   generateEtags: false,
