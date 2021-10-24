415832d24816bf8f09888165b63020047330c8bf
Yogi <yoginth@hey.com>
2021-10-24 02:54:58 +0000

    fix: add page-center class and align 404 page

--- src/components/Onboarding/Topics.tsx
+++ src/components/Onboarding/Topics.tsx
@@ -14,7 +14,7 @@ const Topics: React.FC = () => {
   }
 
   return (
-    <div className="onboarding-bg">
+    <div className="onboarding-bg page-center">
       <Card className="w-full sm:max-w-xl border-2 shadow-lg">
         <CardBody className="linkify space-y-2">
           <div className="flex items-center mb-5 space-x-5">
