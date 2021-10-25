8d7611107718a9f4777f0cda22e7ff002208b47e
Yogi <yoginth@hey.com>
2021-10-25 12:40:18 +0000

    feat: add ProfileForm

--- src/components/Onboarding/Profile.tsx
+++ src/components/Onboarding/Profile.tsx
@@ -1,75 +1,22 @@
-import { gql, useMutation, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
 import { Button } from '@components/UI/Button'
 import { Card, CardBody } from '@components/UI/Card'
-import { Form, useZodForm } from '@components/UI/Form'
-import { Input } from '@components/UI/Input'
 import { ProgressBar } from '@components/UI/ProgressBar'
 import { Spinner } from '@components/UI/Spinner'
-import { TextArea } from '@components/UI/TextArea'
 import { PROFILE_SETTINGS_QUERY } from '@components/User/Settings/Profile'
 import { ProfileSettingsQuery } from '@components/User/Settings/Profile/__generated__/index.generated'
-import {
-  ArrowCircleRightIcon,
-  ArrowLeftIcon,
-  CheckCircleIcon
-} from '@heroicons/react/outline'
+import { ArrowCircleRightIcon, ArrowLeftIcon } from '@heroicons/react/outline'
 import Link from 'next/link'
 import React from 'react'
-import toast from 'react-hot-toast'
-import { object, string } from 'zod'
+import { User } from 'src/__generated__/schema.generated'
 
-import {
-  OnboardingProfileSettingsMutation,
-  OnboardingProfileSettingsMutationVariables
-} from './__generated__/Profile.generated'
-
-const editProfileSchema = object({
-  name: string()
-    .min(2, { message: '👤 Name should atleast have 2 characters' })
-    .max(50, { message: '👤 Name should be within 50 characters' }),
-  bio: string()
-    .max(190, { message: '👤 Bio should not exceed 190 characters' })
-    .nullable(),
-  location: string()
-    .max(50, { message: '📍 Location should not exceed 50 characters' })
-    .nullable()
-})
+import ProfileForm from './ProfileForm'
 
 const Profile: React.FC = () => {
   const { data, loading } = useQuery<ProfileSettingsQuery>(
     PROFILE_SETTINGS_QUERY
   )
-  const [editUser] = useMutation<
-    OnboardingProfileSettingsMutation,
-    OnboardingProfileSettingsMutationVariables
-  >(
-    gql`
-      mutation OnboardingProfileSettingsMutation($input: EditUserInput!) {
-        editUser(input: $input) {
-          id
-        }
-      }
-    `,
-    {
-      onError(error) {
-        toast.error(error.message)
-      },
-      onCompleted() {
-        toast.success('Profile updated successfully!')
-      }
-    }
-  )
-
   const currentUser = data?.me
-
-  const form = useZodForm({
-    schema: editProfileSchema,
-    defaultValues: {
-      name: currentUser?.profile?.name,
-      bio: currentUser?.profile?.bio as string,
-      location: currentUser?.profile?.location as string
-    }
-  })
 
   return (
     <div className="onboarding-bg page-center">
@@ -104,63 +51,13 @@ const Profile: React.FC = () => {
             </div>
           </div>
           <div className="pt-5 max-h-[50vh] overflow-y-auto">
-            {loading ? (
+            {loading && !currentUser ? (
               <div className="p-3 font-bold text-center space-y-2">
                 <Spinner size="md" className="mx-auto" />
                 <div>Get ready to build your profile!</div>
               </div>
             ) : (
-              <Form
-                form={form}
-                className="space-y-4"
-                onSubmit={({ name, bio, location }) =>
-                  editUser({
-                    variables: {
-                      input: {
-                        username: currentUser?.username as string,
-                        email: currentUser?.email as string,
-                        name,
-                        bio: bio as string,
-                        location: location as string,
-                        avatar: currentUser?.profile?.avatar as string,
-                        cover: currentUser?.profile?.cover as string
-                      }
-                    }
-                  })
-                }
-              >
-                <Input
-                  label="Name"
-                  type="text"
-                  placeholder="John Doe"
-                  {...form.register('name')}
-                />
-                <TextArea
-                  label="Bio"
-                  placeholder="Tell us about yourself!"
-                  {...form.register('bio')}
-                />
-                <Input
-                  label="Location"
-                  type="text"
-                  placeholder="Czech Republic"
-                  {...form.register('location')}
-                />
-                <div className="ml-auto">
-                  <Button
-                    type="submit"
-                    icon={
-                      form.formState.isSubmitting ? (
-                        <Spinner size="xs" />
-                      ) : (
-                        <CheckCircleIcon className="h-4 w-4" />
-                      )
-                    }
-                  >
-                    Save
-                  </Button>
-                </div>
-              </Form>
+              <ProfileForm currentUser={currentUser as User} />
             )}
           </div>
         </CardBody>
---
+++ src/components/Onboarding/ProfileForm.tsx
@@ -0,0 +1,120 @@
+import { gql, useMutation } from '@apollo/client'
+import { Button } from '@components/UI/Button'
+import { Form, useZodForm } from '@components/UI/Form'
+import { Input } from '@components/UI/Input'
+import { Spinner } from '@components/UI/Spinner'
+import { TextArea } from '@components/UI/TextArea'
+import { CheckCircleIcon } from '@heroicons/react/outline'
+import React from 'react'
+import toast from 'react-hot-toast'
+import { User } from 'src/__generated__/schema.generated'
+import { object, string } from 'zod'
+
+import {
+  OnboardingProfileSettingsMutation,
+  OnboardingProfileSettingsMutationVariables
+} from './__generated__/ProfileForm.generated'
+
+const editProfileSchema = object({
+  name: string()
+    .min(2, { message: '👤 Name should atleast have 2 characters' })
+    .max(50, { message: '👤 Name should be within 50 characters' }),
+  bio: string()
+    .max(190, { message: '👤 Bio should not exceed 190 characters' })
+    .nullable(),
+  location: string()
+    .max(50, { message: '📍 Location should not exceed 50 characters' })
+    .nullable()
+})
+
+interface Props {
+  currentUser: User
+}
+
+const ProfileForm: React.FC<Props> = ({ currentUser }) => {
+  const [editUser] = useMutation<
+    OnboardingProfileSettingsMutation,
+    OnboardingProfileSettingsMutationVariables
+  >(
+    gql`
+      mutation OnboardingProfileSettingsMutation($input: EditUserInput!) {
+        editUser(input: $input) {
+          id
+        }
+      }
+    `,
+    {
+      onError(error) {
+        toast.error(error.message)
+      },
+      onCompleted() {
+        toast.success('Profile updated successfully!')
+      }
+    }
+  )
+
+  const form = useZodForm({
+    schema: editProfileSchema,
+    defaultValues: {
+      name: currentUser?.profile?.name,
+      bio: currentUser?.profile?.bio as string,
+      location: currentUser?.profile?.location as string
+    }
+  })
+
+  return (
+    <Form
+      form={form}
+      className="space-y-4"
+      onSubmit={({ name, bio, location }) =>
+        editUser({
+          variables: {
+            input: {
+              username: currentUser?.username as string,
+              email: currentUser?.email as string,
+              name,
+              bio: bio as string,
+              location: location as string,
+              avatar: currentUser?.profile?.avatar as string,
+              cover: currentUser?.profile?.cover as string
+            }
+          }
+        })
+      }
+    >
+      <Input
+        label="Name"
+        type="text"
+        placeholder="John Doe"
+        {...form.register('name')}
+      />
+      <TextArea
+        label="Bio"
+        placeholder="Tell us about yourself!"
+        {...form.register('bio')}
+      />
+      <Input
+        label="Location"
+        type="text"
+        placeholder="Czech Republic"
+        {...form.register('location')}
+      />
+      <div className="ml-auto">
+        <Button
+          type="submit"
+          icon={
+            form.formState.isSubmitting ? (
+              <Spinner size="xs" />
+            ) : (
+              <CheckCircleIcon className="h-4 w-4" />
+            )
+          }
+        >
+          Save
+        </Button>
+      </div>
+    </Form>
+  )
+}
+
+export default ProfileForm
