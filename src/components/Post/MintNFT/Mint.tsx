7ef15a3dc5e4a9b850d96a7832057792c39eb340
Yogi <yoginth@hey.com>
2021-10-26 13:25:35 +0000

    chore: update mint modal details

--- src/components/Post/MintNFT/Mint.tsx
+++ src/components/Post/MintNFT/Mint.tsx
@@ -1,5 +1,20 @@
 const Mint: React.FC = () => {
-  return <div>WIP</div>
+  return (
+    <div className="px-5 py-3.5 space-y-5">
+      <div>
+        <div className="font-bold text-lg">Type of NFT</div>
+        <div>WIP</div>
+      </div>
+      <div>
+        <div className="font-bold text-lg">Sale Status and Price</div>
+        <div>WIP</div>
+      </div>
+      <div>
+        <div className="font-bold text-lg">Unlockable Content</div>
+        <div>WIP</div>
+      </div>
+    </div>
+  )
 }
 
 export default Mint
