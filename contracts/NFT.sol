257ac4db7c5fe63ee9f21661097bea5bc4a69acf
Yogi <yoginth@hey.com>
2021-10-26 13:09:45 +0000

    fix: indentation

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,21 +8,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "hardhat/console.sol";
 
 contract NFT is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address contractAddress;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  address contractAddress;
 
-    constructor(address marketplaceAddress) ERC721("Metaverse", "METT") {
-        contractAddress = marketplaceAddress;
-    }
+  constructor(address marketplaceAddress) ERC721("Devparty", "DEV") {
+    contractAddress = marketplaceAddress;
+  }
 
-    function createToken(string memory tokenURI) public returns (uint) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
+  function createToken(string memory tokenURI) public returns (uint) {
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
 
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(contractAddress, true);
-        return newItemId;
-    }
+    _mint(msg.sender, newItemId);
+    _setTokenURI(newItemId, tokenURI);
+    setApprovalForAll(contractAddress, true);
+    return newItemId;
+  }
 }
