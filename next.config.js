290f56bb7bd5be02f83bd8b562cfbeb973d41e48
Yogi <yoginth@hey.com>
2021-10-27 02:31:29 +0000

    feat: add concurrentFeatures

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === 'development',
     runtimeCaching
   },
+  experimental: {
+    concurrentFeatures: true
+  },
   swcMinify: true,
   reactStrictMode: true,
   env: {
