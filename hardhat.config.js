0c647905ba12a389e1c33917fe64dc07434bbe14
Yogi <yoginth@hey.com>
2021-10-27 09:51:33 +0000

    feat: use env var

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,14 @@
-require('@nomiclabs/hardhat-waffle')
-const fs = require('fs')
-const privateKey =
-  fs.readFileSync('.secret').toString().trim() || '01234567890123456789'
+import '@nomiclabs/hardhat-waffle'
 
-module.exports = {
-  defaultNetwork: 'hardhat',
-  networks: {
-    hardhat: { chainId: 1337 },
-    mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
-      accounts: [privateKey]
-    }
-  },
-  solidity: {
-    version: '0.8.9',
-    settings: { optimizer: { enabled: true, runs: 200 } }
+export const defaultNetwork = 'hardhat'
+export const networks = {
+  hardhat: { chainId: 1337 },
+  mumbai: {
+    url: 'https://rpc-mumbai.maticvigil.com',
+    accounts: [process.env.METAMASK_PRIVATE_KEY]
   }
+}
+export const solidity = {
+  version: '0.8.9',
+  settings: { optimizer: { enabled: true, runs: 200 } }
 }
