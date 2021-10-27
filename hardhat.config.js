946b2ee387ef009cdf1188b6764e3751d522a719
Yogi <yoginth@hey.com>
2021-10-27 10:03:24 +0000

    feat: add rinkeby testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,20 @@
-import '@nomiclabs/hardhat-waffle'
+require('@nomiclabs/hardhat-waffle')
 
-export const defaultNetwork = 'hardhat'
-export const networks = {
-  hardhat: { chainId: 1337 },
-  mumbai: {
-    url: 'https://rpc-mumbai.maticvigil.com',
-    accounts: [process.env.METAMASK_PRIVATE_KEY]
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: { chainId: 1337 },
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/3d19324a72854976a7160e0e2ebc9c2b',
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    },
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: '0.8.9',
+    settings: { optimizer: { enabled: true, runs: 200 } }
   }
-}
-export const solidity = {
-  version: '0.8.9',
-  settings: { optimizer: { enabled: true, runs: 200 } }
 }
