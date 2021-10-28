a35b54c07da2a6f52270c18a25e16be771902009
Yogi <yoginth@hey.com>
2021-10-28 11:22:07 +0000

    add: nft check :D

--- src/components/Post/ViewPost.tsx
+++ src/components/Post/ViewPost.tsx
@@ -81,9 +81,14 @@ const ViewPost: React.FC = () => {
       <GridItemFour>
         <div className="space-y-5">
           <UserCard user={post?.user as User} />
-          {currentUser?.id === post?.user?.id && (
-            <MintNFT post={post as Post} />
-          )}
+          {currentUser?.id === post?.user?.id &&
+            (post?.nft ? (
+              <Card>
+                <CardBody>This is already an NFT</CardBody>
+              </Card>
+            ) : (
+              <MintNFT post={post as Post} />
+            ))}
           {currentUser?.isStaff && staffMode && <PostMod post={post as Post} />}
           {post?.type === 'QUESTION' && <MorePosts post={post as Post} />}
         </div>
