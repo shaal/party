782b3b49e5f7c5e4d40304d92fba647a57d9bae9
Yogi <yoginth@hey.com>
2021-10-28 09:06:28 +0000

    feat: add mintingStatus

--- src/components/Post/MintNFT/Mint.tsx
+++ src/components/Post/MintNFT/Mint.tsx
@@ -28,12 +28,18 @@ const Mint: React.FC<Props> = ({ post }) => {
   const router = useRouter()
   const [ethPrice, setEthPrice] = useState<number>(0)
   const [isMinting, setIsMinting] = useState<boolean>(false)
-  const [mintingStatus, setMintingStatus] = useState<string>('')
+  const [mintingStatus, setMintingStatus] = useState<
+    'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED' | 'ERRORED'
+  >('NOT_STARTED')
+  const [mintingStatusText, setMintingStatusText] = useState<string>('')
 
   async function createMarket() {
+    setMintingStatus('IN_PROGRESS')
     setIsMinting(true)
     try {
-      setMintingStatus('Pushing metadata to IPFS')
+      setMintingStatusText(
+        `We're preparing your NFT, We'll ask you to confirm with your wallet shortly`
+      )
       const added = await client.add(JSON.stringify(getNFTData(post)))
       const url = `https://ipfs.infura.io/ipfs/${added.path}`
       createSale(url)
@@ -46,15 +52,14 @@ const Mint: React.FC<Props> = ({ post }) => {
     console.log(NFT_ADDRESS, NFT_MARKET_ADDRESS)
     try {
       // Get signature from the user
-      setMintingStatus('Please sign in your wallet')
       const web3Modal = getWeb3Modal()
       const web3 = new ethers.providers.Web3Provider(await web3Modal.connect())
       const signer = await web3.getSigner()
 
       // Create the item
-      setMintingStatus('Item creation in progress')
       let contract = new ethers.Contract(NFT_ADDRESS as string, NFT.abi, signer)
       let transaction = await contract.createToken(url)
+      setMintingStatusText('Item creation in progress')
       let tx = await transaction.wait()
       let event = tx.events[0]
       let value = event.args[2]
@@ -63,7 +68,6 @@ const Mint: React.FC<Props> = ({ post }) => {
       const price = ethers.utils.parseUnits(ethPrice.toString(), 'ether')
 
       // List the item for sale on the marketplace
-      setMintingStatus('Item listing in progress')
       contract = new ethers.Contract(
         NFT_MARKET_ADDRESS as string,
         Market.abi,
@@ -71,7 +75,7 @@ const Mint: React.FC<Props> = ({ post }) => {
       )
       let listingPrice = await contract.getListingPrice()
       listingPrice = listingPrice.toString()
-
+      setMintingStatusText('Item listing in progress')
       transaction = await contract.createMarketItem(
         NFT_ADDRESS,
         tokenId,
@@ -81,21 +85,24 @@ const Mint: React.FC<Props> = ({ post }) => {
       await transaction.wait()
       console.log(transaction)
       toast.success('Your item has been successfully listed!')
-      setMintingStatus('Listing completed! Reloading this page')
-      router.reload()
+      setMintingStatusText('Listing completed! Reloading this page')
+      setMintingStatus('COMPLETED')
     } catch (error) {
       console.log(error)
       setIsMinting(false)
+      setMintingStatus('ERRORED')
       toast.error('Transaction has been cancelled!')
     }
   }
 
   return (
     <div className="px-5 py-3.5 space-y-3">
-      {isMinting ? (
+      {mintingStatus === 'COMPLETED' ? (
+        <div>Tada</div>
+      ) : isMinting ? (
         <div className="p-5 font-bold text-center space-y-2">
           <Spinner size="md" className="mx-auto" />
-          <div>{mintingStatus}</div>
+          <div>{mintingStatusText}</div>
         </div>
       ) : (
         <div className="space-y-3">
