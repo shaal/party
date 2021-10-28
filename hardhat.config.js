bcc8c9609ab1cecbd5eac0703cc97bc85353f036
Yogi <yoginth@hey.com>
2021-10-28 04:22:39 +0000

    fix: key check

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,22 @@
 require('@nomiclabs/hardhat-waffle')
 
+const privateKey = process.env.METAMASK_PRIVATE_KEY || '01234567890123456789'
+
 module.exports = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: { chainId: 1337 },
     rinkeby: {
       url: 'https://rinkeby.infura.io/v3/3d19324a72854976a7160e0e2ebc9c2b',
-      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
+      accounts: [privateKey]
     },
     mumbai: {
       url: 'https://rpc-mumbai.maticvigil.com',
-      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`]
+      accounts: [privateKey]
     },
     matic: {
       url: 'https://rpc-mainnet.maticvigil.com',
-      accounts: [process.env.METAMASK_PRIVATE_KEY]
+      accounts: [privateKey]
     }
   },
   solidity: {
