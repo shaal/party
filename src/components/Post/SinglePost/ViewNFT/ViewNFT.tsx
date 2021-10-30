614cff6b9714cd09024f8f7f0480b69ae098a769
Yogi <me@yogi.codes>
2021-10-30 19:22:06 +0530

    chore: cleanup solidity contract

--- src/components/Post/SinglePost/ViewNFT/ViewNFT.tsx
+++ src/components/Post/SinglePost/ViewNFT/ViewNFT.tsx
@@ -1,15 +1,10 @@
 import { GridItemSix, GridLayout } from '@components/GridLayout'
-import { Button } from '@components/UI/Button'
 import { ErrorMessage } from '@components/UI/ErrorMessage'
 import { Spinner } from '@components/UI/Spinner'
-import getWeb3Modal from '@components/utils/getWeb3Modal'
 import { useNFT } from '@components/utils/hooks/useNFT'
 import { imagekitURL } from '@components/utils/imagekitURL'
-import { ethers } from 'ethers'
 import { Nft } from 'src/__generated__/schema.generated'
-import { ERROR_MESSAGE, NFT_ADDRESS, NFT_MARKET_ADDRESS } from 'src/constants'
-
-import Market from '../../../../../artifacts/contracts/Market.sol/NFTMarket.json'
+import { ERROR_MESSAGE } from 'src/constants'
 
 interface Props {
   nft: Nft
@@ -21,26 +16,6 @@ const ViewNFT: React.FC<Props> = ({ nft }) => {
     isLoading,
     isError
   } = useNFT(nft?.address, nft?.tokenId)
-
-  const buyNft = async () => {
-    const web3Modal = getWeb3Modal()
-    const web3 = new ethers.providers.Web3Provider(await web3Modal.connect())
-    const signer = await web3.getSigner()
-
-    const contract = new ethers.Contract(
-      NFT_MARKET_ADDRESS as string,
-      Market.abi,
-      signer
-    )
-
-    const price = ethers.utils.parseUnits('1.0', 'ether')
-    const transaction = await contract.createMarketSale(
-      NFT_ADDRESS as string,
-      nft.tokenId,
-      { value: price }
-    )
-    await transaction.wait()
-  }
 
   if (isLoading)
     return (
@@ -80,7 +55,6 @@ const ViewNFT: React.FC<Props> = ({ nft }) => {
         </GridItemSix>
         <GridItemSix className="space-y-3">
           <div className="text-xl font-bold">{metadata?.name}</div>
-          <Button onClick={buyNft}>Buy now</Button>
         </GridItemSix>
       </GridLayout>
     </div>
