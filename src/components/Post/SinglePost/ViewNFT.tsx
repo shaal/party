307bad201c31a438093a4fdf7e2f73c804e8cff2
Yogi <me@yogi.codes>
2021-11-02 12:05:39 +0530

    feat: add Tooltip

--- src/components/Post/SinglePost/ViewNFT.tsx
+++ src/components/Post/SinglePost/ViewNFT.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from '@components/UI/Tooltip'
 import { Nft } from 'src/__generated__/schema.generated'
 import { IS_PRODUCTION, STATIC_ASSETS } from 'src/constants'
 
@@ -8,22 +9,24 @@ interface Props {
 const ViewNFT: React.FC<Props> = ({ nft }) => {
   return (
     <div className="!ml-auto flex items-center">
-      {/* TODO: Update URLs */}
-      <a
-        href={`https://${
-          IS_PRODUCTION ? 'testnets.opensea.io' : 'testnets.opensea.io'
-        }/assets/${IS_PRODUCTION ? 'mumbai' : 'mumbai'}/${nft?.address}/${
-          nft?.tokenId
-        }`}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          className="h-[20px] w-[20px]"
-          src={`${STATIC_ASSETS}/brands/polygon.svg`}
-          alt="Polygon Logo"
-        />
-      </a>
+      <Tooltip content="View in Opensea">
+        {/* TODO: Update URLs */}
+        <a
+          href={`https://${
+            IS_PRODUCTION ? 'testnets.opensea.io' : 'testnets.opensea.io'
+          }/assets/${IS_PRODUCTION ? 'mumbai' : 'mumbai'}/${nft?.address}/${
+            nft?.tokenId
+          }`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="h-[20px] w-[20px]"
+            src={`${STATIC_ASSETS}/brands/polygon.svg`}
+            alt="Polygon Logo"
+          />
+        </a>
+      </Tooltip>
     </div>
   )
 }
