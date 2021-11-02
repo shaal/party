be5ba750381f25bd38c0b66fd6202c682cc4666c
Yogi <me@yogi.codes>
2021-11-02 21:50:45 +0530

    feat: add useIssue hook

---
+++ src/components/utils/hooks/useIssue.ts
@@ -0,0 +1,31 @@
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
+
+/**
+ * Get a issue data from GitHub
+ * @param url - GitHub issue URL
+ * @returns a issue data from GitHub
+ */
+export const useIssue = (
+  url: string
+): { issue: any; slug: string; isLoading: boolean; isError: any } => {
+  const splitedURL = new URL(url).pathname.split('/')
+
+  const { data, error } = useSWR(
+    `https://api.github.com/repos/${splitedURL[1]}/${splitedURL[2]}/issues/${splitedURL[4]}`,
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false
+    }
+  )
+
+  return {
+    issue: data,
+    slug: `${splitedURL[1]}/${splitedURL[2]}`,
+    isLoading: !error && !data,
+    isError: error || data?.message
+  }
+}
