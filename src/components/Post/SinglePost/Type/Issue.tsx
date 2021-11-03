43577824ebe6857be1d5310261a44ec64ed88754
Yogi <me@yogi.codes>
2021-11-03 10:01:04 +0530

    feat: add assignees and dynamic link

--- src/components/Post/SinglePost/Type/Issue.tsx
+++ src/components/Post/SinglePost/Type/Issue.tsx
@@ -3,12 +3,14 @@ import 'linkify-plugin-mention'
 
 import { Tooltip } from '@components/UI/Tooltip'
 import { useIssue } from '@components/utils/hooks/useIssue'
+import { imagekitURL } from '@components/utils/imagekitURL'
 import {
   CheckCircleIcon,
   ExclamationCircleIcon
 } from '@heroicons/react/outline'
 import { CheckCircleIcon as CheckCircleIconSolid } from '@heroicons/react/solid'
 import Markdown from 'markdown-to-jsx'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 import { Post } from 'src/__generated__/schema.generated'
@@ -31,32 +33,55 @@ const IssueType: React.FC<Props> = ({ post }) => {
       </div>
     )
 
+  const isPostPage = router.pathname === '/posts/[postId]'
+
   return (
     <div className="space-y-3 linkify">
       <div>
-        <a
-          className="text-lg font-bold mb-1 flex items-center space-x-2"
-          href={issue?.html_url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {issue?.state === 'open' ? (
-            <Tooltip content="Open">
-              <CheckCircleIcon className="h-5 w-5 text-green-500" />
-            </Tooltip>
-          ) : (
-            <Tooltip content="Closed">
-              <CheckCircleIconSolid className="h-5 w-5 text-brand-500" />
-            </Tooltip>
-          )}
-          <div>{issue?.title}</div>
-          <div className="text-gray-500 font-normal">#{issue?.number}</div>
-        </a>
-        {router.pathname === '/posts/[postId]' && (
+        <Link href={isPostPage ? issue?.html_url : `/posts/${post?.id}`}>
+          <a
+            className="text-lg font-bold mb-1 flex items-center space-x-2"
+            href={isPostPage ? issue?.html_url : `/posts/${post?.id}`}
+            target={isPostPage ? '_blank' : '_self'}
+            rel="noreferrer"
+          >
+            {issue?.state === 'open' ? (
+              <Tooltip content="Open">
+                <CheckCircleIcon className="h-5 w-5 text-green-500" />
+              </Tooltip>
+            ) : (
+              <Tooltip content="Closed">
+                <CheckCircleIconSolid className="h-5 w-5 text-brand-500" />
+              </Tooltip>
+            )}
+            <div>{issue?.title}</div>
+            <div className="text-gray-500 font-normal">#{issue?.number}</div>
+          </a>
+        </Link>
+        {isPostPage && (
           <div className="prose">
             <Markdown options={{ wrapper: 'article' }}>{issue?.body}</Markdown>
           </div>
         )}
+        <div className="space-y-2 pt-3">
+          <div className="font-bold">Assignees</div>
+          <div className="flex space-x-1.5 overflow-hidden">
+            {issue?.assignees?.map((user: any) => (
+              <a
+                key={user?.id}
+                href={`https://github.com/${user?.login}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="rounded-full border h-7 w-7"
+                  src={imagekitURL(user?.avatar_url, 100, 100)}
+                  alt={`@${user?.login}`}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
