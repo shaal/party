f8b598300378d4b02bb03540216bf40d074d1fd9
Yogi <me@yogi.codes>
2021-11-04 16:02:45 +0530

    feat: add more tests

--- e2e/pages/pages.spec.ts
+++ e2e/pages/pages.spec.ts
@@ -26,4 +26,23 @@ test.describe('Pages', () => {
     await expect(content).toContain('Thank you!')
     await expect(content).toContain('for supporting our community')
   })
+
+  test('404 page should contains test texts', async ({ page }) => {
+    await page.goto('/404')
+    const content = await page.content()
+    await expect(content).toContain('Oops, Lost‽')
+    await expect(content).toContain('This page could not be found.')
+  })
+
+  test('500 page should contains test texts', async ({ page }) => {
+    await page.goto('/500')
+    const content = await page.content()
+    await expect(content).toContain('Looks like something went wrong!')
+  })
+
+  test('offline page should contains test texts', async ({ page }) => {
+    await page.goto('/_offline')
+    const content = await page.content()
+    await expect(content).toContain('You are offline!')
+  })
 })
