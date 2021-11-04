fbf96cec0b86ca1f83142563270696e8a4733ff0
Yogi <me@yogi.codes>
2021-11-04 15:44:34 +0530

    test: add describe

--- e2e/landing.spec.ts
+++ e2e/landing.spec.ts
@@ -1,20 +1,22 @@
 import { expect, test } from '@playwright/test'
 
-test('page should containt "Get started"', async ({ page }) => {
-  await page.goto('/')
-  const name = await page.content()
-  await expect(name).toContain('Get started')
-  await expect(name).toContain('Join Devparty today.')
-})
+test.describe('Landing Page', () => {
+  test('page should containt "Get started"', async ({ page }) => {
+    await page.goto('/')
+    const name = await page.content()
+    await expect(name).toContain('Get started')
+    await expect(name).toContain('Join Devparty today.')
+  })
 
-test('redirect to signup page', async ({ page }) => {
-  await page.goto('/')
-  await page.click('text=Get started')
-  await expect(page).toHaveURL('/signup')
-})
+  test('redirect to signup page', async ({ page }) => {
+    await page.goto('/')
+    await page.click('text=Get started')
+    await expect(page).toHaveURL('/signup')
+  })
 
-test('redirect to login page', async ({ page }) => {
-  await page.goto('/')
-  await page.click('text=Log in')
-  await expect(page).toHaveURL('/login')
+  test('redirect to login page', async ({ page }) => {
+    await page.goto('/')
+    await page.click('text=Log in')
+    await expect(page).toHaveURL('/login')
+  })
 })
