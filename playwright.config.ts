1208b9b3008c957540c6f93f4c23e5d61647fae3
Yogi <yoginth@hey.com>
2021-11-05 05:25:25 +0000

    feat: move to testDir and webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,13 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
+  testDir: 'test',
+  webServer: {
+    command: 'yarn dev',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI
+  },
   use: {
     baseURL: 'http://localhost:3000'
   }
