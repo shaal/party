f34cea19501747fdb051d1b01117d12c4506753e
Yogi <me@yogi.codes>
2021-11-05 14:06:22 +0530

    chore: remove oembed test

--- test/pages/api/oembed.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('get oembed', async ({ request }) => {
-  const response = await request.get('/api/oembed', {
-    params: { url: 'https://github.com' }
-  })
-  expect(response.ok()).toBeTruthy()
-})
