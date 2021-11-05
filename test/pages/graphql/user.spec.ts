e3eb0af40f751796fcbf898e3908c88fe59d46c4
Yogi <me@yogi.codes>
2021-11-05 12:40:47 +0530

    test: add ownedProducts test

--- test/pages/graphql/user.spec.ts
+++ test/pages/graphql/user.spec.ts
@@ -11,8 +11,9 @@ test('get single user', async ({ request }) => {
     }
   })
   const result = await response.json()
+  const user = result.data.user
 
-  expect(result.data.user.username).toBe('yoginth')
+  expect(user.username).toBe('yoginth')
 })
 
 test('get multiple users', async ({ request }) => {
@@ -32,4 +33,44 @@ test('get multiple users', async ({ request }) => {
   const result = await response.json()
 
   expect(result.data.users.edges.length).toBe(20)
+})
+
+test('user should have profile', async ({ request }) => {
+  const response = await request.post('/api/graphql', {
+    data: {
+      query: `{
+        user(username: "yoginth") {
+          profile {
+            name
+          }
+        }
+      }`
+    }
+  })
+  const result = await response.json()
+  const user = result.data.user
+
+  expect(user.profile.name).toBe('Yoginth')
+})
+
+test('user should owned products', async ({ request }) => {
+  const response = await request.post('/api/graphql', {
+    data: {
+      query: `{
+        user(username: "yoginth") {
+          ownedProducts(first: 5) {
+            edges {
+              node {
+                id
+              }
+            }
+          }
+        }
+      }`
+    }
+  })
+  const result = await response.json()
+  const user = result.data.user
+
+  expect(user.ownedProducts.edges.length).toBe(5)
 })
