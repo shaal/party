1c3ad4e62dbcd3704cbd55ed3273f41f55d1732e
Yogi <me@yogi.codes>
2021-11-05 12:36:25 +0530

    test: add done status check

--- test/pages/graphql/post.spec.ts
+++ test/pages/graphql/post.spec.ts
@@ -11,6 +11,43 @@ test('get single post', async ({ request }) => {
     }
   })
   const result = await response.json()
+  const post = result.data.post
 
-  expect(result.data.post.id).toBe('89bee9b8-a958-48de-8c9d-55e20b75ccf2')
+  expect(post.id).toBe('89bee9b8-a958-48de-8c9d-55e20b75ccf2')
+})
+
+test('post should have user', async ({ request }) => {
+  const response = await request.post('/api/graphql', {
+    data: {
+      query: `{
+        post(id: "89bee9b8-a958-48de-8c9d-55e20b75ccf2") {
+          id
+          user {
+            id
+          }
+        }
+      }`
+    }
+  })
+  const result = await response.json()
+  const post = result.data.post
+
+  expect(post.user.id.length).toBe(36)
+})
+
+test('post done status shoud be true/false', async ({ request }) => {
+  const response = await request.post('/api/graphql', {
+    data: {
+      query: `{
+        post(id: "89bee9b8-a958-48de-8c9d-55e20b75ccf2") {
+          id
+          done
+        }
+      }`
+    }
+  })
+  const result = await response.json()
+  const post = result.data.post
+
+  expect(typeof post.done).toBe('boolean')
 })
