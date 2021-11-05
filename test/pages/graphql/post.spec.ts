2a446fcfd7710f50a1b82f6c8ec99bfc0beee2cb
Yogi <yoginth@hey.com>
2021-11-05 11:05:39 +0000

    test: add more count check

--- test/pages/graphql/post.spec.ts
+++ test/pages/graphql/post.spec.ts
@@ -68,8 +68,28 @@ test('connection should have count', async ({ request }) => {
     }
   })
   const result = await response.json()
-  const post = result.data.user
+  const post = result.data.post
 
   expect(typeof post.likes.totalCount).toBe('number')
   expect(typeof post.replies.totalCount).toBe('number')
+})
+
+test('flags should return boolean', async ({ request }) => {
+  const response = await request.post('/api/graphql', {
+    data: {
+      query: `{
+        post(id: "89bee9b8-a958-48de-8c9d-55e20b75ccf2") {
+          done
+          hasBookmarked
+          hasLiked
+        }
+      }`
+    }
+  })
+  const result = await response.json()
+  const post = result.data.post
+
+  expect(typeof post.done).toBe('boolean')
+  expect(typeof post.hasBookmarked).toBe('boolean')
+  expect(typeof post.hasLiked).toBe('boolean')
 })
