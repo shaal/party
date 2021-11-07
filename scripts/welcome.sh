37fa410e2739bf5fde7d813f279497da33cd58af
Yogi <yoginth@hey.com>
2021-11-07 03:48:50 +0000

    feat: update welcome script

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-echo "Welcome to Devparty 🥳"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+URL=$(eval "eval gp url 3000")
+
+echo "👋 ${GREEN}Welcome to Devparty${NC}\n"
+echo "Visit: ${BLUE}${URL}${NC}"
