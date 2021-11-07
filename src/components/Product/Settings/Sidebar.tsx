68bc713bd4505b1d18328fa95d8f599d25294494
Yogi <yoginth@hey.com>
2021-11-07 05:17:31 +0000

    feat: add sidebar

---
+++ src/components/Product/Settings/Sidebar.tsx
@@ -0,0 +1,50 @@
+import { ShareIcon, UserIcon } from '@heroicons/react/outline'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+interface MenuProps {
+  children: React.ReactNode
+  current: boolean
+  url: string
+}
+
+const Menu: React.FC<MenuProps> = ({ children, current, url }) => (
+  <Link href={url}>
+    <a
+      href={url}
+      className={clsx(
+        'flex items-center space-x-2 rounded-lg px-3 py-2 hover:bg-brand-100 hover:text-brand-500 dark:hover:bg-opacity-20 dark:bg-opacity-20 hover:bg-opacity-100',
+        { 'bg-brand-100 text-brand-500 font-bold': current }
+      )}
+    >
+      {children}
+    </a>
+  </Link>
+)
+
+const Sidebar: React.FC = () => {
+  const router = useRouter()
+
+  return (
+    <div className="space-y-1.5 mb-4 px-3 sm:px-0">
+      <Menu
+        current={router.pathname == '/settings/profile'}
+        url="/settings/profile"
+      >
+        <UserIcon className="h-4 w-4" />
+        <div>Profile</div>
+      </Menu>
+      <Menu
+        current={router.pathname == '/settings/social'}
+        url="/settings/social"
+      >
+        <ShareIcon className="h-4 w-4" />
+        <div>Social</div>
+      </Menu>
+    </div>
+  )
+}
+
+export default Sidebar
