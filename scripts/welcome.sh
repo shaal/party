465aab4587348d1a674629327403384ac19fde35
Yogi <yoginth@hey.com>
2021-11-07 04:51:49 +0000

    feat: update commands

--- scripts/welcome.sh
+++ scripts/welcome.sh
@@ -3,6 +3,8 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
 NC='\033[0m'
 
 BOLD=$(tput bold)
@@ -10,9 +12,10 @@ NORMAL=$(tput sgr0)
 
 URL=$(eval "eval gp url 3000")
 
-echo "👋 ${GREEN}${BOLD}Welcome to Devparty${NORMAL}${NC}\n"
+echo "\n👋 ${GREEN}${BOLD}Welcome to Devparty${NORMAL}${NC}\n"
 echo "💻 ${BOLD}Commands:${NORMAL}\n"
-awk -F':' '{printf "%s%s", $2, (/^Description/)?"\n":"\t\t\t"}' ./scripts/commands.txt
-echo "\nDevelopment admin username: admin"
-echo "Development admin password: admin\n"
-echo "Visit: ${BLUE}${URL}${NC}"
+awk -F':' '{printf "%s%s", $2, (/^Description/)?"\n":"\t\t"}' ./scripts/commands.txt
+echo "\nDevelopment admin username: ${CYAN}${BOLD}admin${NORMAL}${NC}"
+echo "Development admin password: ${CYAN}${BOLD}admin${NORMAL}${NC}\n"
+echo "Visit: ${BLUE}${URL}${NC}\n"
+echo "${PURPLE}${BOLD}Happy coding${NORMAL}${NC} 😍\n"
