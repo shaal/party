aaa9183f4791278e92a76a455092d62204fa2188
Yogi <yoginth@hey.com>
2021-11-08 06:04:10 +0000

    chore: update config

--- scripts/gp-init.sh
+++ scripts/gp-init.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 yarn install --frozen-lockfile --silent --network-timeout 100000
-gp sync-done yarn
 cp .env.example .env
-gp sync-done env
 gp await-port 3306
 yarn prisma:migrate
-gp sync-done migrate
 yarn prisma:seed
-gp sync-done seed
 yarn prisma:testseed
-gp sync-done testseed
 yarn prepare
-gp sync-done prepare
--- scripts/gp-welcome.sh
+++ scripts/gp-welcome.sh
@@ -6,55 +6,13 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m'
-
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
-
-clear
-
-echo -e "\n✅ ${GREEN}${BOLD}VS Code is ready${NORMAL}${NC} ✌️"
-
-if [ "$(eval "gp sync-await yarn")" == "yarn done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Installed yarn dependencies${NORMAL}${NC} 📦"
-fi
-
-if [ "$(eval "gp sync-await env")" == "env done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Global environment variable setup done${NORMAL}${NC} 🖥️"
-fi
-
-if [ "$(eval "gp sync-await migrate")" == "migrate done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Database migration is completed${NORMAL}${NC} 🐘"
-fi
-
-if [ "$(eval "gp sync-await seed")" == "seed done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Database seeding is completed${NORMAL}${NC} 🌱"
-fi
-
-if [ "$(eval "gp sync-await testseed")" == "testseed done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Database test seeding is completed${NORMAL}${NC} 🧪"
-fi
-
-if [ "$(eval "gp sync-await prepare")" == "prepare done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Final preparation done${NORMAL}${NC} ‍🍳"
-fi
-
-if [ "$(eval "gp sync-await redis")" == "redis done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Redis server started${NORMAL}${NC} 📚"
-fi
-
-if [ "$(eval "gp sync-await codegen")" == "codegen done" ]; then
-    echo -e "✅ ${GREEN}${BOLD}GraphQL Codegen server started${NORMAL}${NC} 🌍"
-fi
-
-if [ "$(eval "gp await-port 3000")" == "Awaiting port 3000... ok" ]; then
-    echo -e "✅ ${GREEN}${BOLD}Development server started${NORMAL}${NC} 🚀"
-fi
-
 URL=$(eval "eval gp url 3000")
 
+clear
 curl -s "$URL/api/graphql?warmup=true" > /dev/null
 echo -e "✅ ${GREEN}${BOLD}GraphQL Server warmed up${NORMAL}${NC} 🔥"
-
 echo -e "\n👋 ${GREEN}${BOLD}Welcome to Devparty${NORMAL}${NC}\n"
 echo -e "💻 ${BOLD}Commands:${NORMAL}\n"
 awk -F':' '{printf "%s%s", $2, (/^Description/)?"\n":"\t\t"}' ./scripts/commands.txt
