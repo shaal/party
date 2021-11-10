fa49e5b10dbd29ff274ce89112c70e2078d80cb7
Yogi <yoginth@hey.com>
2021-11-10 08:44:33 +0000

    feat: add thanks message

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,11 @@
-import { danger, warn } from 'danger'
+import { danger, message, warn } from 'danger'
+
+const context = danger.gitlab
+
+// Say thanks
+message(`Thanks for the MR @${context.mr.author.username}`)
 
 // No MR is too small to include a description of why you made a change
-if (danger.gitlab.mr.description.length < 10) {
+if (context.mr.description.length < 10) {
   warn('Please include a description of your MR changes.')
 }
