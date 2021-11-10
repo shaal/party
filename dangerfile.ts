8a757a7ddf69161626c7e8f5c37abcff7c6df27a
Yogi <yoginth@hey.com>
2021-11-10 08:46:11 +0000

    WIP

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,7 +3,9 @@ import { danger, message, warn } from 'danger'
 const context = danger.gitlab
 
 // Say thanks
-message(`Thanks for the MR @${context.mr.author.username}`)
+message(
+  `Thanks for creating the the MR @${context.mr.author.username} 💝\n\n /cc @yo can you please review this 🙇‍♂️`
+)
 
 // No MR is too small to include a description of why you made a change
 if (context.mr.description.length < 10) {
