03005eaf014e3d8406d6b8423940e6d6ab0de5ce
Yogi <yoginth@hey.com>
2021-11-12 04:28:24 +0000

    feat: add forwardRef to card

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,14 +1,17 @@
-interface CardProps {
+import { ComponentProps, forwardRef } from 'react'
+
+interface CardProps extends ComponentProps<'div'> {
   children: React.ReactNode
   className?: string
   forceRounded?: boolean
 }
 
-export const Card: React.FC<CardProps> = ({
+export const Card = forwardRef<HTMLInputElement, CardProps>(function Card({
   children,
+  forceRounded = false,
   className = '',
-  forceRounded = false
-}) => {
+  ...props
+}) {
   return (
     <div
       className={`border dark:border-gray-800 bg-white dark:bg-gray-900 ${
@@ -16,23 +19,12 @@ export const Card: React.FC<CardProps> = ({
           ? 'rounded-lg'
           : 'lg:rounded-lg md:rounded-lg sm:rounded-lg'
       } shadow-sm dark:shadow-md ${className}`}
+      {...props}
     >
       {children}
     </div>
   )
-}
-
-interface CardHeaderProps {
-  children: React.ReactNode
-  className?: string
-}
-
-export const CardHeader: React.FC<CardHeaderProps> = ({
-  children,
-  className = ''
-}) => {
-  return <div className={`border-b p-3 ${className}`}>{children}</div>
-}
+})
 
 interface CardBodyProps {
   children: React.ReactNode
